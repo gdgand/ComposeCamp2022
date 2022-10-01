43a6b3e9f5860a24b8a8912acb611e7778e3f8d9
Veronikapj <sherlockpj@hotmail.com>
2022-10-01 15:31:28 +0900

    import Compose codelabs

---
+++ week1/BasicsCodelab/README.md
@@ -0,0 +1,10 @@
+# Jetpack Compose Basic Codelab
+
+Week1 Jetpack Compose Basic 코드랩은 새 프로젝트를 만드는 것으로 시작합니다.
+
+## 작업 전 필수 확인
+Basics에 올려둔 프로젝트는 new project 입니다. 바로 이 프로젝트 위에서 코드랩을 시작하시면 됩니다. 
+
+## 작업 후
+직접 생성한 프로젝트를 여기에 붙여 넣어주세요.
+Git 명령어 또는 [SourceTree](https://www.sourcetreeapp.com/), [GitKraken](https://www.gitkraken.com/) 등을 이용해 작업 결과를 push 해주세요.
---
+++ week3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/util/NetworkUtil.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.samples.crane.util
+
+import coil.annotation.ExperimentalCoilApi
+import coil.intercept.Interceptor
+import coil.request.ImageResult
+import coil.size.Size
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
+
+/**
+ * A Coil [Interceptor] which appends query params to Unsplash urls to request sized images.
+ */
+@OptIn(ExperimentalCoilApi::class)
+object UnsplashSizingInterceptor : Interceptor {
+    override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
+        val data = chain.request.data
+        val size = chain.size
+        if (data is String &&
+            data.startsWith("https://images.unsplash.com/photo-")
+        ) {
+            val url = data.toHttpUrl()
+                .newBuilder()
+                .addQueryParameter("w", size.width.toString())
+                .addQueryParameter("h", size.height.toString())
+                .build()
+            val request = chain.request.newBuilder().data(url).build()
+            return chain.proceed(request)
+        }
+        return chain.proceed(chain.request)
+    }
+}
