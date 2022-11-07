64d19ce1bdf47a0a5f87370dd6283716440e6b0f
shim seok bo <shadowog@gmail.com>
2022-11-07 22:03:47 +0900

    열과 행 만들기 - 버튼 추가 - 코드랩과 맞춤

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,14 +50,13 @@ fun Greeting(name: String) {
             .fillMaxWidth()
     ) {
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier.padding(24.dp)
         ) {
-            Column(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { }, modifier = Modifier.padding(24.dp)) {
+            OutlinedButton(onClick = { } ) {
                 Text(text = "Show less")
             }
         }
