b47f31def940f48ecb2136ce9bdb68993002c595
shim seok bo <shadowog@gmail.com>
2022-11-07 21:59:50 +0900

    열과 행 만들기 - 버튼 추가 - 코드랩과 다름

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,12 +43,23 @@ fun MyApp(names : List<String> = listOf("World","Compose")){
 fun Greeting(name: String) {
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .fillMaxWidth()
     ) {
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello")
-            Text(text = name)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.padding(24.dp)) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { }, modifier = Modifier.padding(24.dp)) {
+                Text(text = "Show less")
+            }
         }
+
     }
 }
 
