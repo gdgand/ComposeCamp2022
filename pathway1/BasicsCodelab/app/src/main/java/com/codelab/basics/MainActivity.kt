2746d17915da3848f6a3a5ea4ed9d7f196928b80
NetLSS <futureprogrammer@naver.com>
2022-11-07 21:43:25 +0900

    Feat: Column 펼치기

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -49,14 +49,21 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
  */
 @Composable
 private fun Greeting(name: String) {
+
     val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
