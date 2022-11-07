9bc9f42b6ec1f14036455471b60866a84c991aef
ParkJongHyeon <bll901109@gmail.com>
2022-11-07 18:15:57 +0900

    1-2주차 BasicStateCodelab 코드 완료

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -3,13 +3,21 @@ package com.codelabs.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
@@ -39,5 +47,110 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     BasicStateCodelabTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+//            var showTask by remember { mutableStateOf(true) }
+//            if (showTask) {
+//                WellnessTaskItem(
+//                    onClose = { showTask = false },
+//                    taskName = stringResource(id = R.string.task_name)
+//                )
+//            }
+            Text(text = stringResource(id = R.string.water_count_format, count))
+        }
+        Row(modifier = Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text(stringResource(id = R.string.button_add_one))
+            }
+//            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+//                Text(stringResource(id = R.string.button_clear_water_count))
+//            }
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
+
+    StatelessCounter(count, { count++ })
+//    AnotherStatelessMethod(count, { count *= 2 })
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = stringResource(id = R.string.water_count_format, count))
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text(stringResource(id = R.string.button_add_one))
+        }
+    }
+}
+
+//@Composable
+//fun WellnessTaskItem(
+//    taskName: String,
+//    checked: Boolean,
+//    onCheckedChange: (Boolean) -> Unit,
+//    onClose: () -> Unit,
+//    modifier: Modifier = Modifier
+//) {
+//    Row(
+//        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+//    ) {
+//        Text(
+//            modifier = Modifier
+//                .weight(1f)
+//                .padding(start = 16.dp),
+//            text = taskName
+//        )
+//        Checkbox(
+//            checked = checked,
+//            onCheckedChange = onCheckedChange
+//        )
+//        IconButton(onClick = onClose) {
+//            Icon(Icons.Filled.Close, contentDescription = "Close")
+//        }
+//    }
+//}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by remember { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = {}, // we will implement this later!
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
     }
 }
