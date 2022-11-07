d784f0e8b5e7a8a220a6c739b490065da0cac233
elis0m <elisom615@gmail.com>
2022-11-07 17:02:00 +0900

    feat: rememberSaveable, LazyColumn 사용

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnBoardingScreen {
@@ -68,11 +71,11 @@ fun OnboardingPreview() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("eli", "som")) {
-    Column(
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(
         modifier = Modifier.padding(vertical = 4.dp)
     ) {
-        for (name in names) {
+        items(items = names) { name ->
             Greeting(name)
         }
     }
