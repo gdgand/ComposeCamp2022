bcf88097446829ca25ed11e349569073d0f17d15
yc.park <yc.park@deepfine.ai>
2022-11-09 08:26:23 +0900

    feat($compose): Creating columns and rows

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,10 +3,9 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,30 +19,45 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       BasicsCodelabTheme {
-        MyApp(modifier = Modifier.fillMaxSize())
+        MyApp()
       }
     }
   }
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier) {
-  Surface(
-    modifier = modifier,
-    color = MaterialTheme.colors.background
-  ) {
-    Greeting("Android")
+fun MyApp(
+  modifier: Modifier = Modifier,
+  names: List<String> = listOf("World", "Compose")
+) {
+  Column(modifier = modifier.padding(vertical = 4.dp)) {
+    for (name in names) {
+      Greeting(name = name)
+    }
   }
 }
 
 @Composable
 private fun Greeting(name: String) {
-  Surface(color = MaterialTheme.colors.primary) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+  Surface(
+    color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  ) {
+    Row(modifier = Modifier.padding(24.dp)) {
+      Column(modifier = Modifier.weight(1f)) {
+        Text(text = "Hello, ")
+        Text(text = name)
+      }
+      OutlinedButton(
+        onClick = { /* TODO */ }
+      ) {
+        Text("Show more")
+      }
+    }
   }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
   BasicsCodelabTheme {
