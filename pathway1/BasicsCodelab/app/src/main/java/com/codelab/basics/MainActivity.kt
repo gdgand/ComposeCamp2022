3f3af329a253b2c116e63fffce7a449f0d486f13
rnfkehdrlf <rnfkehdrlf@gmail.com>
2022-11-08 11:08:11 +0900

    test commit

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -14,6 +14,7 @@ import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         //test
         setContent {
