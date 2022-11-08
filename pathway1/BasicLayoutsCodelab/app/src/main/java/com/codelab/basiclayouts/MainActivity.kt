8b807d0b7abc5d93c2429621413423e315d0db13
정재환 <pon5976@naver.com>
2022-11-08 18:47:01 +0900

    [정재환] 5. 정렬 완료.

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,167 +21,209 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent { MySootheApp() }
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent { MySootheApp() }
+  }
 }
 
 // Step: Search bar - Modifiers
 @Composable
 fun SearchBar(
-    modifier: Modifier = Modifier
+  modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+  TextField(
+    value = "",
+    onValueChange = {},
+    leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+    colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+    placeholder = { Text(stringResource(id = R.string.placeholder_search)) },
+    modifier = modifier
+      .fillMaxWidth()
+      .heightIn(min = 56.dp)
+  )
 }
 
 // Step: Align your body - Alignment
 @Composable
 fun AlignYourBodyElement(
-    modifier: Modifier = Modifier
+  @DrawableRes drawable: Int,
+  @StringRes text : Int,
+  modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+  Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = modifier
+  ) {
+    Image(
+      painter = painterResource(drawable),
+      contentDescription = null,
+      contentScale = ContentScale.Crop,
+      modifier = Modifier
+        .size(88.dp)
+        .clip(CircleShape)
+    )
+
+    Text(
+      text = stringResource(text),
+      style = MaterialTheme.typography.h3,
+      modifier = Modifier.paddingFromBaseline(
+        top = 24.dp, bottom = 8.dp
+      )
+    )
+  }
 }
 
 // Step: Favorite collection card - Material Surface
 @Composable
 fun FavoriteCollectionCard(
-    modifier: Modifier = Modifier
+  modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+  // Implement composable here
 }
 
 // Step: Align your body row - Arrangements
 @Composable
 fun AlignYourBodyRow(
-    modifier: Modifier = Modifier
+  modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+  // Implement composable here
 }
 
 // Step: Favorite collections grid - LazyGrid
 @Composable
 fun FavoriteCollectionsGrid(
-    modifier: Modifier = Modifier
+  modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+  // Implement composable here
 }
 
 // Step: Home section - Slot APIs
 @Composable
 fun HomeSection(
-    modifier: Modifier = Modifier
+  modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+  // Implement composable here
 }
 
 // Step: Home screen - Scrolling
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    // Implement composable here
+  // Implement composable here
 }
 
 // Step: Bottom navigation - Material
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
-    // Implement composable here
+  // Implement composable here
 }
 
 // Step: MySoothe App - Scaffold
 @Composable
 fun MySootheApp() {
-    // Implement composable here
+  // Implement composable here
 }
 
 private val alignYourBodyData = listOf(
-    R.drawable.ab1_inversions to R.string.ab1_inversions,
-    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
-    R.drawable.ab3_stretching to R.string.ab3_stretching,
-    R.drawable.ab4_tabata to R.string.ab4_tabata,
-    R.drawable.ab5_hiit to R.string.ab5_hiit,
-    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+  R.drawable.ab1_inversions to R.string.ab1_inversions,
+  R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+  R.drawable.ab3_stretching to R.string.ab3_stretching,
+  R.drawable.ab4_tabata to R.string.ab4_tabata,
+  R.drawable.ab5_hiit to R.string.ab5_hiit,
+  R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
 ).map { DrawableStringPair(it.first, it.second) }
 
 private val favoriteCollectionsData = listOf(
-    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
-    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
-    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
-    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
-    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
-    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+  R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+  R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+  R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+  R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+  R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+  R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
 ).map { DrawableStringPair(it.first, it.second) }
 
 private data class DrawableStringPair(
-    @DrawableRes val drawable: Int,
-    @StringRes val text: Int
+  @DrawableRes val drawable: Int, @StringRes val text: Int
 )
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun SearchBarPreview() {
-    MySootheTheme { SearchBar(Modifier.padding(8.dp)) }
+  MySootheTheme { SearchBar(Modifier.padding(8.dp)) }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun AlignYourBodyElementPreview() {
-    MySootheTheme {
-        AlignYourBodyElement(
-            modifier = Modifier.padding(8.dp)
-        )
-    }
+  MySootheTheme {
+    AlignYourBodyElement(
+      R.drawable.ab1_inversions,
+      R.string.ab1_inversions,
+      modifier = Modifier.padding(8.dp)
+    )
+  }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun FavoriteCollectionCardPreview() {
-    MySootheTheme {
-        FavoriteCollectionCard(
-            modifier = Modifier.padding(8.dp)
-        )
-    }
+  MySootheTheme {
+    FavoriteCollectionCard(
+      modifier = Modifier.padding(8.dp)
+    )
+  }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun FavoriteCollectionsGridPreview() {
-    MySootheTheme { FavoriteCollectionsGrid() }
+  MySootheTheme { FavoriteCollectionsGrid() }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun AlignYourBodyRowPreview() {
-    MySootheTheme { AlignYourBodyRow() }
+  MySootheTheme { AlignYourBodyRow() }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun HomeSectionPreview() {
-    MySootheTheme { HomeSection() }
+  MySootheTheme { HomeSection() }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun ScreenContentPreview() {
-    MySootheTheme { HomeScreen() }
+  MySootheTheme { HomeScreen() }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun BottomNavigationPreview() {
-    MySootheTheme { SootheBottomNavigation(Modifier.padding(top = 24.dp)) }
+  MySootheTheme { SootheBottomNavigation(Modifier.padding(top = 24.dp)) }
 }
 
 @Preview(widthDp = 360, heightDp = 640)
 @Composable
 fun MySoothePreview() {
-    MySootheApp()
+  MySootheApp()
 }
