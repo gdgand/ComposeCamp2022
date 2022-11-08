4f82df3551533442b40560d8b97b59501cf90e04
yc.park <yc.park@deepfine.ai>
2022-11-09 08:34:51 +0900

    feat($compose): State hoisting
    In Composable functions, state that is read or modified by multiple functions should live in a common ancestor—this process is called state hoisting.

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,14 +24,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(
-  modifier: Modifier = Modifier,
-  names: List<String> = listOf("World", "Compose")
-) {
-  Column(modifier = modifier.padding(vertical = 4.dp)) {
+fun MyApp() {
+  var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+  if (shouldShowOnboarding) {
+    OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+  } else {
+    Greetings()
+  }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+  Surface {
+    Column(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text("Welcome to the Basics Codelab!")
+      Button(
+        modifier = Modifier.padding(vertical = 24.dp),
+        onClick = onContinueClicked
+      ) {
+        Text("Continue")
+      }
+    }
+  }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+  Column(modifier = Modifier.padding(vertical = 4.dp)) {
     for (name in names) {
       Greeting(name = name)
     }
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+  BasicsCodelabTheme {
+    OnboardingScreen(onContinueClicked = {})
   }
 }
 
@@ -50,9 +81,10 @@ private fun Greeting(name: String) {
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
   ) {
     Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier
-        .weight(1f)
-        .padding(bottom = extraPadding)
+      Column(
+        modifier = Modifier
+          .weight(1f)
+          .padding(bottom = extraPadding)
       ) {
         Text(text = "Hello, ")
         Text(text = name)
@@ -70,6 +102,6 @@ private fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
   BasicsCodelabTheme {
-    MyApp()
+    Greetings()
   }
 }
