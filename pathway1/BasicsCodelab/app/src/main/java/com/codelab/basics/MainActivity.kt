6fb3c5c6e739302fe082a27412406a66806cd3e4
yc.park <yc.park@deepfine.ai>
2022-11-09 08:59:59 +0900

    feat($compose): Finishing touches!

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -17,7 +18,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +37,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
+private fun MyApp() {
   var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
   if (shouldShowOnboarding) {
@@ -43,7 +48,7 @@ fun MyApp() {
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
+private fun OnboardingScreen(onContinueClicked: () -> Unit) {
   Surface {
     Column(
       modifier = Modifier.fillMaxSize(),
@@ -70,45 +75,59 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
   }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun OnboardingPreview() {
-  BasicsCodelabTheme {
-    OnboardingScreen(onContinueClicked = {})
+private fun Greeting(name: String) {
+  Card(
+    backgroundColor = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  ) {
+    CardContent(name)
   }
 }
 
 @Composable
-private fun Greeting(name: String) {
+private fun CardContent(name: String) {
   var expanded by remember { mutableStateOf(false) }
 
-  val extraPadding by animateDpAsState(
-    if (expanded) 48.dp else 0.dp,
-    animationSpec = spring(
-      dampingRatio = Spring.DampingRatioMediumBouncy,
-      stiffness = Spring.StiffnessLow
-    )
-  )
-  Surface(
-    color = MaterialTheme.colors.primary,
-    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  Row(
+    modifier = Modifier
+      .padding(12.dp)
+      .animateContentSize(
+        animationSpec = spring(
+          dampingRatio = Spring.DampingRatioMediumBouncy,
+          stiffness = Spring.StiffnessLow
+        )
+      )
   ) {
-    Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier
+    Column(
+      modifier = Modifier
         .weight(1f)
-        .padding(bottom = extraPadding)
-      ) {
-        Text(text = "Hello, ")
-        Text(text = name, style = MaterialTheme.typography.h4.copy(
+        .padding(12.dp)
+    ) {
+      Text(text = "Hello, ")
+      Text(
+        text = name,
+        style = MaterialTheme.typography.h4.copy(
           fontWeight = FontWeight.ExtraBold
-        ))
+        )
+      )
+      if (expanded) {
+        Text(
+          text = ("Composem ipsum color sit lazy, " +
+              "padding theme elit, sed do bouncy. ").repeat(4),
+        )
       }
-      OutlinedButton(
-        onClick = { expanded = !expanded }
-      ) {
-        Text(if (expanded) "Show less" else "Show more")
-      }
+    }
+    IconButton(onClick = { expanded = !expanded }) {
+      Icon(
+        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+        contentDescription = if (expanded) {
+          stringResource(R.string.show_less)
+        } else {
+          stringResource(R.string.show_more)
+        }
 
+      )
     }
   }
 }
@@ -124,5 +143,13 @@ private fun Greeting(name: String) {
 fun DefaultPreview() {
   BasicsCodelabTheme {
     Greetings()
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+  BasicsCodelabTheme {
+    OnboardingScreen(onContinueClicked = {})
   }
 }
