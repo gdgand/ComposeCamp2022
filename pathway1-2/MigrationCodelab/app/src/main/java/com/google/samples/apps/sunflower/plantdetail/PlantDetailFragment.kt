168c31755eb096579604b93a6646fdf7932af27d
jeongupark <jeongupark@gmail.com>
2022-11-08 18:26:54 +0900

    Jetpack Compose로 이전 - Compose 시작

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,6 +21,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -92,7 +93,12 @@ class PlantDetailFragment : Fragment() {
                     }
                 }
             )
-
+            composeView.setContent {
+                // You're in Compose world!
+                MaterialTheme {
+                    PlantDetailDescription()
+                }
+            }
             toolbar.setNavigationOnClickListener { view ->
                 view.findNavController().navigateUp()
             }
