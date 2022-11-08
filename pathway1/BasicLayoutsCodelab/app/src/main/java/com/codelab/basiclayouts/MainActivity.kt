3abbb04ee450d32d4875b9306d3de11d294f2527
NetLSS <futureprogrammer@naver.com>
2022-11-08 21:50:11 +0900

    Feat: 검색 아이콘 추가, 색상, 플레이스홀더
    검색 아이콘을 추가합니다. TextField에는 다른 컴포저블을 받는 매개변수 leadingIcon이 있습니다. 내부에는 Icon을 설정할 수 있습니다. 여기서는 Search 아이콘을 설정합니다. 올바른 Compose Icon import를 사용하세요.
    텍스트 필드의 배경색을 MaterialTheme의 surface 색으로 설정합니다. TextFieldDefaults.textFieldColors를 사용하여 특정 색상을 재정의할 수 있습니다.
    자리표시자 텍스트 'Search'를 추가합니다(문자열 리소스 R.string.placeholder_search).

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -24,9 +24,12 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
@@ -46,6 +49,18 @@ fun SearchBar(
     TextField(
         value = "",
         onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 56.dp)
