35553c61d4eb550bc17f918896281775627d6e79
Karl Park <karl.panki.park@gmail.com>
2022-11-08 22:20:20 +0800

    chore: fix a wrong version for compose compiler and fix the wrong package path on MainActivity

--- pathway1/BasicsCodelab/app/build.gradle.kts
+++ pathway1/BasicsCodelab/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.1"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
     packagingOptions {
         resources {
