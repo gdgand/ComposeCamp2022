85cfb656753ea767dc61be889911aefc613d7c43
Kangmin <kms0644804@naver.com>
2022-11-09 10:28:30 +0900

    Pathway 1-1: Write your first Compose app (#1)
    * [BasicsCodelab] refactor: gradle, compose 버전 업
    
    * [BasicsCodelab] Jetpack Compose 기초: UI 조정
    
    * [BasicsCodelab] Jetpack Compose 기초: 컴포저블 재사용
    
    * [BasicsCodelab] Jetpack Compose 기초: 열과 행 만들기
    
    * [BasicsCodelab] Jetpack Compose 기초: Compose에서의 상태
    
    * [BasicsCodelab] Jetpack Compose 기초: 상태 호이스팅
    
    * [BasicsCodelab] Jetpack Compose 기초: 성능 지연 목록 만들기
    
    * [BasicsCodelab] Jetpack Compose 기초: 목록에 애니메이션 적용
    
    * [BasicsCodelab] Jetpack Compose 기초: 앱의 스타일 지정 및 테마 설정
    
    * [BasicsCodelab] Jetpack Compose 기초: 설정 완료

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -5,26 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 @Composable
