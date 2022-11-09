e9151efb668e7d5ebb405293f98f6dad2b1ec81c
NetLSS <futureprogrammer@naver.com>
2022-11-09 22:21:52 +0900

    Feat: 워터카운터를 컬럼으로 포함하고, 버튼을 누를 때마다 카운트가 갱신 되도록 한다

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -6,11 +6,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Column
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
