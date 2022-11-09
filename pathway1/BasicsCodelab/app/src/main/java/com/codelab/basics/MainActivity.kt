cca7cc6bd84bbeae50832beca2d1ff62c0522685
Jooheon <jooheon.lee@bugs.co.kr>
2022-11-09 16:19:34 +0900

    Feat: 홈, 메인화면 네비게이션

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -6,39 +6,62 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.codelab.basics.MainActivity.Companion.HOME
+import com.codelab.basics.MainActivity.Companion.MAIN
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // commit test
             BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AppNavHost(modifier = Modifier.fillMaxSize())
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greeting("Android")
+    companion object {
+        const val MAIN = "main"
+        const val HOME = "home"
     }
 }
+
+@Composable
+private fun AppNavHost(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = MAIN
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(MAIN) {
+            MainScreen(
+                onNavigateToHome = {
+                    navController.navigate(HOME)
+                }
+            )
+        }
+        composable(HOME) {
+            HomeScreen(
+                onNavigateToMain = {
+                    navController.navigate(MAIN)
+                }
+            )
+        }
+    }
+}
+
