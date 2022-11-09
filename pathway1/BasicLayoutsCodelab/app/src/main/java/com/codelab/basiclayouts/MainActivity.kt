e4c17d833dfac5e74c3a5fd5e4eb4bc504db77ee
정재환 <pon5976@naver.com>
2022-11-09 10:44:33 +0900

    1-1 BasicLayoutsCodelab) 7.AlignYourBodyRow

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -23,6 +23,8 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -119,6 +121,15 @@ fun FavoriteCollectionCard(
 fun AlignYourBodyRow(
   modifier: Modifier = Modifier
 ) {
+  LazyRow(
+    horizontalArrangement = Arrangement.spacedBy(8.dp),
+    contentPadding = PaddingValues(16.dp),
+    modifier = modifier
+  ) {
+    items(items = alignYourBodyData) { item ->
+      AlignYourBodyElement(drawable = item.drawable, text = item.text)
+    }
+  }
   // Implement composable here
 }
 
