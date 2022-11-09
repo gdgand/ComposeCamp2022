6e3569a53599932c3201676b85a54bba4f529954
NetLSS <futureprogrammer@naver.com>
2022-11-09 22:30:01 +0900

    Feat: rembmer 를 사용하여 상태를 정의

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -12,14 +12,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
+import androidx.compose.runtime.remember
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        // Changes to count are now tracked by Compose
-        // 그러나
-        // 리컴포지션이 발생하면 count 변수가 다시 0으로 초기화되므로! 값이 항상 0 으로 표기된다.
-        val count: MutableState<Int> = mutableStateOf(0)
-
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
         Text("You've had ${count.value} glasses.")
         Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
