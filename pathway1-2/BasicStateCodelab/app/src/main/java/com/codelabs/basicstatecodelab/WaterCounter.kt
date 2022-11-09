699a593bb35a6d5ada9ebba19f3dae162f5b7340
NetLSS <futureprogrammer@naver.com>
2022-11-09 22:32:30 +0900

    Feat: by 델리게이트를 사용하여 상태를 정의, 변수명에 바로 접근하여 값을 수정 가능

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -14,12 +14,16 @@ import androidx.compose.runtime.mutableStateOf
 
 import androidx.compose.runtime.remember
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
-        Text("You've had ${count.value} glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
