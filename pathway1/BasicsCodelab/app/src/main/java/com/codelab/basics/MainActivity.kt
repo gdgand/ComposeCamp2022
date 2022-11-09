43e397d1c9001b47192b084a51fe69ddea1580a3
zinc0214 <zinc0214@gmail.com>
2022-11-10 01:55:23 +0900

    [CodeLab] 9. 성능 지연 목록 만들기

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -45,7 +47,7 @@ class MainActivity : ComponentActivity() {
 private fun MyApp() {
     var shouldShowOnBoarding by remember { mutableStateOf(true) }
 
-    if(shouldShowOnBoarding) {
+    if (shouldShowOnBoarding) {
         OnboardingScreen(onContinueClicked = {
             shouldShowOnBoarding = false
         })
@@ -53,6 +55,7 @@ private fun MyApp() {
         Greetings()
     }
 }
+
 @Composable
 private fun OnboardingScreen(onContinueClicked: () -> Unit) {
     Surface {
@@ -72,9 +75,9 @@ private fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
