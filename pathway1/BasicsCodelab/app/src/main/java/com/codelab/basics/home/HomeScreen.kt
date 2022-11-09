c35fadc1cdead124f3bd7a996f25836faf4d7077
Jooheon <jooheon.lee@bugs.co.kr>
2022-11-09 19:56:09 +0900

    Feat: pathway 1-1 Basic layout

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/home/HomeScreen.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/home/HomeScreen.kt
@@ -4,10 +4,8 @@ import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.home.HomeData
 import com.codelab.basics.home.HomeViewModel
