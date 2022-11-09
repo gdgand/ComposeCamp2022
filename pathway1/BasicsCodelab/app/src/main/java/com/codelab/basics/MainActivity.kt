c770d489aa31b586008ddd9c6759497f08b69cbb
김동식 <dongsik.kim@navercopr.com>
2022-11-09 21:36:58 +0900

    [pathway1/JetpackComposeBasic] 항목 펼치기

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -43,13 +43,18 @@ fun Greeting(name: String) {
     val expanded = remember {
         mutableStateOf(false)
     }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
