815e229d96cc8289d42027b1ab3a4994409a2c82
Jooheon <jooheon.lee@bugs.co.kr>
2022-11-09 10:39:18 +0900

    Test: 커밋 테스트

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // commit test
             BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
