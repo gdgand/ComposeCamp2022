3cfb35b4d678c4a3b08303dba3d77201921d85dd
seunggyu <seunggyu97@gmail.com>
2022-11-09 17:34:01 +0900

    section12 앱의 스타일 지정 및 테마 설정

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,15 +9,16 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.saveable.rememberSaveable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +33,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember{ mutableStateOf(false)}
+    val expanded = remember { mutableStateOf(false) }
 
     val extraPadding by animateDpAsState(
-        if(expanded.value) 48.dp else 0.dp,
+        if (expanded.value) 48.dp else 0.dp,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioMediumBouncy,
             stiffness = Spring.StiffnessLow
@@ -46,16 +48,22 @@ private fun Greeting(name: String) {
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name, style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
             ) {
-                Text(if(expanded.value) "Show less" else "Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
@@ -63,17 +71,17 @@ private fun Greeting(name: String) {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true)}
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-    if(shouldShowOnboarding){
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
-    }else{
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
         Greetings()
     }
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000){"$it"}){
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
             Greeting(name = name)
@@ -81,30 +89,36 @@ private fun Greetings(names: List<String> = List(1000){"$it"}){
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground =  true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit){
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
     // TODO: 아래 구문은 호이스팅 되어야 함
     // var shouldShowOnboarding by remember { mutableStateOf(true) }
 
-    Surface{
+    Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        ) {
             Text("Welcome to the Basics Codelab!")
             Button(
                 modifier = Modifier.padding(vertical = 24.dp),
                 onClick = onContinueClicked
-            ){
+            ) {
                 Text("Continue")
             }
         }
@@ -113,7 +127,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit){
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun OnboardingPreview(){
+fun OnboardingPreview() {
     BasicsCodelabTheme {
         OnboardingScreen(onContinueClicked = {})
     }
