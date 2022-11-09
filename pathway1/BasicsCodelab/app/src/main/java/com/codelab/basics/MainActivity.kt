a3e56b56baaca8ef1545d8c07bfa8fe9cce140a2
KimYonghyeok <dragon@deali.net>
2022-11-09 14:34:32 +0900

    열과 행 만들기

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,13 +3,16 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +34,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(
+        color = MaterialTheme.colors.primary
+    ) {
+        Column(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Text(
+                text = "Hello,"
+            )
+            Text(
+                text = name
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -39,5 +55,15 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     BasicsCodelabTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MyApp() {
+    Surface(
+        color = MaterialTheme.colors.background
+    ) {
+        Greeting(name = "Android")
     }
 }
