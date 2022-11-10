1a860169af3d28a77f3cedbce38b7817434d9140
Jooheon <jooheon.lee@bugs.co.kr>
2022-11-10 14:47:51 +0900

    Feat: Wellness App

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessViewModel.kt
@@ -0,0 +1,23 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel: ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) {
+        _tasks.find { it.id == item.id } ?.let { task ->
+            task.checked.value = checked
+        }
+    }
+
+    private fun getWellnessTasks() = List(30) {
+        WellnessTask(it, "Task # $it")
+    }
+}
