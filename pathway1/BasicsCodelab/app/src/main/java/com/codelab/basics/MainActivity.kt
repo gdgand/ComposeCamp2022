f116a7740d80a1f6aa2b339e296c620a3de7ed38
Hyuck9 <lhg1304@nexmore.co.kr>
2022-11-10 12:45:14 +0900

    chapter 11. 목록에 애니메이션 적용

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,6 +3,9 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -41,7 +44,7 @@ fun MyApp() {
 @Composable
 private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 	LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-		items (items = names) { name ->
+		items(items = names) { name ->
 			Greeting(name = name)
 		}
 	}
@@ -49,8 +52,14 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 
 @Composable
 private fun Greeting(name: String) {
-	val expanded = remember { mutableStateOf(false) }
-	val extraPadding = if (expanded.value) 48.dp else 0.dp
+	var expanded by remember { mutableStateOf(false) }
+	val extraPadding by animateDpAsState(
+		if (expanded) 48.dp else 0.dp,
+		animationSpec = spring(
+			dampingRatio = Spring.DampingRatioMediumBouncy,
+			stiffness = Spring.StiffnessLow
+		)
+	)
 
 	Surface(
 		color = MaterialTheme.colors.primary,
@@ -60,15 +69,15 @@ private fun Greeting(name: String) {
 			Column(
 				modifier = Modifier
 					.weight(1f)
-					.padding(bottom = extraPadding)
+					.padding(bottom = extraPadding.coerceAtLeast(0.dp))
 			) {
 				Text(text = "Hello, ")
 				Text(text = name)
 			}
 			OutlinedButton(
-				onClick = { expanded.value = !expanded.value }
+				onClick = { expanded = !expanded }
 			) {
-				Text(text = if (expanded.value) "Show less" else "Show more")
+				Text(text = if (expanded) "Show less" else "Show more")
 			}
 		}
 	}
