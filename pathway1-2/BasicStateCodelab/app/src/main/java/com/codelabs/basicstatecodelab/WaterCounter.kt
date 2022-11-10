ea99a776a64977ec2fdd0f7537af3b3db0054bb6
yc.park <yc.park@deepfine.ai>
2022-11-11 08:35:15 +0900

    feat($compose): State driven UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -20,8 +20,10 @@ fun WaterCounter(modifier: Modifier = Modifier) {
   Column(modifier = modifier.padding(16.dp)) {
     var count by remember { mutableStateOf(0) }
 
-    Text("You've has ${count} glasses.")
-    Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+    if (count > 0) {
+      Text("You've has ${count} glasses.")
+    }
+    Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
       Text("Add one")
     }
   }
