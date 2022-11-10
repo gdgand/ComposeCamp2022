d247c3af1c892fbddc745a748ab0171332ef2074
imuz <monoto@gmail.com>
2022-11-10 13:27:13 +0900

    codelab1 ing

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,12 +3,10 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,35 +17,105 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                MyApp(names = listOf("first", "second", "third"))
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Surface(color = MaterialTheme.colors.background) {
+fun OnboardingScreen() {
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+
+    androidx.compose.material.Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "안녕하세요! 기본 Codelab 입니다.!!!")
+            Button(
+                onClick = { shouldShowOnboarding = false },
+                modifier = Modifier.padding(vertical = 24.dp)
+            ) {
+                Text(text = "계속")
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen()
+    } else {
+
+        Surface(color = MaterialTheme.colors.background) {
+            Greetings()
+        }
+    }
+
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose", "Hello")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value == true) 48.dp else 0.dp
+
+    Surface(color = MaterialTheme.colors.primary) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello ")
+                Text(text = "$name!!!!!!!!")
+            }
+            OutlinedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(if (expanded.value) "작게보기" else "더보기")
+            }
+        }
+
+    }
+}
+
+//<---------------------PREVIEW---------------->
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 160)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen()
+    }
+}
+
+@Preview(showBackground = false, widthDp = 320)
+@Composable
+fun DefaultPreview(names: List<String> = listOf("World", "Compose")) {
+    BasicsCodelabTheme {
         Column {
+
             for (name in names) {
                 Greeting(name = name)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello$name!!!!!!!! ", modifier = Modifier.padding(36.dp))
-
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greeting("Android")
     }
 }
