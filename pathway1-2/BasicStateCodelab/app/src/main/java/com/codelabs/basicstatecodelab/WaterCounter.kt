aee8dbbb6415ba82e6116c229e33c6f1b2639a6b
yc.park <yc.park@deepfine.ai>
2022-11-11 08:34:04 +0900

    feat($compose): Memory in a composable function

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -18,8 +18,9 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
   Column(modifier = modifier.padding(16.dp)) {
-    var count = 0
-    Text("You've has $count glasses.")
+    var count by remember { mutableStateOf(0) }
+
+    Text("You've has ${count} glasses.")
     Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
       Text("Add one")
     }
