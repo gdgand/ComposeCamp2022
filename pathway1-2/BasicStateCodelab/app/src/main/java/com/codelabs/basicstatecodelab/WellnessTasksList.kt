f9d412043e246cc598dd25f5fe30f158029ea0e6
얍얍 <pigcos1004@gmail.com>
2022-11-10 20:14:56 +0900

    Jetpack Compose의 상태

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,23 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessTasksList(
+    list: List<WellnessTask>,// = remember { getWellnessTasks() }
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier,
+
+) {
+    LazyColumn(modifier = modifier) {
+        items(items = list, key = { task -> task.id }) { task ->
+            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+        }
+    }
+}
+
+//private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "task # $i")}
---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessViewModel.kt
@@ -0,0 +1,16 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel: ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
