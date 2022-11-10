3de1c805cdff787036e3c22ec2d53b1fadb80403
yc.park <yc.park@deepfine.ai>
2022-11-11 08:17:39 +0900

    feat($compose): State in Compose

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,16 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * @Description
+ * @author yc.park (DEEP.FINE)
+ * @since 2022-11-11
+ * @version 1.0.0
+ */
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+  WaterCounter(modifier)
+}
