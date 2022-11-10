0111654fbcdcde2d3ca5ba57ae1551088f8c5ace
Hyuck9 <lhg1304@nexmore.co.kr>
2022-11-10 16:30:51 +0900

    9. 상태 끌어올리기

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -10,14 +10,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
 	Column(modifier = modifier.padding(16.dp)) {
-		var count by rememberSaveable { mutableStateOf(0) }
 		if (count > 0) {
 			Text("You've had $count glasses.")
 		}
-		Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+		Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
 			Text("Add one")
 		}
 	}
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+	var count by rememberSaveable { mutableStateOf(0) }
+	StatelessCounter(count = count, onIncrement = { count++ }, modifier = modifier)
 }
