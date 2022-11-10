1f93a574241f9cf42eb5dff31911dfad1b38448a
Mo ar <cocomoar@gmail.com>
2022-11-10 21:13:23 +0900

    BasicCodeLab 기초 커밋

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -12,10 +12,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
@@ -32,13 +35,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+private fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
-        OnboardingScreen { shouldShowOnboarding = false }
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
         Greetings()
+    }
+}
+
+@Composable
+private fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
     }
 }
 
@@ -59,8 +81,10 @@ fun Greetings(names: List<String> = List(1000) { "$it" }) {
 }
 
 @Composable
-fun Greeting(name: String) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
+private fun Greeting(name: String) {
+
+    var expanded by remember { mutableStateOf(false) }
+
     val extraPadding by animateDpAsState(
         if (expanded) 48.dp else 0.dp,
         animationSpec = spring(
@@ -68,48 +92,28 @@ fun Greeting(name: String) {
             stiffness = Spring.StiffnessLow
         )
     )
-
-
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
-                Text(text = "Hello,")
+                Text(text = "Hello, ")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { expanded = !expanded }) {
+            OutlinedButton(
+                onClick = { expanded = !expanded }
+            ) {
                 Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
 }
 
-@Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-    // TODO: This state should be hoisted
 
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = { onContinueClicked }
-            ) {
-                Text("Continue")
-            }
-        }
-    }
-}
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
