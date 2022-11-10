4eed62edf30af7071e8342bbde228d66956a1701
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-11-11 07:36:54 +0900

    [pathway1 최종본] 코드실습 완료!
    애니메이션 적용

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,11 +3,14 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +35,7 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun MyApp(){
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding){
         OnboardingScreen(onContinueClicked={shouldShowOnboarding = false})
@@ -58,7 +61,13 @@ fun Greetings(names: List<String> = List(1000){"$it"}) {
 @Composable
 fun Greeting(name: String) {
     var expanded by remember{mutableStateOf(false)}
-    val extraPadding=if(expanded) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if(expanded) 48.dp else 0.dp,
+        animationSpec = tween(
+            durationMillis = 2000
+        )
+    )
+
     Surface(color=MaterialTheme.colors.primary, modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)){
         Row(modifier = Modifier .padding(24.dp)) {
             Column(modifier = Modifier
