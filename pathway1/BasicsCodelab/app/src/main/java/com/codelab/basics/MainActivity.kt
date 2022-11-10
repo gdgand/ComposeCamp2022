14801cef999806274693c5fb734f647f8a9f8450
initialz <monoto@gmail.com>
2022-11-11 01:10:04 +0900

    feat: pathway1 complete

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
@@ -24,7 +37,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun OnboardingScreen() {
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
     var shouldShowOnboarding by remember {
         mutableStateOf(true)
     }
@@ -37,7 +50,7 @@ fun OnboardingScreen() {
         ) {
             Text(text = "안녕하세요! 기본 Codelab 입니다.!!!")
             Button(
-                onClick = { shouldShowOnboarding = false },
+                onClick = onContinueClicked,
                 modifier = Modifier.padding(vertical = 24.dp)
             ) {
                 Text(text = "계속")
@@ -49,10 +62,10 @@ fun OnboardingScreen() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
-        OnboardingScreen()
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
 
         Surface(color = MaterialTheme.colors.background) {
@@ -63,50 +76,92 @@ private fun MyApp() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose", "Hello")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+fun Greetings(names: List<String> = List(1000) { "$it"}) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names)  { name ->
             Greeting(name = name)
+
         }
     }
+
 }
 
 
 @Composable
-fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value == true) 48.dp else 0.dp
-
-    Surface(color = MaterialTheme.colors.primary) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello ")
-                Text(text = "$name!!!!!!!!")
-            }
-            OutlinedButton(onClick = {
-                expanded.value = !expanded.value
-            }) {
-                Text(if (expanded.value) "작게보기" else "더보기")
-            }
-        }
-
+private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
     }
 }
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+}
+
 
 //<---------------------PREVIEW---------------->
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 160)
 @Composable
 fun OnboardingPreview() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
     BasicsCodelabTheme {
-        OnboardingScreen()
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     }
 }
-
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = false, widthDp = 320)
 @Composable
 fun DefaultPreview(names: List<String> = listOf("World", "Compose")) {
