93e26edcaf0792ad0a9e85ef897f881008443fb2
yc.park <yc.park@deepfine.ai>
2022-11-11 08:43:23 +0900

    feat($compose): Restore state in Compose

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -19,25 +20,12 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
   Column(modifier = modifier.padding(16.dp)) {
-    var count by remember { mutableStateOf(0) }
+    var count by rememberSaveable { mutableStateOf(0) }
     if (count > 0) {
-      var showTask by remember { mutableStateOf(true) }
-      if (showTask) {
-        WellnessTaskItem(
-          onClose = { showTask = false },
-          taskName = "Have you taken your 15 minute walk today?"
-        )
-      }
-      Text("You've has ${count} glasses.")
+      Text("You've had $count glasses.")
     }
-
-    Row(Modifier.padding(top = 8.dp)) {
-      Button(onClick = { count++ }, enabled = count < 10) {
-        Text("Add one")
-      }
-      Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
-        Text("Clear water count")
-      }
+    Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+      Text("Add one")
     }
   }
 }
