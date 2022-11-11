81b27959ebf7b6ead6953285cf0ec496d9bbd2bf
jisang <jisangman89@gmail.com>
2022-11-11 12:25:04 +0900

    Update : 열과 행 만들기 - material3 dependeny 추가, sdk 버전업

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,11 +37,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(text = "Hello,")
-            Text(text = name)
-            Introduce(name = "Compose")
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+                Introduce(name = "jay_yoo")
+            }
+            ElevatedButton(onClick = {
+
+            }) {
+                Text(text = "Show more", color = MaterialTheme.colors.primary)
+            }
         }
     }
 }
@@ -53,14 +69,19 @@ fun Introduce(name: String) {
 }
 
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting(name = "Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names)
+            Greeting(name = name)
+
     }
 }
 
 
-@Preview(showBackground = true, name = "Text preview")
+@Preview(showBackground = true, widthDp = 320, name = "Text preview")
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
