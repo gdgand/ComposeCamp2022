f1bf97360059593b2be9b3fa2c43b533517605a1
shim seok bo <shadowog@gmail.com>
2022-11-11 23:08:37 +0900

    성능지연목록만들기 - 코드랩과 맞추기 위하여 메터리얼 3 적용

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -6,7 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material3.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,7 +49,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = "Welcome to the Basic Codelab!")
-            Button(
+            ElevatedButton(
                 modifier = Modifier.padding(vertical = 24.dp),
                 onClick = onContinueClicked
             ) {
@@ -85,7 +86,7 @@ fun Greeting(name: String) {
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
-        color = MaterialTheme.colors.primary,
+        color = MaterialTheme.colorScheme.primary,
         modifier = Modifier
             .padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
@@ -100,7 +101,7 @@ fun Greeting(name: String) {
                 Text(text = "Hello")
                 Text(text = name)
             }
-            OutlinedButton(onClick = {
+            ElevatedButton(onClick = {
                 expanded.value = !expanded.value
             } ) {
                 Text(text = if(expanded.value) "Show less" else "Show more")
