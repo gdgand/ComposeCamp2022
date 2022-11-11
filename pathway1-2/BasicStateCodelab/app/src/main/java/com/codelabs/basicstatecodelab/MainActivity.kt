39f17af1d62eb50ce4dd6eb6fa42df062d0d19dd
MalibinYun <nightmare73@naver.com>
2022-11-11 20:51:30 +0900

    [basic state] 7. 컴포지션의 Remember & 8. Compose에서 상태 복원

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,17 +35,61 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
 
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                        taskName = "Have you taken your 15 minute walk today?",
+                        onClose = { showTask = false },
+                )
+            }
             Text(text = "You've had $count glassed.")
         }
-        Button(
-                onClick = { count++ },
+        Row(
                 modifier = Modifier.padding(top = 8.dp),
-                enabled = count < 10,
         ) {
-            Text("Add one")
+
+            Button(
+                    onClick = { count++ },
+                    enabled = count < 10,
+            ) {
+                Text("Add one")
+            }
+            Button(
+                    onClick = { count = 0 },
+                    Modifier.padding(start = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
+        }
+    }
+}
+
+@Composable
+fun WellnessTaskItem(
+        taskName: String,
+        onClose: () -> Unit,
+        modifier: Modifier = Modifier,
+) {
+    Row(
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+                modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 16.dp),
+                text = taskName,
+        )
+        IconButton(
+                onClick = onClose,
+        ) {
+            Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "Close",
+            )
         }
     }
 }
