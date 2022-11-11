51d7e18a96668caa88959d44ebda21aa4540f3e4
MalibinYun <nightmare73@naver.com>
2022-11-11 16:26:04 +0900

    [basic layout] 8. 즐겨찾는 컬렉션 그리드 - Lazy 그리드

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -154,7 +155,21 @@ fun AlignYourBodyRow(
 fun FavoriteCollectionsGrid(
         modifier: Modifier = Modifier,
 ) {
-    // Implement composable here
+    LazyHorizontalGrid(
+            rows = GridCells.Fixed(2),
+            modifier = modifier.height(120.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(
+                    imageResId = item.drawable,
+                    titleResId = item.text,
+                    modifier = Modifier.height(56.dp),
+            )
+        }
+    }
 }
 
 // Step: Home section - Slot APIs
