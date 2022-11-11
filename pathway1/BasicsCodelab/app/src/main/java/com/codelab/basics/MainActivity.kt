7a9d501f66f5b0403d40065e78bb90212b0ee371
shim seok bo <shadowog@gmail.com>
2022-11-11 22:57:02 +0900

    성능지연목록만들기

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -58,9 +60,10 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-fun Greetings(names : List<String> = listOf("World","Compose")){
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+fun Greetings(names : List<String> = List(100) {"$it"}){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+
+        items(items = names){ name ->
             Greeting(name = name)
         }
     }
