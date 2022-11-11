f41b65efc4feca94375d2a5fa86eba9a3140e8e4
yeongjin <dudwls901@naver.com>
2022-11-11 11:16:06 +0900

    [Refactor] State Hoisting (Stateless, Stateful)

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -13,21 +13,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+){
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-        if (count > 0) {
-            Text(
-                text = "You`ve had ${count} glasses.",
-            )
+        if(count>0){
+            Text("You've had $count glasses.")
         }
         Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
-            enabled = count < 10,
-        ) {
-            Text(text = "Add one")
+            onClick = onIncrement,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ){
+            Text("Add one")
         }
 
     }
+}
+
+@Composable
+fun StateFulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    StatelessCounter(
+        count = count,
+        onIncrement = { count++ },
+        modifier = modifier
+    )
 }
