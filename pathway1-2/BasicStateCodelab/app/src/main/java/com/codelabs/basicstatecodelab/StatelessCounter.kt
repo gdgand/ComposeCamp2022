290c6efed4d9e2b8ddcabd08e0633bbda1f3e717
x1210x <x1210x@gmail.com>
2022-11-11 11:09:29 +0900

    Compose에서의 상태

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/StatelessCounter.kt
@@ -0,0 +1,36 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StatefulCounter() {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ })
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = "You've has $count glasses.")
+        }
+        Button(
+            onClick = onIncrement,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text("Add one")
+        }
+    }
+}
---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,26 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Preview
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTaskList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task -> wellnessViewModel.remove(task) }
+        )
+    }
+}
