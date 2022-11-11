a52c30e63f1d34f864ad250dea46bdbcf906deb0
MalibinYun <nightmare73@naver.com>
2022-11-11 21:13:07 +0900

    [basic state] 9. 상태 호이스팅 & 10. 목록 사용

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -32,6 +34,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
@@ -43,6 +46,8 @@ fun WaterCounter(modifier: Modifier = Modifier) {
                 WellnessTaskItem(
                         taskName = "Have you taken your 15 minute walk today?",
                         onClose = { showTask = false },
+                        checked = false,
+                        onCheckedChange = {},
                 )
             }
             Text(text = "You've had $count glassed.")
@@ -67,9 +72,48 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     }
 }
 
+data class WellnessTask(
+        val id: Int,
+        val label: String,
+)
+
+private fun getWellnessTasks() = List(30) { WellnessTask(it, "Task #$it") }
+
+@Composable
+fun WellnessTasksList(
+        modifier: Modifier = Modifier,
+        list: List<WellnessTask> = remember { getWellnessTasks() },
+) {
+    LazyColumn(
+            modifier = modifier,
+    ) {
+        items(list) {
+            WellnessTaskItem(taskName = it.label)
+        }
+    }
+}
+
 @Composable
 fun WellnessTaskItem(
         taskName: String,
+        modifier: Modifier = Modifier,
+) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+            taskName = taskName,
+            checked = checkedState,
+            onCheckedChange = { checkedState = it },
+            onClose = { },
+            modifier = modifier,
+    )
+}
+
+@Composable
+fun WellnessTaskItem(
+        taskName: String,
+        checked: Boolean,
+        onCheckedChange: (Boolean) -> Unit,
         onClose: () -> Unit,
         modifier: Modifier = Modifier,
 ) {
@@ -83,6 +127,10 @@ fun WellnessTaskItem(
                         .padding(start = 16.dp),
                 text = taskName,
         )
+        Checkbox(
+                checked = checked,
+                onCheckedChange = onCheckedChange
+        )
         IconButton(
                 onClick = onClose,
         ) {
@@ -94,9 +142,52 @@ fun WellnessTaskItem(
     }
 }
 
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var waterCount by rememberSaveable { mutableStateOf(0) }
+//    var juiceCount by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(
+            count = waterCount,
+            onIncrement = { waterCount++ },
+            modifier = modifier,
+    )
+
+//    StatelessCounter(
+//            count = juiceCount,
+//            onIncrement = { juiceCount++ },
+//            modifier = modifier,
+//    )
+}
+
+@Composable
+fun StatelessCounter(
+        count: Int,
+        onIncrement: () -> Unit,
+        modifier: Modifier = Modifier,
+) {
+    Column(
+            modifier = modifier.padding(16.dp)
+    ) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(
+                modifier = Modifier.padding(top = 8.dp),
+                onClick = onIncrement,
+                enabled = count < 10
+        ) {
+            Text("Add one")
+        }
+    }
+}
+
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter(modifier)
+        WellnessTasksList(modifier)
+    }
 }
 
 @Preview(showBackground = true)
