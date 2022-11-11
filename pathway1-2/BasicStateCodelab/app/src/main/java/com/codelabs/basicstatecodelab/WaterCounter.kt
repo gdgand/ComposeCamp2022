940d3f355d1b3b32ef66701949ddd052b2701812
NetLSS <futureprogrammer@naver.com>
2022-11-11 23:20:23 +0900

    Feat: 카운트가 1이상이면, TaskItem 을 표시

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -18,16 +18,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
+fun WaterCounter() {
+    Column(modifier = Modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            // This text is present if the button has been clicked
-            // at least once; absent otherwise
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+
+        Button(onClick = { count++ }, enabled = count < 10) {
             Text("Add one")
         }
     }
