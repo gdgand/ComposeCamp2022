8ca178f4a648f7c8695a2b3c7415c3415e9529ea
yc.park <yc.park@deepfine.ai>
2022-11-11 09:11:38 +0900

    feat($compose): State in ViewModel

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
@@ -1,9 +1,19 @@
 package com.codelabs.basicstatecodelab
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
 /**
  * @Description
  * @author yc.park (DEEP.FINE)
  * @since 2022-11-11
  * @version 1.0.0
  */
-data class WellnessTask(val id: Int, val label: String)
+class WellnessTask(
+  val id: Int,
+  val label: String,
+  initialChecked: Boolean = false
+) {
+  var checked by mutableStateOf(initialChecked)
+}
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 @Composable
 fun WellnessTasksList(
   list: List<WellnessTask>,
+  onCheckedTask: (WellnessTask, Boolean) -> Unit,
   onCloseTask: (WellnessTask) -> Unit,
   modifier: Modifier = Modifier,
 ) {
@@ -24,7 +25,12 @@ fun WellnessTasksList(
       items = list,
       key = { task -> task.id }
     ) { task ->
-      WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+      WellnessTaskItem(
+        taskName = task.label,
+        checked = task.checked,
+        onCheckedChange = { checked -> onCheckedTask(task, checked) },
+        onClose = { onCloseTask(task) }
+      )
     }
   }
 }
