1325d80e6c2e500ecbf2e5d28bac97c7713a23f3
shim seok bo <shadowog@gmail.com>
2022-11-12 22:41:43 +0900

    State in Jetpack Compose - 5. Memory in a composable function

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -4,14 +4,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier : Modifier = Modifier){
     Column(modifier = Modifier.padding(16.dp)) {
-        var count = 0
+
+        var count by remember{ mutableStateOf(0) }
+
         Text(text = "you've had $count glasses")
         Button(onClick = { count++ },Modifier.padding(top = 8.dp)) {
             Text(text = "Add one")
