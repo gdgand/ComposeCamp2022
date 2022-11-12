8eba0fe52fcc6064dfeb70140de57e8942a50ff4
seojin <imy123123@naver.com>
2022-11-12 22:08:45 +0900

    [ (Basic layouts in Compose) step 11]
     SootheBottomNavigation 작성완료

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -34,7 +34,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -160,12 +162,30 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
 // Step: Bottom navigation - Material
 @Composable
-private fun SootheBottomNavigation(modifier: Modifier = Modifier) { // Implement composable here
+private fun SootheBottomNavigation(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        BottomNavigationItem(icon = { Icon(imageVector = Icons.Default.Spa, contentDescription = null) },
+            label = { Text(stringResource(id = R.string.bottom_navigation_home)) },
+            selected = true,
+            onClick = { /*TODO*/ }
+        )
+        BottomNavigationItem(icon = { Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null) },
+            label = { Text(stringResource(id = R.string.bottom_navigation_profile)) },
+            selected = false,
+            onClick = { /*TODO*/ }
+        )
+    }
 }
 
 // Step: MySoothe App - Scaffold
 @Composable
-fun MySootheApp() { // Implement composable here
+fun MySootheApp() {
+
 }
 
 private val alignYourBodyData = listOf(R.drawable.ab1_inversions to R.string.ab1_inversions,
