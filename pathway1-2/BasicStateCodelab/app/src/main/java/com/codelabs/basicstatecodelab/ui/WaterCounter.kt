9277b1f8a33257ff605d4dc1ea44423e7d5b3929
jaeryo2357 <jaeryo2357@naver.com>
2022-11-12 20:14:09 +0900

    implement Memory in a composable function

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WaterCounter.kt
@@ -5,15 +5,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        val count: MutableState<Int> = mutableStateOf(0)
         Text(text = "You've had $count glasses.")
-        Button(onClick = { count++ },  Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count.value++ },  Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
