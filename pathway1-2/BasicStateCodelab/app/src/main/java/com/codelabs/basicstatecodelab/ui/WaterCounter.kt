0652ffbb11c77c2eb26eee26c7ddca8d28d1828d
jaeryo2357 <jaeryo2357@naver.com>
2022-11-12 20:27:20 +0900

    implement Remember in Composition

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.codelabs.basicstatecodelab.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -19,10 +20,22 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         var count by remember { mutableStateOf(0) }
 
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ },  Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Add one")
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add One")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
