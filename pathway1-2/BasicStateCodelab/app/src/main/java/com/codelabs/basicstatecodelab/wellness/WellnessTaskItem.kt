bf47d8a1e284453881e1fe6949ca240b94a60d3c
wotosts <tosts522@gmail.com>
2022-11-12 22:02:18 +0900

    pathway1-2 BasicStateCodelab 9~13

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/wellness/WellnessTaskItem.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/wellness/WellnessTaskItem.kt
@@ -2,6 +2,7 @@ package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -15,6 +16,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -22,8 +25,14 @@ fun WellnessTaskItem(
         modifier = modifier, verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
             text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
