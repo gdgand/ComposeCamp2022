a4d291c40a09c6e008e4ea9df10779971e681333
yoontaeseong <tsyun@bbros.kr>
2022-11-12 18:53:38 +0900

    [ Feature ] : 컴포저블 Column 사용

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,13 +3,16 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 /***
@@ -30,7 +33,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.background) {
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello!")
+            Text(text = "$name!")
+        }
+    }
 }
 
 
@@ -39,7 +47,7 @@ fun Greeting(name: String) {
 private fun MyApp(modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier,
-        color = MaterialTheme.colors.background
+
     ) {
         Greeting("Android")
     }
