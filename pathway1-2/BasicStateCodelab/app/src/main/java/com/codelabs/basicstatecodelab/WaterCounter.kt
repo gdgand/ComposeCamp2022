5d65d269a8e8ce8e3af75b6f09a7705fa5b54b25
seunggyu <seunggyu97@gmail.com>
2022-11-12 22:23:07 +0900

    section 8 - rememberSaveable

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,45 +1,33 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        // remember은 리컴포지션 간에만 상태를 유지하는데 도움이 되지만
+        // remeberSaveable은 화면회전, 다크모드 전환 등 다른 구성 변경에도 도움이 된다.
+        var count by rememberSaveable { mutableStateOf(0) }
 
         if (count > 0) {
-            var showTask by remember {
-                mutableStateOf(true)
-            }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false }
-                )
-            }
             Text(text = "You've had $count glasses.")
         }
 
-        Row(Modifier.padding(top = 8.dp)) {
-            /**
-             * 버튼 설정
-             * @param enabled 활성화 조건
-             * enabled = count < 10 은
-             * count가 10 미만일 때만 버튼을 활성화한다는 의미
-             */
-            Button(onClick = { count++ }, enabled = count < 10) {
-                Text("Add one")
-            }
-            Button(onClick = { count = 0}, Modifier.padding(start = 8.dp)){
-                Text("Clear water count")
-            }
+        /**
+         * 버튼 설정
+         * @param enabled 활성화 조건
+         * enabled = count < 10 은
+         * count가 10 미만일 때만 버튼을 활성화한다는 의미
+         */
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
