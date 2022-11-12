a0ca1becd561ace44eedeac8067caf616443c8dc
Durk-jae Yun <durkjae.yun@directionsoft.com>
2022-11-12 20:19:45 +0900

    7. 컴포지션의 Remember - 최종

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,7 +1,6 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -16,32 +15,17 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             mutableStateOf(0)
         }
         if (count > 0) {
-            var showTask by remember {
-                mutableStateOf(true)
-            }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false })
-            }
             Text(
                 text = "You've had $count glasses."
             )
         }
 
-        Row(modifier = Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10
-            ) {
-                Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp),
-            ) {
-                Text("Clear water count")
-            }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text("Add one")
         }
     }
 }
