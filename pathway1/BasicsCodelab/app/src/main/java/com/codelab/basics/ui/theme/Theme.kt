b4da0aa3a78757fb7c12519500e9fce3e3d6ed87
manjee <manjee.official@gmail.com>
2022-11-12 23:50:57 +0900

    UPDATE: 앱의 스타일 지정 및 테마 설정

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColorScheme(
     primary = Purple200,
@@ -13,9 +14,10 @@ private val DarkColorPalette = darkColorScheme(
 )
 
 private val LightColorPalette = lightColorScheme(
-    primary = Purple500,
-    primaryContainer = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color.White,
