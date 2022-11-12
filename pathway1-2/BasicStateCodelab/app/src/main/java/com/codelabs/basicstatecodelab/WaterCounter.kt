aece3409cf5b85d1b274f26cf8f28e6f1b474247
want8607 <gotjd8607@gmail.com>
2022-11-12 23:11:05 +0900

    [Pathway 1-2] compose state완료

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,20 +1,36 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.codelabs.basicstatecodelab.ui.theme.WellnessTaskItem
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
+    StatefulCounter(modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-        Text(text = "You've had ${count} glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text("Add one")
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text(text = "Add one")
         }
     }
+}
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var waterCount by remember { mutableStateOf(0) }
+    var juiceCount by remember { mutableStateOf(0) }
+    StatelessCounter(count = waterCount, onIncrement = { waterCount++ }, modifier = modifier)
+    StatelessCounter(count = juiceCount, onIncrement = { juiceCount++ }, modifier = modifier)
 }
---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,32 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.codelabs.basicstatecodelab.ui.theme.WellnessTaskItem
+
+
+@Composable
+fun WellnessTaskList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit
+) {
+    LazyColumn(modifier = modifier) {
+        items(
+            items = list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { onCheckedTask(task,task.checked)},
+                onClose = { onCloseTask(task) },
+            )
+        }
+    }
+}
