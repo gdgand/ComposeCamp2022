7e419f1680bd2a7cc4c057d44874f91ebf74c945
seunggyu <seunggyu97@gmail.com>
2022-11-13 00:34:18 +0900

    section 12 - ViewModel

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
@@ -1,3 +1,14 @@
 package com.codelabs.basicstatecodelab
 
-data class WellnessTask(val id: Int, val label: String)
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+){
+    var checked by mutableStateOf(initialChecked)
+}
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -11,7 +11,8 @@ private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
 @Composable
 fun WellnessTasksList(
     modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() },
+    list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit
 ) {
     LazyColumn(modifier = modifier){
@@ -19,7 +20,12 @@ fun WellnessTasksList(
             items = list,
             key = { task -> task.id}
         ){task ->
-            WellnessTaskItem(taskName =  task.label, onClose = { onCloseTask(task)})
+            WellnessTaskItem(
+                taskName =  task.label,
+                onClose = { onCloseTask(task)},
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked)}
+            )
         }
     }
 }
