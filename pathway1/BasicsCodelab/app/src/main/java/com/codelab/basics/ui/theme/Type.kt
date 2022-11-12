a40c53817fa3c58974b47753e4a55e2ec9bb62c5
juntae kim <endorphin0710@gmail.com>
2022-11-13 03:49:43 +0900

    jetpack compose basics

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Type.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Type.kt
@@ -1,14 +1,13 @@
 package com.codelab.basics.ui.theme
 
-import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
+val Typography = androidx.compose.material3.Typography(
+    bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
