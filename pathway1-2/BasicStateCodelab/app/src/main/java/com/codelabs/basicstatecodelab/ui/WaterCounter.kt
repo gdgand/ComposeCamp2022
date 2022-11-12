9449bcb4cf0343ecbc860e7af74a6e29cd0cd5f1
jaeryo2357 <jaeryo2357@naver.com>
2022-11-12 15:23:55 +0900

    implement State in Compose

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WaterCounter.kt
@@ -0,0 +1,16 @@
+package com.codelabs.basicstatecodelab.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
+    )
+}
