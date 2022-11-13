aee20c1606c0cb2acfafe2bea1fb39f5db5978dc
leesh96 <suho2718@gmail.com>
2022-11-13 16:44:04 +0900

    [Pathway-1] Codelab 4. Tweaking the UI

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,33 +1,29 @@
 package com.codelab.basics
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.codelab.basics.data.Message
-import com.codelab.basics.data.SampleData
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
+/**
+ * 컴포즈를 사용해도 Activity가 앱의 진입점
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
+        기존에 View 방식에서 setContentView(XML Layout file) 호출과 다르게,
+        setContent 블럭에서 컴포저블을 호출
+         */
         setContent {
             BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
@@ -35,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard(Message("Android", "Jetpack Compose!"))
+                    Greeting("Android")
                 }
             }
         }
@@ -43,89 +39,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MessageCard(message: Message) {
-    Row {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(
-                    width = 1.5.dp,
-                    color = MaterialTheme.colors.secondary,
-                    shape = CircleShape,
-                ),
-            contentDescription = "profile image",
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        var isExpanded by remember { mutableStateOf(false) }
-
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
-        )
-
-        Column(
-            modifier = Modifier.clickable { isExpanded = !isExpanded }
-        ) {
-            Text(
-                text = message.user,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2,
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                elevation = 1.dp,
-                color = surfaceColor,
-                modifier = Modifier.animateContentSize().padding(1.dp),
-            ) {
-                Text(
-                    text = message.conversation,
-                    modifier = Modifier.padding(all = 4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.body2,
-                )
-            }
-        }
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
     }
 }
 
-@Preview(
-    name = "Light Mode",
-    showBackground = true,
-)
-@Preview(
-    name = "Dark Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-)
+@Preview(showBackground = true)
 @Composable
-fun MessageCardPreview() {
+fun DefaultPreview() {
     BasicsCodelabTheme {
-        Surface {
-            MessageCard(Message("Android", "Jetpack Compose!"))
-        }
-    }
-}
-
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        messages.map { item { MessageCard(message = it) } }
-    }
-}
-
-@Preview(
-    name = "Light Mode",
-    showBackground = true,
-)
-@Composable
-fun ConversationPreview() {
-    MaterialTheme {
-        Conversation(messages = SampleData.conversationSample)
+        Greeting("Android")
     }
 }
