e3ea87385f0b00356549ac5b99fc70f29ad22e71
eunie9498 <bvegemilb@naver.com>
2022-11-13 22:54:35 +0900

    :zap: rememberSaveable

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -6,34 +6,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false })
-            }
             Text(
                 text = "You've had ${count} glasses"
             )
         }
-        Row(Modifier.padding(top=10.dp)) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10
-            ) {
-                Text("Add one")
-            }
-            Button(onClick = { count = 0 }, Modifier.padding(start = 10.dp)) {
-                Text("Clear Count")
-            }
+
+        Button(
+            onClick = { count++ }, Modifier.padding(top = 10.dp),
+            enabled = count < 10
+        ) {
+            Text("Add one")
         }
+
     }
 }
