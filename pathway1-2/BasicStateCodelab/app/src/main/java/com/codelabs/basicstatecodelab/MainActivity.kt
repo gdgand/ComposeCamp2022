a00046829a696b3925245acccd96223335b3d021
blucky8649 <blucky4511@gmail.com>
2022-11-13 19:14:49 +0900

    [BasicStateCodelab]상태 기반 UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -39,10 +39,13 @@ class MainActivity : ComponentActivity() {
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text(text = "You've had $count glasses.",)
+        if (count > 0) {
+            Text(text = "You've had $count glasses.",)
+        }
         Button(
             onClick = { count++ },
-            Modifier.padding(top = 8.dp)
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
             Text(text = "Add one")
         }
