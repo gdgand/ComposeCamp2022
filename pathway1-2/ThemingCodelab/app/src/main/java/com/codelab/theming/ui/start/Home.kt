3632bc4ae0b837b2f1c9f796ba66e8101f5191d7
yc.park <yc.park@deepfine.ai>
2022-11-14 08:51:30 +0900

    feat($compose): Working with Shapes

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -35,6 +35,7 @@ import androidx.compose.runtime.CompositionLocal
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -203,6 +204,7 @@ fun PostItem(
     icon = {
       Image(
         painter = painterResource(post.imageThumbId),
+        modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
         contentDescription = null
       )
     },
