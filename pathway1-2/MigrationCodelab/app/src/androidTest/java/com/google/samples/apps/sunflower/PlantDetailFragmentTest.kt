dbb4267380d5baa60982a44342d7c3ae549eecbf
Gyuil-Hwang <hwanggyuil@gmail.com>
2022-11-13 21:06:08 +0900

    Pathway 1-추가 Compose Migration 진행

--- pathway1-2/MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
+++ pathway1-2/MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
@@ -20,6 +20,9 @@ import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.Navigation.findNavController
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -50,27 +53,42 @@ class PlantDetailFragmentTest {
 
     @Rule
     @JvmField
-    val activityTestRule = ActivityScenarioRule(GardenActivity::class.java)
+    val composeTestRule = createAndroidComposeRule<GardenActivity>()
 
     // Note that keeping these references is only safe if the activity is not recreated.
     private lateinit var activity: ComponentActivity
+
+//    @Before
+//    fun jumpToPlantDetailFragment() {
+//        populateDatabase()
+//
+//        activityTestRule.scenario.onActivity { gardenActivity ->
+//            activity = gardenActivity
+//
+//            val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
+//            findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
+//        }
+//    }
 
     @Before
     fun jumpToPlantDetailFragment() {
         populateDatabase()
 
-        activityTestRule.scenario.onActivity { gardenActivity ->
+        composeTestRule.activityRule.scenario.onActivity { gardenActivity ->
             activity = gardenActivity
 
-            val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
-            findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
+            val bundle = Bundle().apply {
+                putString("plantId", "malus-pumila")
+            }
+            findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment)
         }
     }
 
     @Test
     fun testPlantName() {
-        onView(ViewMatchers.withText("Apple"))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+//        onView(ViewMatchers.withText("Apple"))
+//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
     }
 
     @Test
