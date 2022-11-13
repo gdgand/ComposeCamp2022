aecc5943734447204219f91fa9f5e28b77812905
cwpark <cwpark@bbros.kr>
2022-11-13 19:00:38 +0900

    열과 행 만들기
     - Row를 사용하여 Button 추가
     - 본인은 Box를 사용하는 방식으로 구현을 했음

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,16 +3,17 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
+import com.codelab.basics.ui.theme.Shapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,30 +28,64 @@ class MainActivity : ComponentActivity() {
 
 //컴포저블 재사용
 @Composable
-fun MyApp(modifier: Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colors.background
-    ) {
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Surface(
-        color = MaterialTheme.colors.primary
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Text(text = "Hello $name!",
-            modifier = Modifier.padding(24.dp)
-        )
+        //내가 짠 코드
+//        Box(modifier = Modifier.fillMaxWidth()) {
+//            Column(modifier = Modifier
+//                .padding(24.dp)
+//            ) {
+//                Text(text = "Hello,")
+//                Text(text = name)
+//            }
+//
+//            OutlinedButton(
+//                modifier = Modifier
+//                .align(Alignment.CenterEnd)
+//                .padding(16.dp),
+//                shape = RoundedCornerShape(20.dp),
+//                onClick = { /*TODO*/ }
+//            ) {
+//                Text(text = "Show more")
+//            }
+//        }
+        //코드랩 솔루션 코드
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+
+            //ElevatedButton은 material3
+            OutlinedButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp(Modifier.fillMaxSize())
+        MyApp()
     }
 }
