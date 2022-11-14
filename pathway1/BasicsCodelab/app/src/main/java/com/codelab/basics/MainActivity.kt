3f75add48a2fcd77b3701a09a0bcd99194901900
Yebon Kim <v954242@gmail.com>
2022-11-15 00:16:13 +0900

    [BasicCodelab] material to material3, did until 6 step

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,41 +3,67 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      BasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
+      }
     }
+  }
+}
+
+@Composable
+fun MyApp(
+  modifier: Modifier = Modifier,
+  names: List<String> = listOf("World", "Compose")
+) {
+  Column(modifier = modifier.padding(vertical = 4.dp)) {
+    for (name in names) {
+      Greeting(name = name)
+    }
+  }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+  Surface(
+    color = MaterialTheme.colorScheme.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  ) {
+    Row(modifier = Modifier.padding(24.dp)) {
+      Column(modifier = Modifier.weight(1f)) {
+        Text(text = "Hello,")
+        Text(text = name)
+      }
+      ElevatedButton(
+        onClick = { /* TODO */ }
+      ) {
+        Text("Show more")
+      }
+    }
+  }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greeting("Android")
-    }
+  BasicsCodelabTheme {
+    MyApp()
+  }
 }
