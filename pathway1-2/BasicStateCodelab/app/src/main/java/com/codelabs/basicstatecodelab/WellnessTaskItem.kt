f8fa644ec420a680e9e75d475a4baa3c188f3b08
zinc0214 <zinc0214@gmail.com>
2022-11-15 00:37:18 +0900

    7. 컴포지션의 Remember

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
@@ -0,0 +1,32 @@
+package com.codelabs.basicstatecodelab
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = taskName, modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+        IconButton(onClick = { onClose() }) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
