ed8ba4bc15c7b686b7acb420249b57b77ed911e4
rosf73 <alswlzhsclq@naver.com>
2022-11-14 16:15:47 +0900

    Align Your Body Element 컴포넌트 수정 - 재사용 가능하도록 Resources 를 파라미터에 정의

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -71,6 +71,8 @@ fun SearchBar(
 // Step: Align your body - Alignment
 @Composable
 fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -78,13 +80,13 @@ fun AlignYourBodyElement(
         modifier = modifier
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ab1_inversions),
+            painter = painterResource(id = drawable),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier.size(88.dp).clip(CircleShape)
         )
         Text(
-            stringResource(id = R.string.ab1_inversions),
+            stringResource(id = text),
             style = MaterialTheme.typography.h3,
             modifier = Modifier
                 .paddingFromBaseline(top = 24.dp, bottom = 8.dp)
@@ -176,6 +178,8 @@ fun SearchBarPreview() {
 fun AlignYourBodyElementPreview() {
     MySootheTheme {
         AlignYourBodyElement(
+            drawable = R.drawable.ab1_inversions,
+            text = R.string.ab1_inversions,
             modifier = Modifier.padding(8.dp)
         )
     }
