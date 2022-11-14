48d99e08d7efded77eb6045271eac485fb578386
middlefgr <2giwon@gmail.com>
2022-11-14 20:30:42 +0900

    [pathway 1-2] 7. 컴포지션의 Remember

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -48,17 +48,11 @@ private fun WaterCounter(
 ) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            Text(text = "You've had $count glasses.")
+            Text("You've had $count glasses.")
         }
-
-        Button(
-            onClick = { count++ },
-            modifier = modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
-            Text(text = "Add one")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 
