3dbdb69043a9c5fe495934fccfce3e0b21085ec3
JiSeokYeom <ygs01033@naver.com>
2022-11-14 21:50:06 +0900

    pathway1: 열과 행 만들기 (material3 임포트)

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,11 +3,14 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,26 +30,40 @@ class MainActivity : ComponentActivity() {
 
 // 함수는 기본적으로 빈 수정자가 할당되는 수정자 매개변수를 포함하는 것이 좋다
 @Composable
-private fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colors.background
-    ) {
-        Greeting("Android")
+private fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) { // for를 사용하여 Column에 요소 추가
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 private fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) { // Surface로 text의 백그라운드 색상 전환
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp) //Modifier로 패딩 추가
-        )
+    Surface(
+        color = MaterialTheme.colorScheme.primary, // Surface로 text의 백그라운드 색상 전환
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            //Column을 이용하여 세로 배치
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            //버튼 생성
+           ElevatedButton(
+               onClick = { /*TODO*/ }
+           ) {
+               Text("Show more")
+           }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320) //소형 스마트폰의 일반적인 너비인 320으로 테스트
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
