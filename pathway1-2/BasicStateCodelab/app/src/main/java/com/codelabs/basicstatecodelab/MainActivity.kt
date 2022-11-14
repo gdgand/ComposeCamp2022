21a5300495e0268ba57171cc7ea450dbedf58067
middlefgr <2giwon@gmail.com>
2022-11-14 18:41:21 +0900

    [pathway 1-2] 4. Compose의 이벤트

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -3,8 +3,10 @@ package com.codelabs.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -33,22 +35,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    WaterCounter(modifier) {
+
+    }
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    val count: Int = 0
-    Text(
-        text = "You've had $count glasses.",
+private fun WaterCounter(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Column(
         modifier = modifier.padding(16.dp)
-    )
+    ) {
+        var count = 0
+        Text(text = "You've had $count glasses.")
+        Button(
+            onClick = onClick,
+            modifier = modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicStateCodelabTheme {
-        WaterCounter()
+        WellnessScreen()
     }
 }
