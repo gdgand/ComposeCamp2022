d9d5ca9f48647f0fed2f0ff6b9cab8362fe6f786
NetLSS <futureprogrammer@naver.com>
2022-11-15 21:27:24 +0900

    Feat: rememberSaveable 로 mutableStateList 를 저장할 경우에는 error

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -3,6 +3,7 @@ package com.codelabs.basicstatecodelab
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 
@@ -11,7 +12,7 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         StatefulCounter()
 
-        val list = remember { getWellnessTasks().toMutableStateList() }
+        val list = rememberSaveable { getWellnessTasks().toMutableStateList() }
         WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
