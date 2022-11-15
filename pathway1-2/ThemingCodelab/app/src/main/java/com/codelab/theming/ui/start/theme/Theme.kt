d1039cbd087246c5199d763efa609d1484acab5d
NetLSS <futureprogrammer@naver.com>
2022-11-15 22:19:43 +0900

    Feat: use JetnewsTypography

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -2,17 +2,21 @@ package com.codelab.theming.ui.start
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Typography
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.codelab.theming.ui.start.theme.Red700
-import com.codelab.theming.ui.start.theme.Red800
-import com.codelab.theming.ui.start.theme.Red900
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.codelab.theming.ui.start.theme.*
 
 @Composable
 fun JetnewsTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colors = LightColors,
+        typography = JetnewsTypography,
         content = content
     )
 }
@@ -25,4 +29,56 @@ private val LightColors = lightColors(
     secondaryVariant = Red900,
     onSecondary = Color.White,
     error = Red800
+)
+
+val JetnewsTypography = Typography(
+    h4 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.W600,
+        fontSize = 30.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.W600,
+        fontSize = 16.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Domine,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = Montserrat,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp
+    )
 )
--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Typography.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Typography.kt
@@ -5,13 +5,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.codelab.theming.R
 
-private val Montserrat = FontFamily(
+val Montserrat = FontFamily(
     Font(R.font.montserrat_regular),
     Font(R.font.montserrat_medium, FontWeight.W500),
     Font(R.font.montserrat_semibold, FontWeight.W600)
 )
 
-private val Domine = FontFamily(
+val Domine = FontFamily(
     Font(R.font.domine_regular),
     Font(R.font.domine_bold, FontWeight.Bold)
 )
