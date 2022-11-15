ad94fe3ae56453fdbea7fa0a0ee17e471e5b5310
NetLSS <futureprogrammer@naver.com>
2022-11-15 22:15:58 +0900

    Feat: use colors in theme

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -1,9 +1,28 @@
 package com.codelab.theming.ui.start
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.codelab.theming.ui.start.theme.Red700
+import com.codelab.theming.ui.start.theme.Red800
+import com.codelab.theming.ui.start.theme.Red900
 
 @Composable
 fun JetnewsTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
+    MaterialTheme(
+        colors = LightColors,
+        content = content
+    )
 }
+
+private val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
