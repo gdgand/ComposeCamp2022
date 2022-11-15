07bc51d3fecefe1e4431642f42e1e19f7b1d2453
NetLSS <futureprogrammer@naver.com>
2022-11-15 21:47:29 +0900

    Refactor: viewModel 로 부터 체크 상태 변환 콜백을 제공 받음

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -11,15 +11,22 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 @Composable
 fun WellnessScreen(
     modifier: Modifier = Modifier,
-    wellnessViewModel: WellnessViewModel = viewModel() // viewModel()은 기존 ViewModel을 반환하거나 지정된 범위에서 새 ViewModel을 생성합니다.
+    wellnessViewModel: WellnessViewModel = viewModel()
 ) {
     Column(modifier = modifier) {
         StatefulCounter()
 
         WellnessTasksList(
             list = wellnessViewModel.tasks,
-            onCloseTask = { task -> wellnessViewModel.remove(task) })
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
     }
 }
+
 
 private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
