59dde3f8c7cb33548770f3a5b240ed4875ea308a
NetLSS <futureprogrammer@naver.com>
2022-11-15 21:39:50 +0900

    Comment: viewModel()은 기존 ViewModel을 반환하거나 지정된 범위에서 새 ViewModel을 생성함

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 @Composable
 fun WellnessScreen(
     modifier: Modifier = Modifier,
-    wellnessViewModel: WellnessViewModel = viewModel()
+    wellnessViewModel: WellnessViewModel = viewModel() // viewModel()은 기존 ViewModel을 반환하거나 지정된 범위에서 새 ViewModel을 생성합니다. 
 ) {
     Column(modifier = modifier) {
         StatefulCounter()
