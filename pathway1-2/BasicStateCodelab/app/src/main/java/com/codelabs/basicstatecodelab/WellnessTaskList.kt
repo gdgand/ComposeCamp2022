e928a2a0921e8878bcbc23f9bf556801f1c72a86
elis0m <elisom615@gmail.com>
2022-11-15 17:53:11 +0900

    viewmodel 구현

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskList.kt
@@ -3,14 +3,12 @@ package com.codelabs.basicstatecodelab
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTaskList(
-    list: List<WellnessTask> = remember {
-        getWellnessTasks()
-    },
+    list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -18,9 +16,11 @@ fun WellnessTaskList(
         modifier = modifier
     ) {
         items(items = list, key = { task -> task.id }) { task ->
-            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) })
         }
     }
 }
-
-private fun getWellnessTasks() = List(50) { i -> WellnessTask(i, "Task # $i") }
