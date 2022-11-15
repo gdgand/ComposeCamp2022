fffbdaad98aac87e131a5ac3457cd318338485dd
NetLSS <futureprogrammer@naver.com>
2022-11-15 21:50:11 +0900

    Feat: Boolean 대신 MutableState<Boolean> 을 사용

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
@@ -1,4 +1,7 @@
 package com.codelabs.basicstatecodelab
 
-data class WellnessTask(val id: Int, val label: String, var checked: Boolean = false)
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+data class WellnessTask(val id: Int, val label: String, val checked: MutableState<Boolean> = mutableStateOf(false))
 
