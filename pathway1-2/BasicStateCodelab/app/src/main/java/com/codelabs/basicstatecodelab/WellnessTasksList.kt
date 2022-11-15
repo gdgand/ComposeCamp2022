b3219c8803409909a17bb32cb71020be16317919
NetLSS <futureprogrammer@naver.com>
2022-11-15 21:20:04 +0900

    Comment: list 를 상위 수준으로 호이스팅 했기 때문에, 람다로 close 콜백을 받는다.

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -11,7 +11,7 @@ private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
 @Composable
 fun WellnessTasksList(
     list: List<WellnessTask>,
-    onCloseTask: (WellnessTask) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit, // list 를 상위 수준으로 호이스팅 했기 때문에, 람다로 close 콜백을 받는다.
     modifier: Modifier = Modifier
 ) {
     LazyColumn(modifier = modifier) {
