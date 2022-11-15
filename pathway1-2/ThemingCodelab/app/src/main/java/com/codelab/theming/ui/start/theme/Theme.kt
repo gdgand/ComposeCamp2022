ac8a55cc0a6913a7e15d750c0bb91806772edfe3
sungho8 <dkdlah0830@naver.com>
2022-11-15 16:23:02 +0900

    feat : ThemingCodelab

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -0,0 +1,9 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JetnewsTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
