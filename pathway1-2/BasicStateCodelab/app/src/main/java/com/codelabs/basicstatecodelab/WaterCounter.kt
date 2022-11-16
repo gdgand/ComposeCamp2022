7bdc50bba8e0145e824fc32f7ffc2be5bba32491
rosf73 <alswlzhsclq@naver.com>
2022-11-16 19:37:57 +0900

    Water Counter 버튼 추가 - 하지만 UI의 값은 동적으로 변경되지 않음 - string resources 분리

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,19 +1,29 @@
 package com.codelabs.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(
     modifier: Modifier = Modifier
 ) {
-    val count = 0
-
-    Text(
-        text = "You've had $count glasses.",
+    Column(
         modifier = modifier.padding(16.dp)
-    )
+    ) {
+        var count = 0
+
+        Text(stringResource(R.string.wc_counter, count))
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(stringResource(R.string.wc_add))
+        }
+    }
 }
