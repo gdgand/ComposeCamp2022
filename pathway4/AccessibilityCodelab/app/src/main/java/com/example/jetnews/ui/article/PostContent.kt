0b43901a654f1428affbdbb1d5aec681a5ca8344
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-11-17 01:40:24 +0900

    [Pathway 4-2] 8. Custom merging

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/PostContent.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/PostContent.kt
@@ -134,7 +134,7 @@ private fun PostHeaderImage(post: Post) {
 @Composable
 private fun PostMetadata(metadata: Metadata) {
     val typography = MaterialTheme.typography
-    Row {
+    Row(Modifier.semantics(true) {}) {
         Image(
             imageVector = Icons.Filled.AccountCircle,
             contentDescription = null,
@@ -183,7 +183,9 @@ private fun Paragraph(paragraph: Paragraph) {
             )
             ParagraphType.Header -> {
                 Text(
-                    modifier = Modifier.padding(4.dp).semantics { heading() },
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .semantics { heading() },
                     text = annotatedString,
                     style = textStyle.merge(paragraphStyle)
                 )
