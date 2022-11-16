6b5be971c784ca118160b6bb400757e2b71c6d79
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-11-17 01:29:46 +0900

    [Pathway 4-2] 5. Custom actions

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
@@ -48,6 +48,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.CustomAccessibilityAction
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.customActions
 import androidx.compose.ui.semantics.onClick
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
@@ -62,10 +65,20 @@ import com.example.jetnews.ui.theme.JetnewsTheme
 @Composable
 fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
     var openDialog by remember { mutableStateOf(false) }
+    val showFewerLabel = stringResource(R.string.cd_show_fewer)
     Row(
         Modifier.clickable(
             onClickLabel = stringResource(R.string.action_read_article)
-        ) { navigateToArticle(post.id) }
+        ) {
+            navigateToArticle(post.id)
+        }.semantics {
+            customActions = listOf(
+                CustomAccessibilityAction(
+                    label = showFewerLabel,
+                    action = { openDialog = true; true }
+                )
+            )
+        }
     ) {
         Image(
             painter = painterResource(post.imageThumbId),
@@ -97,6 +110,7 @@ fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
         }
         CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
             IconButton(
+                modifier = Modifier.clearAndSetSemantics {  },
                 onClick = { openDialog = true }
             ) {
                 Icon(
