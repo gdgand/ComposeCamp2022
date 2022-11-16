a1a5cb3cdf77fecb2004658763824411b0e293a7
yc.park <yc.park@deepfine.ai>
2022-11-17 08:41:25 +0900

    feat($compose): Testing

--- pathway1-2/MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
+++ pathway1-2/MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
@@ -20,6 +20,9 @@ import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.Navigation.findNavController
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -48,64 +51,63 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class PlantDetailFragmentTest {
 
-    @Rule
-    @JvmField
-    val activityTestRule = ActivityScenarioRule(GardenActivity::class.java)
+  @Rule
+  @JvmField
+  val composeTestRule = createAndroidComposeRule<GardenActivity>()
 
-    // Note that keeping these references is only safe if the activity is not recreated.
-    private lateinit var activity: ComponentActivity
+  // Note that keeping these references is only safe if the activity is not recreated.
+  private lateinit var activity: ComponentActivity
 
-    @Before
-    fun jumpToPlantDetailFragment() {
-        populateDatabase()
+  @Before
+  fun jumpToPlantDetailFragment() {
+    populateDatabase()
 
-        activityTestRule.scenario.onActivity { gardenActivity ->
-            activity = gardenActivity
+    composeTestRule.activityRule.scenario.onActivity { gardenActivity ->
+      activity = gardenActivity
 
-            val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
-            findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
-        }
+      val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
+      findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
     }
+  }
 
-    @Test
-    fun testPlantName() {
-        onView(ViewMatchers.withText("Apple"))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-    }
+  @Test
+  fun testPlantName() {
+    composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
+  }
 
-    @Test
-    fun testShareTextIntent() {
-        val shareText = activity.getString(R.string.share_text_plant, testPlant.name)
+  @Test
+  fun testShareTextIntent() {
+    val shareText = activity.getString(R.string.share_text_plant, testPlant.name)
 
-        Intents.init()
-        onView(withId(R.id.action_share)).perform(click())
-        intended(
-            chooser(
-                allOf(
-                    hasAction(Intent.ACTION_SEND),
-                    hasType("text/plain"),
-                    hasExtra(Intent.EXTRA_TEXT, shareText)
-                )
-            )
+    Intents.init()
+    onView(withId(R.id.action_share)).perform(click())
+    intended(
+      chooser(
+        allOf(
+          hasAction(Intent.ACTION_SEND),
+          hasType("text/plain"),
+          hasExtra(Intent.EXTRA_TEXT, shareText)
         )
-        Intents.release()
+      )
+    )
+    Intents.release()
 
-        // dismiss the Share Dialog
-        InstrumentationRegistry.getInstrumentation()
-            .uiAutomation
-            .performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK)
-    }
+    // dismiss the Share Dialog
+    InstrumentationRegistry.getInstrumentation()
+      .uiAutomation
+      .performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK)
+  }
 
-    // TODO: This workaround is needed due to the real database being used in tests.
-    //  A fake database created with a Room.inMemoryDatabaseBuilder should be used instead.
-    //  That's difficult to do in the current state of the project since there are no
-    //  dependency injection best practices in place.
-    private fun populateDatabase() {
-        val request = TestListenableWorkerBuilder<SeedDatabaseWorker>(
-            InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
-        ).build()
-        runBlocking {
-            request.doWork()
-        }
+  // TODO: This workaround is needed due to the real database being used in tests.
+  //  A fake database created with a Room.inMemoryDatabaseBuilder should be used instead.
+  //  That's difficult to do in the current state of the project since there are no
+  //  dependency injection best practices in place.
+  private fun populateDatabase() {
+    val request = TestListenableWorkerBuilder<SeedDatabaseWorker>(
+      InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+    ).build()
+    runBlocking {
+      request.doWork()
     }
+  }
 }
