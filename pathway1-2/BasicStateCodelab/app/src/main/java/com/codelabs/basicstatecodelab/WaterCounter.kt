b039d0385a676d55eb3b8c37efb97d63cb2c89d7
sindorim5 <kihunbuzz@naver.com>
2022-11-17 01:55:35 +0900

    State in Compose

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,51 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaterPreview() {
+    BasicStateCodelabTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            StatefulCounter()
+        }
+    }
+}
