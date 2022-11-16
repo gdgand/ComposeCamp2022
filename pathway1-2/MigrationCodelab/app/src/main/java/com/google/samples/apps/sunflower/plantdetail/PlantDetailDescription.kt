7564216c5a21d4a8b5bd204c3e71801babb5e879
강신범 <mimic12@spidor.co.kr>
2022-11-16 17:09:38 +0900

    [Pathway 1-2] ViewModel 및 LiveData 진행

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -23,18 +23,27 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 //region Composable
 @Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
-    }
+fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
+    val plant by plantDetailViewModel.plant.observeAsState()
+    
+    plant?.let { PlantDetailContent(plant = it) }
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant) {
+    PlantName(name = plant.name)
 }
 
 @Composable
@@ -51,11 +60,28 @@ fun PlantName(name: String) {
 //endregion
 
 //region Preview
+//@Preview
+//@Composable
+//private fun PlantDetailDescriptionPreview() {
+//    MaterialTheme {
+//        PlantDetailDescription()
+//    }
+//}
+
 @Preview
 @Composable
-private fun PlantDetailDescriptionPreview() {
+private fun PlantDetailContentPreview() {
+    val plant = Plant(
+        "id",
+        "Apple",
+        "description",
+        3,
+        30,
+        ""
+    )
+    
     MaterialTheme {
-        PlantDetailDescription()
+        PlantDetailContent(plant = plant)
     }
 }
 
