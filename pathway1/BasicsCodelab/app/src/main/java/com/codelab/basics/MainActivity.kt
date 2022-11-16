09bb7da786aa9ec0111ee6ad4d338d2029fe8d7e
Yebon Kim <v954242@gmail.com>
2022-11-16 22:24:22 +0900

    [BasicCodelab] until step 13

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
@@ -29,12 +44,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(
+fun MyApp(modifier: Modifier = Modifier) {
+  var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+  Surface(modifier) {
+    if (shouldShowOnboarding) {
+      OnboardingScreen(onContinuedClicked = { shouldShowOnboarding = false })
+    } else {
+      Greetings()
+    }
+  }
+}
+
+@Composable
+private fun Greetings(
   modifier: Modifier = Modifier,
-  names: List<String> = listOf("World", "Compose")
+  names: List<String> = List(1000) { "$it" }
 ) {
-  Column(modifier = modifier.padding(vertical = 4.dp)) {
-    for (name in names) {
+  LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+    items(items = names) { name ->
       Greeting(name = name)
     }
   }
@@ -42,28 +70,92 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
+  val expanded = remember { mutableStateOf(false) }
+  val extraPadding by animateDpAsState(
+    if (expanded.value) 48.dp else 0.dp,
+    animationSpec = spring(
+      dampingRatio = Spring.DampingRatioMediumBouncy,
+      stiffness = Spring.StiffnessLow
+    )
+  )
+
   Surface(
     color = MaterialTheme.colorScheme.primary,
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
   ) {
     Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier.weight(1f)) {
+      Column(
+        modifier = Modifier
+          .weight(1f)
+          .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+      ) {
         Text(text = "Hello,")
-        Text(text = name)
+        Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+          fontWeight = FontWeight.ExtraBold
+        ))
       }
       ElevatedButton(
-        onClick = { /* TODO */ }
+        onClick = { expanded.value = !expanded.value }
       ) {
-        Text("Show more")
+        Text(if (expanded.value) "Show less" else "Show more")
       }
     }
   }
 }
 
+@Composable
+fun OnboardingScreen(
+  onContinuedClicked: () -> Unit,
+  modifier: Modifier = Modifier
+) {
+
+  Column(
+    modifier = modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Text("Welcome to the Basics Codelab!")
+    Button(
+      modifier = Modifier.padding(vertical = 24.dp),
+      onClick = onContinuedClicked
+    ) {
+      Text("Continue")
+    }
+  }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+  BasicsCodelabTheme {
+    MyApp(Modifier.fillMaxSize())
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun GreetingsPreview() {
+  BasicsCodelabTheme {
+    Greetings()
+  }
+}
+
+@Preview(showBackground = true,
+  widthDp = 320,
+  uiMode = UI_MODE_NIGHT_YES,
+  name = "Dark")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
   BasicsCodelabTheme {
-    MyApp()
+    MyApp(Modifier.fillMaxSize())
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+  BasicsCodelabTheme {
+    OnboardingScreen(onContinuedClicked = {})
   }
 }
--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -1,21 +1,37 @@
 package com.codelab.basics.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 private val DarkColorPalette = darkColorScheme(
-    primary = Purple200,
+    primary = Navy,
     primaryContainer = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    surface = Blue,
+    onSurface = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorPalette = lightColorScheme(
-    primary = Purple500,
+    primary = LightBlue,
     primaryContainer = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    surface = Blue,
+    onSurface = Color.White,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color.White,
@@ -30,16 +46,28 @@ private val LightColorPalette = lightColorScheme(
 @Composable
 fun BasicsCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content
