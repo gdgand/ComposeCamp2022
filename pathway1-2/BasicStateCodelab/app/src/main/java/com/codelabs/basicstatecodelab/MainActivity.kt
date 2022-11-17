d9462beab4a25100369369ae7a9c1c54ee10c86b
kim-yongsuk <dydtjr@98>
2022-11-17 18:46:17 +0900

    Composable Function - Memory

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +43,8 @@ fun WellnessScreen(
 fun WaterCount(
     modifier: Modifier = Modifier
 ) {
-    var count = 0
+    var count by remember { mutableStateOf(0) }
+
     Column(
         modifier = modifier.padding(16.dp)
     ) {
