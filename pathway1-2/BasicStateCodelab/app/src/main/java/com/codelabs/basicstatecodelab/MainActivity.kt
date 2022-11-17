93ba007c9e7f21a98355e43008fa31e8ff8cbc5b
bcchoi0202 <bcchoi0202@gmail.com>
2022-11-17 16:51:22 +0900

    5. Memory in a composable function

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,10 +39,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text(text = "You've had $count glasses.")
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        Text(text = "You've had ${count.value} glasses.")
         Button(
-            onClick = { count++ },
+            onClick = { count.value++ },
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text("Add one")
