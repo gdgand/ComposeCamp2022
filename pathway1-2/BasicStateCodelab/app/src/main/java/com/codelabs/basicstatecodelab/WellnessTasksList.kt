0378c9c8b4f6ebd5c19427d870921af280a69873
gudrmsglgl <gudrmsglgl@gmail.com>
2022-11-17 15:02:40 +0900

    gudrmsglgl #pathway1_2 - BasicStateCodelab

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,30 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit,
+    list: List<WellnessTask>
+) {
+    LazyColumn(modifier = modifier) {
+        items(
+            list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                isCheck = task.checked,
+                onCheckChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) },
+            )
+        }
+    }
+}
+
