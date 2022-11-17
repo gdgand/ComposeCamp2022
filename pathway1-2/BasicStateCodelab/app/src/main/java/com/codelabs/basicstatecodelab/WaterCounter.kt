5ae4e603785145de63ca02bcb22d5184a6d8e1af
pmh <pmh@hwajintni.co.kr>
2022-11-17 18:00:50 +0900

    [1-2][스테이트] 구성 가능한 함수의 메모리

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -4,16 +4,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember { mutableStateOf(0) }
         Text(
-            text = "너는 $count 잔의 물을 마셨다.",
+            text = "너는 ${count} 잔의 물을 마셨다.",
             modifier = modifier.padding(16.dp)
         )
         Button(
