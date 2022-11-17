ea0bfdf13a8896c2a4f36a715ecfda4f2bf53994
Moon-Young, Lee <ogretest@gmail.com>
2022-11-17 16:38:42 +0900

    Pathway 1-2 : BasicStateCodelab : 6. 상태 기반 UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -12,8 +12,14 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(value = 0) }
-        Text(text = "You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+        Button(
+            onClick = { count++ },
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text("Add one")
         }
     }
