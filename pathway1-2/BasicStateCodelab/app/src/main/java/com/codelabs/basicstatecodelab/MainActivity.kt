807a27a7ca55fce967f8a51719ad378dc80e06ec
kim-yongsuk <dydtjr@98>
2022-11-17 20:06:45 +0900

    Composition Remember

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,6 +41,31 @@ fun WellnessScreen(
 }
 
 @Composable
+fun WellNessTaskItem(
+    modifier: Modifier = Modifier,
+    name : String,
+    onClose: () -> Unit
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = name,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+        IconButton(onClick = onClose) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Composable
 fun WaterCount(
     modifier: Modifier = Modifier
 ) {
@@ -49,18 +75,36 @@ fun WaterCount(
         modifier = modifier.padding(16.dp)
     ) {
         if (count > 0) {
+
+            var isShowTask by remember { mutableStateOf(true) }
+
+            if (isShowTask) WellNessTaskItem(name = "TASK NAME") { isShowTask = false }
+
             Text(
                 text = "Count : $count",
                 modifier = Modifier.padding(16.dp)
             )
         }
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(8.dp),
-            enabled = count < 10
+
+        Row(
+            Modifier.padding(top = 8.dp)
         ) {
-            Text(text = "Add One")
+
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text(text = "Add One")
+            }
+
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "Clear")
+            }
         }
+
     }
 }
 
