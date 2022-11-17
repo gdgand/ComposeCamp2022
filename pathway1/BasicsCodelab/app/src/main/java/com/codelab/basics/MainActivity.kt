341d97c3ec5bf53adaaa2543fff4bcdb8fc057c7
jhj5897 <wkdgpwls1@naver.com>
2022-11-17 19:22:25 +0900

    12. 앱의 스타일 지정 및 테마 설정

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Button
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,47 +36,120 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) } //OnboardingScreen 의 변수를 호이스팅
+    //remember : 컴포저블이 컴포지션에 유지되는 동안 작동
+    //rememberSaveable : 구성 변경, 프로세스 중단 시에도 값 저장
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"   //Preview 이름
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = { })
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if(expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Surface(color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        //Surface 는 일종의 container. 색상 설정하면 텍스트의 배경색처럼 보임
+
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)) {
+                Text(text = "Hello, ") //Surface color 에 따라 적절한 색상으로 처리(테마의 onPrimary 색상)
+                Text(text = name, style = MaterialTheme.typography.h5
+//                    .copy(fontWeight =  FontWeight.ExtraBold)
+                    , fontWeight = FontWeight.ExtraBold
+                )
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded }
+            ) {
+
+                Text(if(expanded) "Show less" else "Show more")
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }  //$it : 목록의 index
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for(name in names) {
+    //RecyclerView 와 유사하나 하위 요소 재활용을 하지 않음.
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        //Android studio 가 기본적으로 다른 items 함수를 선택하므로 별도로 선언 필요 (androidx.compose.foundation.lazy.items)
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }   //Hoisting 변수
+    //Q. by 로 변수 생성 시 .value 가 필요 없는 이유?
 
-    Surface(color = MaterialTheme.colors.primary,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        //Surface 는 일종의 container. 색상 설정하면 텍스트의 배경색처럼 보임
-
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(Modifier.weight(1f)
-                .padding(bottom = extraPadding)) {
-                Text(text = "Hello, ") //Surface color 에 따라 적절한 색상으로 처리(테마의 onPrimary 색상)
-                Text(text = name)
-            }
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-
-                Text(if(expanded.value) "Show less" else "Show more")
-            }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
         }
-
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        MyApp()
     }
 }
