6a02eea5853393d13af025f6b13f0034542a7048
Hyuck9 <lhg1304@nexmore.co.kr>
2022-11-17 13:48:38 +0900

    5. 맞춤 작업

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
@@ -39,6 +39,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.CustomAccessibilityAction
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.customActions
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,10 +55,19 @@ import com.example.jetnews.ui.theme.JetnewsTheme
 @Composable
 fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
     var openDialog by remember { mutableStateOf(false) }
+    val showFewerLabel = stringResource(id = R.string.cd_show_fewer)
     Row(
         Modifier.clickable(
             onClickLabel = stringResource(id = R.string.action_read_article)
         ) { navigateToArticle(post.id) }
+            .semantics {
+                customActions = listOf(
+                    CustomAccessibilityAction(
+                        label = showFewerLabel,
+                        action = { openDialog = true; true }
+                    )
+                )
+            }
     ) {
         Image(
             painter = painterResource(post.imageThumbId),
@@ -85,7 +98,10 @@ fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
             }
         }
         CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            IconButton(onClick = { openDialog = true }) {
+            IconButton(
+                modifier = Modifier.clearAndSetSemantics {  },
+                onClick = { openDialog = true }
+            ) {
                 Icon(
                     imageVector = Icons.Default.Close,
                     contentDescription = stringResource(R.string.cd_show_fewer),
