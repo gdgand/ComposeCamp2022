a28423c036327c07dd0ef5de2e366d75aaa11b1c
ka <insideka01@gmail.com>
2022-11-18 00:24:01 +0900

    11.17 State in Compose - 9. State hoisting

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -4,21 +4,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatefulCounter(modifier: Modifier= Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, onIncrement = { count++ }, modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = mutableStateOf(0)
-        
-        Text(text = "You've had ${count.value} glasses.")
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
         Button(
-            onClick = { count.value++ },
-            modifier = Modifier.padding(top = 8.dp)
+            onClick = onIncrement,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
             Text(text = "Add one")
         }
