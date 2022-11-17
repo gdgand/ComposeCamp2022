a79e5690971019242f33ccb650acc150f747a689
Moon-Young, Lee <ogretest@gmail.com>
2022-11-17 16:29:03 +0900

    Pathway 1-2 : BasicStateCodelab : 3. Compose에서의 상태
    - WaterCounter 추가
    - WellnessScreen 추가

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
