d7c1c3eea25bdd0d6d7584d9f6dce75ffe2168c9
kangmin1012 <kangmin1012@naver.com>
2022-11-18 17:26:17 +0900

    [Pathway 1-2] 컴포즈 상태
    [Pathway 1-2] 컴포즈 상태 - Section.3
    
    [Pathway 1-2] 컴포즈 상태 - Section.4
    
    
    [Pathway 1-2] 컴포즈 상태 - Section.5
    
    
    [Pathway 1-2] 컴포즈 상태 - Section.6
    
    
    [Pathway 1-2] 컴포즈 상태 - Section.7 ~ 8
    
    
    [Pathway 1-2] 컴포즈 상태 - Section. 9
    
    
    [Pathway 1-2] 컴포즈 상태 - Section. 10
    
    
    [Pathway 1-2] 컴포즈 상태 - Section. 11
    
    
    [Pathway 1-2] 컴포즈 상태 - Section. 12

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -3,13 +3,19 @@ package com.codelabs.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
@@ -30,14 +36,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
+
+    }
+}
+
+
+@Composable
+fun StatelessCounter(
+    modifier: Modifier = Modifier,
+    count: Int,
+    onIncrement: () -> Unit
+) {
+   Column(modifier = modifier.padding(16.dp)) {
+       if (count > 0) {
+           Text("You've had $count glasses.")
+       }
+
+       Button(
+           onClick = onIncrement,
+           modifier = Modifier.padding(top = 8.dp),
+           enabled = count < 10
+       ) {
+           Text(text = "Add one")
+       }
+   }
+}
+
+@Composable
+fun StatefulCounter(
+    modifier: Modifier = Modifier
+) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(modifier, count) { count++ }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun WellnessScreenPreview() {
     BasicStateCodelabTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            WellnessScreen()
+        }
     }
 }
---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskList.kt
@@ -0,0 +1,101 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCloseTask = { task -> wellnessViewModel.remove(task) },
+            onCheckedTask = { task, changed -> wellnessViewModel.changeTaskChecked(task, changed)}
+        )
+    }
+}
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onClose = { onCloseTask(task) },
+                onCheckedChange = { checked -> onCheckedTask(task, checked) }
+            )
+        }
+    }
+}
+
+@Composable
+fun WellnessTaskItem(
+    modifier: Modifier = Modifier,
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(checked = checked, onCheckedChange = onCheckedChange)
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessTaskListPreview() {
+    BasicStateCodelabTheme {
+        WellnessTasksList(
+            modifier = Modifier,
+            onCloseTask = { },
+            onCheckedTask = { _: WellnessTask, _: Boolean -> },
+            list = getWellnessTasks()
+        )
+    }
+}
