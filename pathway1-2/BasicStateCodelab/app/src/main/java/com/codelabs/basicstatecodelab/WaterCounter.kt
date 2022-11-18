3fdcbcf198ce6e95588acf9250cb461eff32166d
amy <essim@wemade.com>
2022-11-18 09:24:09 +0900

    [Jetpack Compose의 상태] 컴포지션의 Remember - 코드 원복

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -14,23 +14,10 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text("You've had $count glasses.")
         }
-
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(onClick = { count++ }, enabled = count < 10) {
-                Text("Add one")
-            }
-            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
-                Text("Clear water count")
-            }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
