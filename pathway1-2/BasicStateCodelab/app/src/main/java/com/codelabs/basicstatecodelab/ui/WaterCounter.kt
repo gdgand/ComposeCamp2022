bdeb9bcf1d2a9c8cc9e5f9736f502ed7ceca15e7
aurora32s <aurora32s@naver.com>
2022-11-18 16:47:18 +0900

    [Ch.7] Remember in Composition

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.codelabs.basicstatecodelab.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -13,13 +14,31 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
-        if (count > 0) Text(text = "You've had $count glassed")
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
-            enabled = count <= 10
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
+            Text(text = "You've had $count glassed")
+        }
+        Row(
+            modifier = Modifier.padding(top = 8.dp)
         ) {
-            Text(text = "Add one")
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "Clear water count")
+            }
         }
     }
 }
