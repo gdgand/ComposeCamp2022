9be63513dfa699cb2ebb6d610abdd46a268cd95d
pmh <pmh@hwajintni.co.kr>
2022-11-18 10:12:42 +0900

    [1-2][스테이트] Compose에서 상태 복원

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -6,16 +6,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text(
-                text = "너는 ${count} 잔의 물을 마셨다.",
+                text = "너는 $count 잔의 물을 마셨다.",
                 modifier = modifier.padding(16.dp)
             )
         }
