1f90fd22d1d47878f51cd3a2b55aee273a53d933
may <saka6502@gmail.com>
2022-11-18 11:14:44 +0900

    p1-2 : Jetpack Compose의 상태 - 12

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
@@ -1,3 +1,13 @@
 package com.codelabs.basicstatecodelab
 
-data class WellnessTask(val id: Int, val label: String)
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
