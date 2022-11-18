7b62cc2a58632d3c9e7095d37a3616ecf8ec277a
김대우 <kdw0310@ajou.ac.kr>
2022-11-18 20:33:48 +0900

    2022.11.18 Pathway1-2 진행중

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,19 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+    }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
