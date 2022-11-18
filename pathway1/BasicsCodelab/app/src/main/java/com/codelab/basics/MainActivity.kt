d638e19c6d2ff091a61f1b4d3c10d95b7372d51e
cwpark <cwpark@bbros.kr>
2022-11-18 21:58:23 +0900

    설정완료
     - 버튼 -> 아이콘 버튼으로 변경
     - 컨텐츠 Card로 wrapping
     - string 리소스 추가

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,17 +4,24 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +43,7 @@ class MainActivity : ComponentActivity() {
 fun MyApp(
     modifier: Modifier = Modifier
 ) {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(false) }
     Surface(modifier = modifier) {
         if(shouldShowOnboarding) {
             OnboardingScreen(
@@ -52,55 +59,59 @@ fun MyApp(
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
-    ) {
-        //내가 짠 코드
-//        Box(modifier = Modifier.fillMaxWidth()) {
-//            Column(modifier = Modifier
-//                .padding(24.dp)
-//            ) {
-//                Text(text = "Hello,")
-//                Text(text = name)
-//            }
-//
-//            OutlinedButton(
-//                modifier = Modifier
-//                .align(Alignment.CenterEnd)
-//                .padding(16.dp),
-//                shape = RoundedCornerShape(20.dp),
-//                onClick = { /*TODO*/ }
-//            ) {
-//                Text(text = "Show more")
-//            }
-//        }
-        val expanded = remember { mutableStateOf(false) }
-        val extraPadding by animateDpAsState (
-            if(expanded.value) 48.dp else 0.dp,
+fun CardContents(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(modifier = Modifier
+        .padding(24.dp)
+        .animateContentSize(
             animationSpec = spring(
                 dampingRatio = Spring.DampingRatioMediumBouncy,
                 stiffness = Spring.StiffnessLow
             )
-        )
-        //코드랩 솔루션 코드
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello,")
-                Text(text = name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
-            }
-
-            //ElevatedButton은 material3
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(text = if(expanded.value) "show less" else "Show more")
+        )) {
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(text = name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
         }
+
+        IconButton(
+            onClick = { expanded = !expanded },
+        ) {
+            Icon(
+                imageVector = if(expanded) {
+                    Icons.Filled.ExpandLess
+                } else {
+                    Icons.Filled.ExpandMore
+                },
+                contentDescription = if(expanded) {
+                    stringResource(id = R.string.show_less)
+                } else {
+                    stringResource(id = R.string.show_more)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Card (
+        backgroundColor = MaterialTheme.colors.primary,
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+        elevation = 4.dp
+    ) {
+        CardContents(name = name)
     }
 }
 
