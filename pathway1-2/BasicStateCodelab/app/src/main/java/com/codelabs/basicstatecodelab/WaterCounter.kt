ecc423ac4f75b234f01830d2a92e012834e2101b
Dex <dex@hpcnt.com>
2022-11-18 23:40:41 +0900

    pathway1-2 remember mutableState

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,26 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        Text(
+            text = "You've had ${count.value} glasses.",
+        )
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
+
+}
