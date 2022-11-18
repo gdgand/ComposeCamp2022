a105654157aa9149ea747e64c861a5ac1205df47
ka <insideka01@gmail.com>
2022-11-18 23:38:14 +0900

    11.18 State in Compose - 10.Work with lists

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -1,9 +1,21 @@
 package com.codelabs.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     StatefulCounter(modifier)
+    WellnessTaskList()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessScreenPreview(modifier: Modifier = Modifier) {
+    Column {
+        StatefulCounter()
+        WellnessTaskList()
+    }
 }
