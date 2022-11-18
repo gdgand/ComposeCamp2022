718bc2fc8745799694f028bddde1402dfc32a001
jaeYoun <example@example.com>
2022-11-18 21:45:20 +0900

    [feat: compose camp 1주차 마무리]

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -13,7 +23,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +35,7 @@ class MainActivity : ComponentActivity() {
         // setContent를 사용하여 레이아웃 정의 후 구성 가능한 함수(@Compose 함수)를 호출
         setContent {
             // 구성 가능한 함수 스타일을 지정
+            // 스타일을 지정할 경우 content 내부에 있는 구성요소로 하향 적용.
             BasicsCodelabTheme {
                 MyApp(modifier = Modifier.fillMaxSize())     // 너비, 높이 모두 전체 채우기
             }
@@ -32,7 +46,8 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MyApp(modifier: Modifier = Modifier) {
         // 타 화면을 표시하는 로직
-        var shouldShowOnbording by remember { mutableStateOf(true) }
+        // rememberSaveable을 사용하여 현재 상태를 유지 시켜줌.
+        var shouldShowOnbording by rememberSaveable { mutableStateOf(true) }
 
         // OnboardingScreen이 상태를 변경하도록 하는 대신 사용자가 Continue 버튼을 클릭했을 때
         // 앱에 알리도록 한다. 이벤트 전달은 콜백으로 전달.
@@ -44,66 +59,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    @Composable
-    private fun Greetings(
-        modifier: Modifier = Modifier,
-        names: List<String> = listOf("World", "Compose")
-    ) {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            for (name in names) {
-                Greeting(name = name)
-            }
-        }
-    }
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-private fun GreetingsPreview() {
-    BasicsCodelabTheme {
-        Greetings()
-    }
-}
-
-
-// Composable 주석을 통해 구성 가능한 함수를 만들어줌.
-@Composable
-private fun Greeting(name: String) {
-    /*
-    UI를 지정하기 위해 Text() 를 Surface() {} 로 감싸줌
-    Surface()로 배경색 지정이 가능함.
-     */
-    // Composable에 내부 상태를 추가하려면 mutableStateOf 함수를 사용.
-    // 리컴포지션 간에 상태를 유지하기 위해 remember를 사용하여 변경 가능한 상태를 기억해야 함.
-    val expanded = remember { mutableStateOf(false) }
-
-    // 상태에 따라 달라지는 추가 변수 , 간단한 계산을 실행하는 변수
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            // weight를 사용해 가중치를 주어 가중치가 적용되지 않은 요소를 밀어내고 해당 버튼을 배치함.
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
-            ) {
-            Text(text = "Hello, ")
-            Text(text = name)
-        }
-        // 코너 커스텀이 들어간 버튼 생성 후 텍스트 변경 설정
-        ElevatedButton(
-            onClick = { expanded.value = !expanded.value} ,
-        ) {
-            Text(if (expanded.value) "Show less" else "Show more")
-        }
-
-        }
-
-    }
-}
 
 // 상태가 아닌 함수를 OnboardingScreen에 전달하는 방식으로 Composable의 재사용성 가능성을 높이고
 // 다른 Composable이 상태를 변경하지 않도록 보호함.
@@ -129,6 +84,102 @@ fun OnboardingScreen(
     }
 }
 
+@Composable
+private fun Greetings(
+        modifier: Modifier = Modifier,
+        names: List<String> = List(1000) {"$it"}            // 1000개의 목록을 생성.
+    ) {
+        // 리사이클러뷰와 동일한 역할
+        LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+           items(items = names) { name ->
+               Greeting(name = name)
+           }
+        }
+    }
+// Composable 주석을 통해 구성 가능한 함수를 만들어줌.
+// Greeting을 Card Composable로 재구성 해줌.
+// Greeting 내부의 column에 새로운 Text 추가, CardDefaults.cardColors를 호출하고 변경하려는 색상을 재정의하여
+// Card의 색상을 변경.
+@Composable
+private fun Greeting(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    // 기본 상태 정의
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                // 열고 닫을 애니메이션을 정의함.
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                // 헤더 스타일 지정
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy +" +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = {expanded = !expanded}) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    // strings.xml에 정의해둔 문자열 가져오기
+                    stringResource(R.string.show_less)
+                }else {
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
+    }
+}
+
+// 다크모드 미리보기 설정
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES,
+    widthDp = 320,
+    name = "Dark"
+)
+
+// @Preview => 미리보기 사용을 위한 어노테이션 지정, 동일한 파일에 미리보기를 여러 개 만들고 이름을 지정 가능.
+// 미리보기 가로 크기 추가(320dp)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
+
+
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
@@ -137,15 +188,7 @@ fun OnboardingPreview() {
     }
 }
 
-// @Preview => 미리보기 사용을 위한 어노테이션 지정, 동일한 파일에 미리보기를 여러 개 만들고 이름을 지정 가능.
-// 미리보기 가로 크기 추가(320dp)
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        MyApp()
-    }
-}
+
 
 // MyApp의 Composable의 Preview
 @Preview
--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -1,20 +1,31 @@
 package com.codelab.basics.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
-private val DarkColorPalette = darkColorScheme(
-    primary = Purple200,
-    onPrimary = Purple700,
-    secondary = Teal200
+private val DarkColorScheme = darkColorScheme(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
-private val LightColorPalette = lightColorScheme(
-    primary = Color.Blue,
-    onPrimary = Color.White,
-    secondary = Teal200
+private val LightColorScheme = lightColorScheme(
+   surface = Blue,
+   onSurface = Color.White,
+   primary = LightBlue,
+   onPrimary = Navy
 
     /* Other default colors to override
     background = Color.White,
@@ -29,14 +40,30 @@ private val LightColorPalette = lightColorScheme(
 @Composable
 fun BasicsCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    // dynamicColor boolean 매개변수를 사용하여 Theme.kt에서 동적으로 색상을 추가하는 로직을 확인 가능.
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        val currentWindow = (view.context as? Activity)?.window?: throw Exception("Not in an activity - unable to get window reference")
+
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
+    // Material 디자인 사양의 스타일 적용이 가능한 함수.
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
