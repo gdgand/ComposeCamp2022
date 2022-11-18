97e8d277e05e13be11860a90f34c75d316d65542
Keel_im <kimh00335@gmail.com>
2022-11-18 15:09:27 +0900

    Jetpack Compose theming

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val RED200 = Color(0xfff297a2)
+val RED300 = Color(0xffea6d7e)
+val RED700 = Color(0xffdd0d3c)
+val RED800 = Color(0xffd00036)
+val RED900 = Color(0xffc20029)
+
