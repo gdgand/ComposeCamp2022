5a4393fd909720624a0b48d5389dc7dab797c786
taeseongyun <yts0646@gmail.com>
2022-11-19 18:44:24 +0900

    [ Feature ] : 코드랩 호이스팅 챕터 완료

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.codelab.basics.onboarding.OnBoarding
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 /***
@@ -65,7 +68,18 @@ fun Greeting(name: String) {
 
 // 기본적으로 함수는 빈 수정자 ( Modifier ) 를 가지고 있는것이 좋다.
 @Composable
-private fun MyApp(
+private fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    if(shouldShowOnBoarding) {
+        OnBoarding { shouldShowOnBoarding = false }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
@@ -82,7 +96,14 @@ private fun MyApp(
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+private fun OnBoardingPreview() {
+    BasicsCodelabTheme {
+        OnBoarding(onClickListener = {})
+    }
+}
