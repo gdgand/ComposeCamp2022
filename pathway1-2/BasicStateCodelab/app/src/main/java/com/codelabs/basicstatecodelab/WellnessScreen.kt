c37794492e60093d3e6262f76218099e93e03327
ka <insideka01@gmail.com>
2022-11-20 00:57:13 +0900

    11.19 State in Compose - 11.Observable MutableList

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -2,20 +2,28 @@ package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     StatefulCounter(modifier)
-    WellnessTaskList()
+
+    val list = remember { getWellnessTasks().toMutableStateList() }
+    WellnessTaskList(list = list, onCloseTask = { task -> list.remove(task) })
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
 
 @Preview(showBackground = true)
 @Composable
 fun WellnessScreenPreview(modifier: Modifier = Modifier) {
     Column {
         StatefulCounter()
-        WellnessTaskList()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTaskList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -6,18 +6,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
+
 
 @Composable
 fun WellnessTaskList(
     modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit
 ) {
     LazyColumn(
         modifier = modifier
     ) {
         items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
         }
     }
 }
