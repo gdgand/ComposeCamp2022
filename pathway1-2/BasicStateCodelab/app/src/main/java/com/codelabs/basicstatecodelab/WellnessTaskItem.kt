9c7352f187e1a0c664d2d7ded3e5ecad6cc4679e
shim seok bo <shadowog@gmail.com>
2022-11-19 15:29:39 +0900

    State in Jetpack Compose - 10 work with lists

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
@@ -2,12 +2,14 @@ package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,20 +17,43 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName : String,
-    onClose : ()->Unit,
     modifier: Modifier = Modifier,
 ){
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = {}, // we will implement this later!
+        modifier = modifier,
+    )
+
+}
+
+@Composable
+fun WellnessTaskItem(
+    taskName : String,
+    checked : Boolean,
+    onCheckedChange : (Boolean) -> Unit,
+    onClose : ()-> Unit,
+    modifier : Modifier = Modifier,
+){
     Row(
-        modifier = modifier, verticalAlignment = Alignment.CenterVertically,
-    ) {
+        modifier = modifier, verticalAlignment =  Alignment.CenterVertically,
+    ){
         Text(
             modifier = Modifier
                 .weight(1f)
                 .padding(start = 16.dp),
-            text = taskName,
+            text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
-            Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
+            Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
 }
