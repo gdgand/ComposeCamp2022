e13a3d47c34977a5c10a03cd74d86d933fc9a76e
larry.p <larry.p@kakaocorp.com>
2022-11-19 21:50:53 +0900

    feat - PW1-2 테마 라이트 테마, 타이포그라피, 모양 추가

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
