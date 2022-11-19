f421e6293a4531691c6648405008db3e9400c2b2
leesh96 <suho2718@gmail.com>
2022-11-19 10:12:42 +0900

    [Pathway-1] BasicsCodelab 9. Creating a performant lazy list

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -67,11 +69,11 @@ fun OnboardingScreen(
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Android", "Compose"),
+    names: List<String> = List(1000) { "$it" },
 ) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+    LazyColumn(modifier = Modifier.padding(4.dp)) {
+        items(items = names) { name ->
+            Greeting(name)
         }
     }
 }
