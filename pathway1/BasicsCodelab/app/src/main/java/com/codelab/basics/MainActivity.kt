9288fab46f1123df4edebda3360d00cfb259bc71
taeseongyun <yts0646@gmail.com>
2022-11-19 18:17:15 +0900

    [ Refactor ] : 코드랩 상태 완료

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,14 +3,16 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
@@ -33,22 +35,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    // var isExpanded = false // 절대 이렇게 해서는 안된다. -> 그 이유 해당 컴포즈가 추적을 하지 못하기 때문
+    val isExpanded = remember { mutableStateOf(false) }
+
+    val extraPending = if (isExpanded.value) 48.dp else 0.dp
+
     Surface(
-        color = MaterialTheme.colors.primary,
+        color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp)
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Hello, ")
-                Text(text = "$name!")
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPending)
+            ) {
+                Text(text = "Hello, ", color = Color.White)
+                Text(text = "$name!", color = Color.White)
             }
 
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Show More")
+            ElevatedButton(
+                onClick = { isExpanded.value = !isExpanded.value }
+            ) {
+                Text(text = if (isExpanded.value) "Show Less" else "Show More")
             }
         }
     }
