79ab020e2f8063553c17373249ebb8a665f4b73c
YS <yoonsam86@gmail.com>
2022-11-19 23:07:05 +0900

    pathway1-2 Basic State Codelab - Composable 함수의 메모리

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -5,6 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -17,12 +21,20 @@ fun WaterCounter(modifier: Modifier = Modifier) {
      * 상태 수정을 야기하는 작업을 '이벤트'라고 합니다.
      */
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        // Changes to count are now tracked by Compose
+        var count by remember { mutableStateOf(0) }
+
         Text(text = "You've had $count glasses.")
 
         /**
          * count 변수에 다른 값을 설정해도 Compose에서 이 값을 상태 변경으로 감지하지 않으므로 아무 일도 일어나지 않습니다.
          * 이는 상태가 변경될 때 Compose에 화면을 다시 그려야 한다고(즉, Composable 함수를 'recompose') 알리지 않았기 때문입니다.
+         *
+         * count가 초깃값이 0인 mutableStateOf API를 사용하도록 WaterCounter 컴포저블을 업데이트합니다.
+         * mutableStateOf가 MutableState 유형을 반환하므로 value를 업데이트하여 상태를 업데이트할 수 있고 Compose는 value를 읽는 이러한 함수에 리컴포지션을 트리거합니다.
+         *
+         * count가 변경되면 count의 value를 자동으로 읽는 Composable 함수의 리컴포지션이 예약됩니다.
+         * 이 경우 WaterCounter는 버튼을 클릭할 때마다 재구성됩니다.
          */
         Button(
             onClick = { count++ },
