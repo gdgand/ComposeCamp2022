8096611430e4210f24a4cc027d0eb0ee8490330e
leesy <leesy@rsupport.com>
2022-11-19 15:56:42 +0900

    [3] State in Jetpack Compose

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessViewModel.kt
@@ -0,0 +1,26 @@
+package com.codelabs.basicstatecodelab
+
+/**
+ * Created by jerry on 2022/11/19
+ **/
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel : ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) =
+        tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
