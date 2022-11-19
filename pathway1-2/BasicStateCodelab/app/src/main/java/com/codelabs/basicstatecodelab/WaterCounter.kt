d6263ee08fdb0c90de828ba087b2561ef8e0a9c8
mbkim95 <mbkim95@naver.com>
2022-11-19 21:12:27 +0900

    [BasicState] 상태 호이스팅

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -29,3 +29,25 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(
+            onClick = onIncrement,
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10,
+        ) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier = modifier)
+}
