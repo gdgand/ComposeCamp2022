842486a69428176e821a7bdb97c82db412592122
larry.p <larry.p@kakaocorp.com>
2022-11-19 21:55:40 +0900

    feat - PW1-2 테마 다크 테마 추가

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -2,6 +2,11 @@ package com.codelab.theming.ui.start.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Light
 val Red700 = Color(0xffdd0d3c)
 val Red800 = Color(0xffd00036)
 val Red900 = Color(0xffc20029)
+
+// Dark
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
