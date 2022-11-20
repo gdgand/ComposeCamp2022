0b89b6f83ae82a120306539b75f3c56cb0c15d46
mbkim95 <mbkim95@naver.com>
2022-11-20 19:29:13 +0900

    [Theming] '스타일' 구성요소

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -95,15 +95,15 @@ fun Header(
     Surface(
         color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
         contentColor = MaterialTheme.colors.primary,
-        modifier = modifier,
+        modifier = modifier.semantics { heading() },
     ) {
-        Text(text = text,
+        Text(
+            text = text,
             style = MaterialTheme.typography.subtitle2,
             modifier = modifier
                 .fillMaxWidth()
-                .background(Color.LightGray)
-                .semantics { heading() }
-                .padding(horizontal = 16.dp, vertical = 8.dp))
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+        )
     }
 }
 
