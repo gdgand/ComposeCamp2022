0b1fc94820d8413977aaae2c4bc90b63a901e563
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-11-21 02:42:50 +0900

    [pathway1-2] ThemingCodelab 실습 완료

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Shape.kt
@@ -0,0 +1,7 @@
+package com.codelab.theming.ui.start
+
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
