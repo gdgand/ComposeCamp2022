ab261124a26f5fbe54d732e1af648952aa770099
jinhyungchoi <wlsgudchoi@gmail.com>
2022-11-20 23:48:27 +0900

    feat: State in ViewModel

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/Wellness.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/Wellness.kt
@@ -2,27 +2,24 @@ package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier = modifier) {
         StatefulCounter()
-        val list = remember {
-            getWellnessTasks().toMutableStateList()
-        }
         WellnessTaskList(
-            list = list,
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
             onCloseTask = { task ->
-                list.remove(task)
+                wellnessViewModel.remove(task)
             }
         )
     }
-}
-
-
-private fun getWellnessTasks() = List(30) { i ->
-    WellnessTask(i, "Task # $i")
 }
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -14,13 +14,21 @@ private fun getWellnessTasks() = List(30) { i ->
 fun WellnessTaskList(
     modifier: Modifier = Modifier,
     list: List<WellnessTask> = remember { getWellnessTasks() },
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit
 ) {
     LazyColumn(modifier = modifier) {
         items(list) { task ->
-            WellnessTaskItem(taskName = task.label, onClose = {
-                onCloseTask(task)
-            })
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked ->
+                    onCheckedTask(task, checked)
+                },
+                onClose = {
+                    onCloseTask(task)
+                }
+            )
         }
     }
 }
