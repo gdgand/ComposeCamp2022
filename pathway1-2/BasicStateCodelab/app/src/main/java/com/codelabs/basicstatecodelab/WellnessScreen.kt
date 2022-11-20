381441c6df9abd97c23ce21b8689db18e6e9b23f
jisang <jisangman89@gmail.com>
2022-11-21 01:23:53 +0900

    Update : 목록 사용

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -1,10 +1,14 @@
 package com.codelabs.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
 //    WaterCounter(modifier)
-    StatefulCounter(modifier)
+        StatefulCounter(modifier)
+        WellnessTasksList()
+    }
 }
