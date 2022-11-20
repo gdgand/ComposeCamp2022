d995cebdf6e9aac76b2157223e3ef96f629f4094
심은석 <eushim@gmarket.com>
2022-11-20 17:06:43 +0900

    Compose에서의 이벤트

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,12 +9,14 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
+    var count = 0
 
     Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
+        text = "You've had $count glasses."
     )
+    Button(onClick = { count++ }) {
+        Text("Add one")
+    }
 }
 
 @Composable
