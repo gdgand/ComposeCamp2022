ecddef3be8846ea9cbac3f448512679634131f8c
EaseTheWorld <easetheworld@gmail.com>
2022-11-20 11:30:54 +0900

    [Pathway1-2] 5 Memory in a composable function

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,24 +1,23 @@
 package com.codelabs.basicstatecodelab
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier : Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text(
-            text = "You've had $count glasses.",
-            modifier = modifier.padding(16.dp)
-        )
+        var count by remember { mutableStateOf(0) }
+        Log.i("Dukoo", "recomposition $count")
+        Text("You've had $count glasses.")
         Button(
             onClick = {
-                ++count
+               ++count
             },
             modifier = Modifier.padding(top = 8.dp)
         ) {
