fc2006077f3bffeba61c52d70c1192ad9c6970e7
sikbongda <jys1016@gmail.com>
2022-11-20 22:10:59 +0900

    Add if condition to the WaterCounter

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -4,19 +4,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
-        Text("You've had ${count.value} glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(8.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
