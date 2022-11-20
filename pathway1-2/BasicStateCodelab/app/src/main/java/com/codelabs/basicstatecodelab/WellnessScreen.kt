85661227ee9d849135375f2e54c529f6c5fef96b
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-11-20 22:33:10 +0900

    Restore state , State hoisting

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -1,10 +1,13 @@
-package com.codelabs.basicstatecodelab.ui
+package com.codelabs.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.codelabs.basicstatecodelab.WaterCount
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCount(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
