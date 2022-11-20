75dc6763b74d650967efeec4ee0b665a8053ee4e
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-11-20 21:43:08 +0900

    State driven UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCount.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCount.kt
@@ -12,10 +12,13 @@ import androidx.compose.ui.unit.dp
 fun WaterCount(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text("You've had $count glasses.")
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
         Button(
             onClick = { count++ },
-            Modifier.padding(top = 8.dp)
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
             Text("Add one")
         }
