7416e5f6f93aed4bad47adb2fe6f4b1a365ef456
NetLSS <futureprogrammer@naver.com>
2022-11-20 16:52:11 +0900

    Feat: PlantDescription 추가
    - html 스트링을 전달 받아서 기억한다.
    - html 로 변환하는 컨버팅 작업이 추가된다.
    - 안드로이드 뷰 로 텍스트 뷰를 만들고, 업데이트 콜백을 설정한다.

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -17,6 +17,8 @@
 package com.google.samples.apps.sunflower.plantdetail
 
 import android.content.res.Configuration
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -27,6 +29,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -35,6 +38,8 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
@@ -101,6 +106,35 @@ private fun PlantWatering(wateringInterval: Int) {
     }
 }
 
+@Composable
+private fun PlantDescription(description: String) {
+    // Remembers the HTML formatted description. Re-executes on a new description
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    // Displays the TextView on the screen and updates with the HTML description when inflated
+    // Updates to htmlDescription will make AndroidView recompose and update the text
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDescription
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+    MaterialTheme {
+        PlantDescription("HTML<br><br>description")
+    }
+}
+
 @Preview
 @Composable
 private fun PlantDetailContentPreview() {
@@ -123,14 +157,6 @@ private fun PlantNamePreview() {
 private fun PlantWateringPreview() {
     MdcTheme {
         PlantWatering(7)
-    }
-}
-
-@Preview
-@Composable
-private fun PlantDescriptionPreview() {
-    MdcTheme {
-//        PlantDetailDescription(plantDetailViewModel = PlantDetailViewModel())
     }
 }
 
