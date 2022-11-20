2a62ea437601efde748fcb07527e278a7817d746
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-11-20 21:34:37 +0900

    State in Compose

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,10 @@
+package com.codelabs.basicstatecodelab.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.codelabs.basicstatecodelab.WaterCount
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCount(modifier)
+}
