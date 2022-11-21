2dd45de0b290818bc9980845d847b1d39416cf66
Moon-Young, Lee <ogretest@gmail.com>
2022-11-21 15:24:21 +0900

    Pathway 3 : NavigationCodelab : 8. 딥 링크 지원 사용 설정

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -100,7 +100,8 @@ fun RallyApp() {
                 }
                 composable(
                     route = SingleAccount.routeWithArgs,
-                    arguments = SingleAccount.arguments
+                    arguments = SingleAccount.arguments,
+                    deepLinks = SingleAccount.deepLinks
                 ) { navBackStackEntry ->
                     val accountType =
                         navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
