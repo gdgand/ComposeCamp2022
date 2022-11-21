3b6271b0fb5817afa9066a1d9a71c8030e62602e
조엘_전국진 <joel@afreecatv.com>
2022-11-21 23:05:11 +0900

    styling and theming your app

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -25,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
@@ -88,7 +90,9 @@ fun Greeting(name: String) {
         Row(Modifier.padding(24.dp).padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h3.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
 
             Button(onClick = { expanded = !expanded }) {
@@ -119,7 +123,14 @@ fun OnboardingScreen(
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
