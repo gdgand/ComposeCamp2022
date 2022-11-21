2f0336be7611fcd3eac3453771413ac523186a7a
Hooooong <h921013@gmail.com>
2022-11-21 21:49:07 +0900

    heungki:pathway1/BasicsCodelab - Compose에서의 상태

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,20 +44,26 @@ private fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
+    val expended = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expended.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
 
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "show More")
+            ElevatedButton(onClick = { expended.value = !expended.value }) {
+                Text(text = if (expended.value) "Show Less" else "Show More")
             }
         }
     }
