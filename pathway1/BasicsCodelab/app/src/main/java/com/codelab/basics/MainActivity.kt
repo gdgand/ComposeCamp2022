9675b88df11641350c8b7047b999b86b3f4c2014
조엘_전국진 <joel@afreecatv.com>
2022-11-21 22:26:42 +0900

    State in Compose

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,14 +53,17 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(Modifier.padding(24.dp)) {
+        val expanded = remember { mutableStateOf(false) }
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+        Row(Modifier.padding(24.dp).padding(bottom = extraPadding)) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
 
-            Button(onClick = {}) {
-                Text("Show more")
+            Button(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
