cf4f66bf13fba8c386ff8530dd18376ed3028231
shshksh <shshksh58@gmail.com>
2022-11-21 22:22:02 +0900

    BasicState: State driven UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCount.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCount.kt
@@ -6,16 +6,32 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
-fun WaterCount(modifier: Modifier = Modifier) {
+fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
-        Text(text = "You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text(text = "Add one")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaterCounterPreview() {
+    BasicStateCodelabTheme {
+        WaterCounter()
     }
 }
