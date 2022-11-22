d642462eae2cdfc25e7b436c8c2e2b24b82fb4e6
최민재 <cmjcmj1230@gmail.com>
2022-11-22 23:16:24 +0900

    [PATHWAY 1-1] : BasicCodelab
    흐엉

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -27,15 +28,30 @@ private val LightColorPalette = lightColors(
     */
 )
 
+private val LightColorScheme = lightColors(
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+private val DarkColorScheme = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
 @Composable
 fun BasicsCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        DarkColorScheme
     } else {
-        LightColorPalette
+        LightColorScheme
     }
 
     MaterialTheme(
