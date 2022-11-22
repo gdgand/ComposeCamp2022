04abfbba962c76c0f87b74c62f7b35accd8e3bbc
KangTaeJong98 <rkdxowhd98@naver.com>
2022-11-22 22:00:31 +0900

    Theme

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
+val Red700 = Color(0xFFDD0D3C)
+val Red800 = Color(0xFFD00036)
+val Red900 = Color(0xFFC20029)
