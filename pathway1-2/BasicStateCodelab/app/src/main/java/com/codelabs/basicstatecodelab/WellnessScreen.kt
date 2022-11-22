0c5256cf7808405347fd521986c618088aa7a6d8
jhj5897 <wkdgpwls1@naver.com>
2022-11-22 22:23:16 +0900

    [PATHWAY1-2]6.13축하합니다

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -5,17 +5,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier = modifier) {
         StatefulCounter(modifier)
 
         //긴 직렬화/역직렬화가 필요한 복잡한 데이터 구조나 대량의 데이터를 저장하는 데 rememberSaveable 을 사용하면 안된다.
-        val list = remember { getWellnessTasks().toMutableStateList() }
-        WellnessTasksList(list = list, onCloseTask = {task -> list.remove(task) })
+//        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked -> wellnessViewModel.changeTaskChecked(task, checked) },
+            onCloseTask = { task -> wellnessViewModel.remove(task) }
+        )
     }
 }
-
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i,"Task # $i" )}
