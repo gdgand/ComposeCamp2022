c35273039a59dbefc5e96f8fa1c6e0fe07e58514
rosf73 <alswlzhsclq@naver.com>
2022-11-22 14:06:48 +0900

    state 분리 연습 코드 삭제

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -13,17 +13,11 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun StatefulCounter(modifier: Modifier = Modifier) {
-    var waterCount by remember { mutableStateOf(0) }
-    var juiceCount by remember { mutableStateOf(0) }
+    var count by remember { mutableStateOf(0) }
 
     StatelessCounter(
-        waterCount,
-        { waterCount++ },
-        modifier
-    )
-    StatelessCounter(
-        juiceCount,
-        { juiceCount++ },
+        count,
+        { count++ },
         modifier
     )
 }
