56a079e77356e8857eba9d49f87d97e5d60b4a32
Jooheon <jooheon.lee@bugs.co.kr>
2022-11-22 14:57:29 +0900

    Feat: ComposeTheming

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.codelab.theming.theme
+
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
+
+internal val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
+
+internal val DarkColors = darkColors(
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,
+    secondary = Red300,
+    onSecondary = Color.Black,
+    error = Red200
+)
