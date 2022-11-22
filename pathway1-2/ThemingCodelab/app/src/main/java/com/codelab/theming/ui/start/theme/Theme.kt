30201e4ef0bc11b52880e6f4309eceb64ed7f042
rosf73 <alswlzhsclq@naver.com>
2022-11-22 20:26:40 +0900

    Jetnews Theme 에 지정 Color 적용

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -1,11 +1,26 @@
 package com.codelab.theming.ui.start.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
 
 @Composable
 fun JetnewsTheme(
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(content = content)
+    MaterialTheme(
+        colors = LightColors,
+        content = content
+    )
 }
