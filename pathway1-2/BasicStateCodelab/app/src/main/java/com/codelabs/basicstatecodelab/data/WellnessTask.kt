a3766b5b867fafae2b04d186c6540bd2ad2b4f87
rosf73 <alswlzhsclq@naver.com>
2022-11-22 15:16:39 +0900

    Wellness Task 에 check state 를 Mutable State 로 정의

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/data/WellnessTask.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/data/WellnessTask.kt
@@ -1,7 +1,13 @@
 package com.codelabs.basicstatecodelab.data
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 data class WellnessTask(
     val id: Int,
     val label: String,
-    var checked: Boolean = false
-)
+    val initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
