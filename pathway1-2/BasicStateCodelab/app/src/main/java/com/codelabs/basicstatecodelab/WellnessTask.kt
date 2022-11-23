cc386a1e02e75a1c129aada5dbc23ca772da2175
hee.youn <heesung6701@naver.com>
2022-11-23 22:06:48 +0900

    [Compose에서의 상태] 진행 완료

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
@@ -0,0 +1,7 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.runtime.*
+
+data class WellnessTask(val id: Int, val label: String, val initialChecked: Boolean = false) {
+    var checked by mutableStateOf(initialChecked)
+}
---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessViewModel.kt
@@ -0,0 +1,22 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+class WellnessViewModel : ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(task: WellnessTask) {
+        _tasks.remove(task)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) {
+        tasks.find { item.id == it.id }?.let {
+            it.checked = checked
+        }
+    }
+}
