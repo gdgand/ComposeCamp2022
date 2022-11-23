be15bd49692ec8780f1ee7e43aa67b6a21e3489a
jisang <jisangman89@gmail.com>
2022-11-23 11:26:04 +0900

    Update : 테마 정의

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val JetnewsShape = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 8.dp),
+    large = RoundedCornerShape(topStart = 8.dp)
+)
