0bc5b8c47a496ab0d6c92cacf8a8de1dfc5e4098
Animir <kagi1@naver.com>
2022-11-24 15:25:49 +0900

    [State] Jetpack Compose의 상태
    - 상태 호이스팅

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,34 +1,37 @@
 package com.codelabs.basicstatecodelab
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@SuppressLint("UnrememberedMutableState")
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
-            Text(text = "You've had $count glasses.")
+            Text("You've had $count glasses.")
         }
-
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(onClick = { count++ }, modifier = Modifier.padding(start = 8.dp), enabled = count < 10) {
-                Text("Add one")
-            }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
 
 @Composable
+fun StatefulCounter() {
+    var count by remember { mutableStateOf(0) }
+
+    Row {
+        StatelessCounter(count, { count++ })
+        StatelessCounter(count, { count *= 2 })
+    }
+}
+
+@Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier = modifier)
+    StatefulCounter()
 }
