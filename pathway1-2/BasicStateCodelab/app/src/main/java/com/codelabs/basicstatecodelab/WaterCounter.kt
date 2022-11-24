8c2f5481e48f37309d90d1e2a469c4e3080edcfd
Animir <kagi1@naver.com>
2022-11-24 14:03:00 +0900

    [State] Jetpack Compose의 상태
    - Compose에서의 상태

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,21 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier){
+    val count = 0
+    Text(
+       text = "You've had $count glasses.",
+       modifier = modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier){
+    WaterCounter(modifier = modifier)
+}
