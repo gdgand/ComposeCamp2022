3bab3e71f098ad7504586efa7f7615d52ee06255
blucky8649 <blucky4511@gmail.com>
2022-11-24 21:46:37 +0900

    [testing]Debugging tests

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,9 @@
 package com.example.compose.rally
 
 import androidx.compose.material.Text
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -28,7 +27,20 @@ class TopAppBarTest {
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
     }
-
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
+    }
 
 
 }
