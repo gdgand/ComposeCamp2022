6c9deaa0c98eea51f87e24fb3a55dd2712e18b5a
blucky8649 <blucky4511@gmail.com>
2022-11-24 21:57:49 +0900

    [testing]Merged and unmerged Semantics trees

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -37,8 +37,17 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 
