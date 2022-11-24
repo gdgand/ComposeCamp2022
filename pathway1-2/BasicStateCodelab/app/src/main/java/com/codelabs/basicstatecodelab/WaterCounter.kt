668cf8fed7ddf02c7588224a45d823801d2f3e7d
Animir <kagi1@naver.com>
2022-11-24 14:13:43 +0900

    [State] Jetpack Compose의 상태
    - 구성 가능한 함수의 메모리

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,25 +1,27 @@
 package com.codelabs.basicstatecodelab
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
+fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember { mutableStateOf(0) }
         Text(text = "You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)){
+        Button(  onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
 }
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier){
+fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier = modifier)
 }
