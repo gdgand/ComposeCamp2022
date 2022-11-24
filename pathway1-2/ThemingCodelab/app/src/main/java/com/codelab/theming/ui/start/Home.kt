e88bbbb857ea674be7970219e926534d2863dbfe
limchaemyung <limchaemyung@lgcns.com>
2022-11-24 14:10:29 +0900

    pathway1-2: [Jetpack Compose theming] 완료

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -27,21 +27,14 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.ListItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Palette
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -49,29 +42,28 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.theming.R
 import com.codelab.theming.data.Post
 import com.codelab.theming.data.PostRepo
+import com.codelab.theming.ui.start.theme.JetNewsTheme
 import java.util.Locale
 
 @Composable
 fun Home() {
     val featured = remember { PostRepo.getFeaturedPost() }
     val posts = remember { PostRepo.getPosts() }
-    MaterialTheme {
-        Scaffold(
-            topBar = { AppBar() }
-        ) { innerPadding ->
+    JetNewsTheme {
+        Scaffold(topBar = { AppBar() }) { innerPadding ->
             LazyColumn(contentPadding = innerPadding) {
                 item {
                     Header(stringResource(R.string.top))
                 }
                 item {
                     FeaturedPost(
-                        post = featured,
-                        modifier = Modifier.padding(16.dp)
+                        post = featured, modifier = Modifier.padding(16.dp)
                     )
                 }
                 item {
@@ -88,47 +80,44 @@ fun Home() {
 
 @Composable
 private fun AppBar() {
-    TopAppBar(
-        navigationIcon = {
-            Icon(
-                imageVector = Icons.Rounded.Palette,
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 12.dp)
-            )
-        },
-        title = {
-            Text(text = stringResource(R.string.app_title))
-        },
-        backgroundColor = MaterialTheme.colors.primary
+    TopAppBar(navigationIcon = {
+        Icon(
+            imageVector = Icons.Rounded.Palette,
+            contentDescription = null,
+            modifier = Modifier.padding(horizontal = 12.dp)
+        )
+    }, title = {
+        Text(text = stringResource(R.string.app_title))
+    }, backgroundColor = MaterialTheme.colors.primarySurface
     )
 }
 
 @Composable
 fun Header(
-    text: String,
-    modifier: Modifier = Modifier
+    text: String, modifier: Modifier = Modifier
 ) {
-    Text(
-        text = text,
+    Surface(
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
+        contentColor = MaterialTheme.colors.primary,
         modifier = modifier
-            .fillMaxWidth()
-            .background(Color.LightGray)
-            .semantics { heading() }
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-    )
+    ) {
+        Text(text = text,
+            style = MaterialTheme.typography.subtitle2,
+            modifier = modifier
+                .fillMaxWidth()
+                .semantics { heading() }
+                .padding(horizontal = 16.dp, vertical = 8.dp))
+    }
 }
 
 @Composable
 fun FeaturedPost(
-    post: Post,
-    modifier: Modifier = Modifier
+    post: Post, modifier: Modifier = Modifier
 ) {
     Card(modifier) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable { /* onClick */ }
-        ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .clickable { /* onClick */ }) {
             Image(
                 painter = painterResource(post.imageId),
                 contentDescription = null,
@@ -141,12 +130,10 @@ fun FeaturedPost(
 
             val padding = Modifier.padding(horizontal = 16.dp)
             Text(
-                text = post.title,
-                modifier = padding
+                text = post.title, modifier = padding
             )
             Text(
-                text = post.metadata.author.name,
-                modifier = padding
+                text = post.metadata.author.name, modifier = padding
             )
             PostMetadata(post, padding)
             Spacer(Modifier.height(16.dp))
@@ -156,8 +143,7 @@ fun FeaturedPost(
 
 @Composable
 private fun PostMetadata(
-    post: Post,
-    modifier: Modifier = Modifier
+    post: Post, modifier: Modifier = Modifier
 ) {
     val divider = "  •  "
     val tagDivider = "  "
@@ -166,42 +152,43 @@ private fun PostMetadata(
         append(divider)
         append(stringResource(R.string.read_time, post.metadata.readTimeMinutes))
         append(divider)
+        val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+            background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+        )
         post.tags.forEachIndexed { index, tag ->
             if (index != 0) {
                 append(tagDivider)
             }
-            append(" ${tag.uppercase(Locale.getDefault())} ")
+            withStyle(tagStyle) {
+                append(" ${tag.uppercase(Locale.getDefault())} ")
+            }
         }
     }
-    Text(
-        text = text,
-        modifier = modifier
-    )
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text(
+            text = text, modifier = modifier
+        )
+    }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PostItem(
-    post: Post,
-    modifier: Modifier = Modifier
+    post: Post, modifier: Modifier = Modifier
 ) {
-    ListItem(
-        modifier = modifier
-            .clickable { /* todo */ }
-            .padding(vertical = 8.dp),
-        icon = {
-            Image(
-                painter = painterResource(post.imageThumbId),
-                contentDescription = null
-            )
-        },
-        text = {
-            Text(text = post.title)
-        },
-        secondaryText = {
-            PostMetadata(post)
-        }
-    )
+    ListItem(modifier = modifier
+        .clickable { /* todo */ }
+        .padding(vertical = 8.dp), icon = {
+        Image(
+            painter = painterResource(post.imageThumbId),
+            modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
+            contentDescription = null
+        )
+    }, text = {
+        Text(text = post.title)
+    }, secondaryText = {
+        PostMetadata(post)
+    })
 }
 
 @Preview("Post Item")
@@ -217,7 +204,18 @@ private fun PostItemPreview() {
 @Composable
 private fun FeaturedPostPreview() {
     val post = remember { PostRepo.getFeaturedPost() }
-    FeaturedPost(post = post)
+    JetNewsTheme {
+        FeaturedPost(post = post)
+    }
+}
+
+@Preview("Featured Post , Dark")
+@Composable
+private fun FeaturedPostDarkPreview() {
+    val post = remember { PostRepo.getFeaturedPost() }
+    JetNewsTheme(darkTheme = true) {
+        FeaturedPost(post = post)
+    }
 }
 
 @Preview("Home")
