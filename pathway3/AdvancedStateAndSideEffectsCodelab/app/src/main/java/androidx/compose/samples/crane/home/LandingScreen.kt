3c11ac177097dfbfecba0d521f82bbaca62dafe7
sangsulee <sangsulee@gmarket.com>
2022-11-25 01:01:21 +0900

    Feat:`rememberUpdatedState` 사용, `LaunchedEffect` 에 상수값 키 사용
    - `rememberUpdatedState` 를 사용하여 항상 최신의 `onTimeout` 함수를 참조할 수 있다. (만약에 컴포저블이 리컴포지션 된다고 하더라도!)
    - `LaunchedEffect(true)` 를 통해서 랜딩 스크린 컴포저블의 라이프사이클과 연동하여 사이드이팩트를 줄 수 있으며, 리컴포지션 또는 `onTimeout` 이 변경된다고 하더라도 내부에 있는 (여기서는 `delay`) 작업이 또 다시 실행되지 않도록 할 수 있다.

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -21,6 +21,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,14 +34,18 @@ private const val SplashWaitTime: Long = 2000
 @Composable
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // Start a side effect to load things in the background
-        // and call onTimeout() when finished.
-        // Passing onTimeout as a parameter to LaunchedEffect
-        // is wrong! Don't do this. We'll improve this code in a sec.
-        LaunchedEffect(onTimeout) {
-            delay(SplashWaitTime) // Simulates loading things
-            onTimeout()
+        // This will always refer to the latest onTimeout function that
+        // LandingScreen was recomposed with
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // Create an effect that matches the lifecycle of LandingScreen.
+        // If LandingScreen recomposes or onTimeout changes,
+        // the delay shouldn't start again.
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
         }
+
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
