62cc281e7522ab03089c0f58f1405c4578f2a049
sjLee <imy123123@naver.com>
2022-11-25 11:15:08 +0900

    [(State in Jetpack Compose) step 7 ]  recomposition 에서의 상태 변화 확인

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
@@ -13,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.codelabs.basicstatecodelab.ui.WellnessTaskItem
 import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,12 +40,24 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         var count by remember { mutableStateOf(0) }
 
         if(count > 0) {
-            Text(
-                text = "You've had $count glasses."
-            )
+            var showTask by remember { mutableStateOf(true) }
+            if(showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
+            Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, modifier = Modifier.padding(8.dp), enabled = count < 10) {
-            Text("Add one")
+
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+
+            Button(onClick = { count = 0 }, modifier = Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
