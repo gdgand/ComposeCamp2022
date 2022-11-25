62f22286bf9db09965c14ec9e72179d7dddca480
kin9700 <kin9700@gmail.com>
2022-11-25 12:30:02 +0900

    * AccessibilityCodelab - 상태 설명

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
@@ -45,6 +45,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -158,8 +160,17 @@ fun InterestsScreen(
 @Composable
 private fun TopicItem(itemTitle: String, selected: Boolean, onToggle: () -> Unit) {
     val image = painterResource(R.drawable.placeholder_1_1)
+    val stateNotSubscribed = stringResource(R.string.state_not_subscribed)
+    val stateSubscribed = stringResource(R.string.state_subscribed)
     Row(
         modifier = Modifier
+            .semantics {
+                stateDescription = if (selected) {
+                    stateSubscribed
+                } else {
+                    stateNotSubscribed
+                }
+            }
             .toggleable(
                 value = selected,
                 onValueChange = { _ -> onToggle() },
