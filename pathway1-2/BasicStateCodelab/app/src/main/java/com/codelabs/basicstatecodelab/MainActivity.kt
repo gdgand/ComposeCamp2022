581a30e0cb279bada00e81b03c4dda5f80fba7ae
sjLee <imy123123@naver.com>
2022-11-25 10:34:56 +0900

    [(State in Jetpack Compose) step 4 ]  버튼 추가와 람다 작성

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -3,14 +3,15 @@ package com.codelabs.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
@@ -33,12 +34,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
 
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+        Text(
+            text = "You've had $count glasses."
+        )
+        Button(onClick = { count++ }, modifier = Modifier.padding(8.dp)) {
+            Text("Add one")
+        }
+    }
 }
 
 @Composable
