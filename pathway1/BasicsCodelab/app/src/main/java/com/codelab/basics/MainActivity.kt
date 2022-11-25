230b68a8955b22e97c35b985c583d2f4435ee00f
ridsync <ridsync@gmail.com>
2022-11-25 21:38:50 +0900

    feature(compose) : #0001 - 목록에 애니메이션 적용, 앱의스타일 지정 및 테마, 설정 완료

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +39,9 @@ class MainActivity : ComponentActivity() {
 /**
  *  Composable Previews
  */
+@Preview(
+    showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -66,7 +74,7 @@ private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("W
 
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-    Surface(modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
         if (shouldShowOnboarding) {
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
@@ -77,8 +85,7 @@ private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("W
 
 @Composable
 fun OnboardingScreen(
-    onContinueClicked: () -> Unit,
-    modifier: Modifier = Modifier
+    onContinueClicked: () -> Unit, modifier: Modifier = Modifier
 ) {
 
     Column(
@@ -88,8 +95,7 @@ fun OnboardingScreen(
     ) {
         Text("Welcome to the Basics Codelab!")
         Button(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
+            modifier = Modifier.padding(vertical = 24.dp), onClick = onContinueClicked
         ) {
             Text("Continue")
         }
@@ -98,8 +104,7 @@ fun OnboardingScreen(
 
 @Composable
 private fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = List(20) { "Compose $it" }
+    modifier: Modifier = Modifier, names: List<String> = List(20) { "Compose $it" }
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
@@ -115,32 +120,73 @@ private fun Greetings(
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = rememberSaveable { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ), modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
 
+@Composable
+fun CardContent(name: String) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+//    val extraPadding by animateDpAsState(
+//        if (expanded) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
     Surface(
-        color = MaterialTheme.colors.primaryVariant,
+        color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
 
-        Row(modifier = Modifier
-            .padding(24.dp)
-            .padding(bottom = extraPadding)
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
-            Column(modifier = Modifier
+            Column(
+                modifier = Modifier
                     .weight(1f)
+                    .padding(12.dp)
             ) {
                 Text(text = "Hello, ", modifier = Modifier.fillMaxWidth())
-                Text(text = name)
-            }
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                val text =
-                    if (expanded.value) stringResource(id = R.string.show_less) else stringResource(
-                        id = R.string.show_more
+                Text(
+                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ), modifier = Modifier.padding(bottom = 16.dp)
+                )
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " + "padding theme elit, sed do bouncy. ").repeat(
+                            4
+                        )
                     )
-                Text(text, color = MaterialTheme.colors.primary)
+                }
+            }
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
+//                val text =
+//                    if (expanded) stringResource(id = R.string.show_less) else stringResource(
+//                        id = R.string.show_more
+//                    )
+//                Text(text, color = MaterialTheme.colorScheme.primary)
             }
         }
     }
