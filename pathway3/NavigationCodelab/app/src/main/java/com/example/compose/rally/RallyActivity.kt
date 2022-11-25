0d9ca666279137806ee828b181122825a5ee24d5
Animir <kagi1@naver.com>
2022-11-25 11:16:08 +0900

    [Navigation] Jetpack Compose 탐색
    - RallyNavHost에 NavHost 추출

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -56,6 +56,50 @@ class RallyActivity : ComponentActivity() {
 }
 
 @Composable
+fun RallyNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = Overview.route,
+        modifier = modifier
+    ) {
+        composable(route = Overview.route) {
+            OverviewScreen(
+                onAccountClick = { accountType ->
+                    navController.navigateToSingleAccount(accountType)
+                },
+                onClickSeeAllAccounts = {
+                    navController.navigateSingleTopTo(Accounts.route)
+                },
+                onClickSeeAllBills = {
+                    navController.navigateSingleTopTo(Bills.route)
+                }
+            )
+        }
+        composable(route = Accounts.route) {
+            AccountsScreen(
+                onAccountClick = { accountType ->
+                    navController.navigateToSingleAccount(accountType)
+                }
+            )
+        }
+        composable(route = Bills.route) {
+            BillsScreen()
+        }
+        composable(
+            route = SingleAccount.routeWithArgs,
+            arguments = SingleAccount.arguments,
+            deepLinks = SingleAccount.deepLinks
+        ){ navBackStackEntry ->
+            val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+            SingleAccountScreen(accountType = accountType)
+        }
+    }
+}
+
+@Composable
 fun RallyApp() {
     RallyTheme {
 //        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
@@ -75,61 +119,66 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
+            RallyNavHost(
+                navController = navController,
+                modifier = Modifier.padding(innerPadding)
+            )
 //            Box(Modifier.padding(innerPadding)) {
 //                currentScreen.screen()
 //            }
-            NavHost(
-                navController = navController,
-                startDestination = Overview.route,
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                composable(route = Overview.route) {
-                    OverviewScreen(
-                        onAccountClick = { accountType ->
-                            navController.navigateToSingleAccount(accountType)
-                        },
+//            NavHost(
+//                navController = navController,
+//                startDestination = Overview.route,
+//                modifier = Modifier.padding(innerPadding)
+//            ) {
+//                composable(route = Overview.route) {
+//                    OverviewScreen(
+//                        onAccountClick = { accountType ->
+//                            navController.navigateToSingleAccount(accountType)
+//                        },
 //                        onClickSeeAllAccounts = {
 //                            navController.navigateSingleTopTo(Accounts.route)
 //                        },
 //                        onClickSeeAllBills = {
 //                            navController.navigateSingleTopTo(Bills.route)
 //                        }
-                    )
-                }
-                composable(route = Accounts.route) {
-                    AccountsScreen(
-                        onAccountClick = { accountType ->
-                            navController.navigateToSingleAccount(accountType)
-                        }
-                    )
-                }
-                composable(route = Bills.route) {
-                    BillsScreen()
-                }
-                composable(
-                    route = SingleAccount.routeWithArgs,
-                    arguments = SingleAccount.arguments,
-                    deepLinks = SingleAccount.deepLinks
-                ){ navBackStackEntry ->
-                    val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-                    SingleAccountScreen(accountType = accountType)
-                }
-            }
+//                    )
+//                }
+//                composable(route = Accounts.route) {
+//                    AccountsScreen(
+//                        onAccountClick = { accountType ->
+//                            navController.navigateToSingleAccount(accountType)
+//                        }
+//                    )
+//                }
+//                composable(route = Bills.route) {
+//                    BillsScreen()
+//                }
+//                composable(
+//                    route = SingleAccount.routeWithArgs,
+//                    arguments = SingleAccount.arguments,
+//                    deepLinks = SingleAccount.deepLinks
+//                ){ navBackStackEntry ->
+//                    val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+//                    SingleAccountScreen(accountType = accountType)
+//                }
+//            }
         }
     }
 }
 
 // 확장함수
 fun NavHostController.navigateSingleTopTo(route: String) =
-    this.navigate(route) {
-        popUpTo(
-            this@navigateSingleTopTo.graph.findStartDestination().id
-        ) {
-            saveState = true
-        }
-        launchSingleTop = true
-        restoreState = true
-    }
+//    this.navigate(route) {
+//        popUpTo(
+//            this@navigateSingleTopTo.graph.findStartDestination().id
+//        ) {
+//            saveState = true
+//        }
+//        launchSingleTop = true
+//        restoreState = true
+//    }
+    this.navigate(route) { launchSingleTop = true }
 
 private fun NavHostController.navigateToSingleAccount(accountType: String) {
     this.navigateSingleTopTo("${SingleAccount.route}/$accountType")
