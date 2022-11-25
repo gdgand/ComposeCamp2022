a08bdf64a8b349e9c5f68f34e3482fcbf55362e9
jeongupark9 <jeongupark@gmail.com>
2022-11-26 00:13:43 +0900

    Jetpack Compose 탐색

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/ui/overview/OverviewScreen.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/ui/overview/OverviewScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.rally.R
 import com.example.compose.rally.data.UserData
@@ -56,6 +57,7 @@ import com.example.compose.rally.ui.components.BillRow
 import com.example.compose.rally.ui.components.RallyAlertDialog
 import com.example.compose.rally.ui.components.RallyDivider
 import com.example.compose.rally.ui.components.formatAmount
+import com.example.compose.rally.ui.theme.RallyTheme
 import java.util.Locale
 
 @Composable
