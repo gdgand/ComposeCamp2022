fb9d060f53d3a4314f03330afc7b44472d29dbcd
Willy <willy.jung@shoplworks.com>
2022-11-25 09:13:50 +0900

    feat(theme): 5. 색상 사용

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -27,19 +27,11 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.ListItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Palette
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -61,7 +53,7 @@ import java.util.Locale
 fun Home() {
     val featured = remember { PostRepo.getFeaturedPost() }
     val posts = remember { PostRepo.getPosts() }
-    JetnewsTheme {
+    JetnewsTheme(darkTheme = true) {
         Scaffold(
             topBar = { AppBar() }
         ) { innerPadding ->
@@ -89,19 +81,21 @@ fun Home() {
 
 @Composable
 private fun AppBar() {
-    TopAppBar(
-        navigationIcon = {
-            Icon(
-                imageVector = Icons.Rounded.Palette,
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 12.dp)
-            )
-        },
-        title = {
-            Text(text = stringResource(R.string.app_title))
-        },
-        backgroundColor = MaterialTheme.colors.primary
-    )
+    Surface(color = MaterialTheme.colors.surface) {
+        TopAppBar(
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Rounded.Palette,
+                    contentDescription = null,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            },
+            title = {
+                Text(text = stringResource(R.string.app_title))
+            },
+            backgroundColor = MaterialTheme.colors.primarySurface
+        )
+    }
 }
 
 @Composable
@@ -109,14 +103,19 @@ fun Header(
     text: String,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        text = text,
+    Surface(
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
+        contentColor = MaterialTheme.colors.primary,
         modifier = modifier
-            .fillMaxWidth()
-            .background(Color.LightGray)
-            .semantics { heading() }
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-    )
+    ) {
+        Text(
+            text = text,
+            modifier = modifier
+                .fillMaxWidth()
+                .semantics { heading() }
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
 }
 
 @Composable
@@ -174,10 +173,12 @@ private fun PostMetadata(
             append(" ${tag.uppercase(Locale.getDefault())} ")
         }
     }
-    Text(
-        text = text,
-        modifier = modifier
-    )
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text(
+            text = text,
+            modifier = modifier
+        )
+    }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -237,5 +238,7 @@ private fun FeaturedPostDarkPreview() {
 @Preview("Home")
 @Composable
 private fun HomePreview() {
-    Home()
+    JetnewsTheme(darkTheme = true) {
+        Home()
+    }
 }
