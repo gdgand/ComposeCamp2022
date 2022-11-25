1ad85df3a09182c930699b46946d60d876e233b5
Animir <kagi1@naver.com>
2022-11-25 14:29:48 +0900

    [Testing] Testing in Jetpack Compose
    - Synchronization

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -11,56 +11,56 @@ class TopAppBarTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun rallyTopAppBarTest() {
-        val allScreens = RallyScreen.values().toList()
-        composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
-        }
-        Thread.sleep(5000)
-    }
-
-    @Test
-    fun rallyTopAppBarTest_currentTabSelected() {
-        val allScreens = RallyScreen.values().toList()
-        composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
-        }
-
-        composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertIsSelected()
-    }
-
-    @Test
-    fun rallyTopAppBarTest_currentLabelExists() {
-        val allScreens = RallyScreen.values().toList()
-        composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
-        }
-
-        composeTestRule.onNode(
-            hasText(
-                RallyScreen.Accounts.name.uppercase()
-            ) and
-                    hasParent(
-                        hasContentDescription(RallyScreen.Accounts.name)
-                    ),
-            useUnmergedTree = true
-        )
-            .assertExists()
-    }
+//    @Test
+//    fun rallyTopAppBarTest() {
+//        val allScreens = RallyScreen.values().toList()
+//        composeTestRule.setContent {
+//            RallyTopAppBar(
+//                allScreens = allScreens,
+//                onTabSelected = { },
+//                currentScreen = RallyScreen.Accounts
+//            )
+//        }
+//        Thread.sleep(5000)
+//    }
+//
+//    @Test
+//    fun rallyTopAppBarTest_currentTabSelected() {
+//        val allScreens = RallyScreen.values().toList()
+//        composeTestRule.setContent {
+//            RallyTopAppBar(
+//                allScreens = allScreens,
+//                onTabSelected = { },
+//                currentScreen = RallyScreen.Accounts
+//            )
+//        }
+//
+//        composeTestRule
+//            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+//            .assertIsSelected()
+//    }
+//
+//    @Test
+//    fun rallyTopAppBarTest_currentLabelExists() {
+//        val allScreens = RallyScreen.values().toList()
+//        composeTestRule.setContent {
+//            RallyTopAppBar(
+//                allScreens = allScreens,
+//                onTabSelected = { },
+//                currentScreen = RallyScreen.Accounts
+//            )
+//        }
+//
+//        composeTestRule.onNode(
+//            hasText(
+//                RallyScreen.Accounts.name.uppercase()
+//            ) and
+//                    hasParent(
+//                        hasContentDescription(RallyScreen.Accounts.name)
+//                    ),
+//            useUnmergedTree = true
+//        )
+//            .assertExists()
+//    }
 
 }
