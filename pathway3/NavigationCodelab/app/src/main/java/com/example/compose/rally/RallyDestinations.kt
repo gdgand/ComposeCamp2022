45646ca4bd71b73ccfb663f302d5db0be10fc107
Animir <kagi1@naver.com>
2022-11-25 10:01:27 +0900

    [Navigation] Jetpack Compose 탐색
    - RellyDestinations에서 화면 컴포저블 추출

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -34,7 +34,7 @@ import com.example.compose.rally.ui.overview.OverviewScreen
 interface RallyDestination {
     val icon: ImageVector
     val route: String
-    val screen: @Composable () -> Unit
+//    val screen: @Composable () -> Unit
 }
 
 /**
@@ -43,19 +43,19 @@ interface RallyDestination {
 object Overview : RallyDestination {
     override val icon = Icons.Filled.PieChart
     override val route = "overview"
-    override val screen: @Composable () -> Unit = { OverviewScreen() }
+//    override val screen: @Composable () -> Unit = { OverviewScreen() }
 }
 
 object Accounts : RallyDestination {
     override val icon = Icons.Filled.AttachMoney
     override val route = "accounts"
-    override val screen: @Composable () -> Unit = { AccountsScreen() }
+//    override val screen: @Composable () -> Unit = { AccountsScreen() }
 }
 
 object Bills : RallyDestination {
     override val icon = Icons.Filled.MoneyOff
     override val route = "bills"
-    override val screen: @Composable () -> Unit = { BillsScreen() }
+//    override val screen: @Composable () -> Unit = { BillsScreen() }
 }
 
 object SingleAccount : RallyDestination {
@@ -63,7 +63,7 @@ object SingleAccount : RallyDestination {
     // part of the RallyTabRow selection
     override val icon = Icons.Filled.Money
     override val route = "single_account"
-    override val screen: @Composable () -> Unit = { SingleAccountScreen() }
+//    override val screen: @Composable () -> Unit = { SingleAccountScreen() }
     const val accountTypeArg = "account_type"
 }
 
