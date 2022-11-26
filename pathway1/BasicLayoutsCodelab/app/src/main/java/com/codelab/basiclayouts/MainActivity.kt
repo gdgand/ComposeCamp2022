00cb435c9a7d5ae187f2eb25d21eda45c1781388
sorea.no <sorae.no@thingsflow.com>
2022-11-26 14:46:56 +0900

    Feat: AlignYourBodyElement

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,9 +21,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
@@ -35,23 +45,56 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 // Step: Search bar - Modifiers
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
     TextField(
-        modifier = modifier,
-
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(stringResource(id = R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
     )
 }
 
 // Step: Align your body - Alignment
 @Composable
 fun AlignYourBodyElement(
-    modifier: Modifier = Modifier
-) {
-    // Implement composable here
+    modifier: Modifier = Modifier,
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+
+    ) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = drawable),
+            contentDescription = null,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = stringResource(id = text),
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
 }
 
 // Step: Favorite collection card - Material Surface
@@ -59,7 +102,7 @@ fun AlignYourBodyElement(
 fun FavoriteCollectionCard(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+
 }
 
 // Step: Align your body row - Arrangements
@@ -138,20 +181,23 @@ fun SearchBarPreview() {
 fun AlignYourBodyElementPreview() {
     MySootheTheme {
         AlignYourBodyElement(
-            modifier = Modifier.padding(8.dp)
+            text = R.string.ab1_inversions,
+            drawable = R.drawable.ab1_inversions,
+            modifier = Modifier.padding(8.dp),
         )
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
-@Composable
-fun FavoriteCollectionCardPreview() {
-    MySootheTheme {
-        FavoriteCollectionCard(
-            modifier = Modifier.padding(8.dp)
-        )
-    }
-}
+//@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+//@Composable
+//fun FavoriteCollectionCardPreview() {
+//    MySootheTheme {
+//        FavoriteCollectionCard(
+//            text =
+//            modifier = Modifier.padding(8.dp)
+//        )
+//    }
+//}
 
 @Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
---
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/memo.md
@@ -0,0 +1,38 @@
+## Memo
+동일한 결과를 얻는 방법에는 여러가지 방법이 있을 수 있다.
+가장 중요한 것은 디자인 및 Compose 가이드라인을 준수하여 구현하는 것
+
+
+- 텍스트의 기준선
+  - 텍스트의 기준선이란 문자가 '놓여' 있는 선을 가리킵니다. 디자이너들은 주로 상단이나 하단이 아닌 기준선을 기준으로 텍스트 요소를 정렬합니다.\
+  - paddingFromBaseline 으로 기준선 기준으로 패딩을 줄 수 있음
+
+- Content Description
+  - 순전히 장식용이므로 contentDescription을 null로 설정
+
+- 정렬방식
+  - 일반적으로, 상위 컨테이너 내부에서 컴포저블을 정렬하려면 상위 컨테이너의 alignment를 설정합니다. 
+  - 즉, 하위 요소에 상위 요소 배부에 배치되도록 지시하는 대신 상위 요소에 하위 요소를 정렬하는 방법을 지시합니다. 
+  - Column 
+    - Start 
+    - CenterHorizontally 
+    - End
+  - Row 
+    - Top 
+    - CenterVertically 
+    - Bottom
+  - Box 
+    - TopStart 
+    - TopCenter 
+    - TopEnd 
+    - CenterStart 
+    - Center 
+    - CenterEnd 
+    - BottomStart 
+    - BottomCenter 
+    - BottomEnd
+  - 컨테이너의 모든 하위 요소가 동일한 정렬 패턴을 따릅니다. 
+  - 하지만 align 수정자를 추가하여 단일 하위 요소의 동작을 재정의할 수 있습니다.
+
+
+컴포저블에 modifier 아규먼트를 줄 때 레이아웃은 위로 하위 컴포저블은 아래로 주는 것 같다.
