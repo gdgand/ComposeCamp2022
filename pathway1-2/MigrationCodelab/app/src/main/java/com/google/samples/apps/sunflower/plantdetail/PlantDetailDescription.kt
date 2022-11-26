eab5fca87cdeac06bb229d24316f738efb8bb209
cwpark <cwpark@bbros.kr>
2022-11-26 23:17:52 +0900

    Jetpack Compose로 이전
     - 추가 XML 코드 이전

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -26,8 +27,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
@@ -46,7 +51,12 @@ fun PlantDetailDescription(
 
 @Composable
 fun PlantDetailContent(plant: Plant) {
-    PlantName(name = plant.name)
+    Surface {
+        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_normal))) {
+            PlantName(name = plant.name)
+            PlantWatering(wateringInterval = plant.wateringInterval)
+        }
+    }
 }
 
 @Composable
@@ -61,11 +71,50 @@ fun PlantName(name: String) {
     )
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun PlantWatering(
+    modifier: Modifier = Modifier,
+    wateringInterval: Int
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(
+            text = stringResource(id = R.string.watering_needs_prefix),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primaryVariant, // colorAccent
+            modifier = Modifier
+                .padding(
+                    start = dimensionResource(id = R.dimen.margin_small),
+                    top = dimensionResource(id = R.dimen.margin_normal),
+                    end = dimensionResource(id = R.dimen.margin_small)
+                )
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = pluralStringResource(id = R.plurals.watering_needs_suffix, count = wateringInterval, wateringInterval), //@BindingAdapter("wateringText")
+            modifier = Modifier
+                .padding(
+                    start = dimensionResource(id = R.dimen.margin_small),
+                    end = dimensionResource(id = R.dimen.margin_small)
+                )
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
 @Preview
 @Composable
 fun PlantNamePreview() {
     MaterialTheme {
         PlantName(name = "Apple")
+    }
+}
+
+@Preview
+@Composable
+fun PlantWateringPreview() {
+    MaterialTheme {
+        PlantWatering(wateringInterval = 7)
     }
 }
 
