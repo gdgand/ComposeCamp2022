23508467159c1cc6b1da4b4bae3bcbf018009de1
jinhyungchoi <wlsgudchoi@gmail.com>
2022-11-26 22:40:12 +0900

    feat: Hello Compose

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,6 +21,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -108,6 +109,12 @@ class PlantDetailFragment : Fragment() {
             }
         }
         setHasOptionsMenu(true)
+
+        binding.composeView.setContent {
+            MaterialTheme {
+                PlantDetailDescription()
+            }
+        }
 
         return binding.root
     }
