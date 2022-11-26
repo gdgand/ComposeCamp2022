d0983921650e1c0fda6a109b83914185a3d09142
Kuni-Cho <jkoon07@gmail.com>
2022-11-26 15:51:50 +0900

    pathway 3 NavigationCodelab Done!!

---
+++ pathway3/NavigationCodelab/app/src/androidTest/java/example/compose/rally/NavigationTest.kt
@@ -0,0 +1,61 @@
+package example.compose.rally
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.compose.rally.RallyNavHost
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupRallyNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            RallyNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun rallyNavHost_verifyOverviewStartDestination() {
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyNavHost_clickAllAccount_navigatesToAccounts() {
+        composeTestRule
+            .onNodeWithContentDescription("All Accounts")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
+            .assertIsDisplayed()
+
+    }
+
+    @Test
+    fun rallyNavHost_clickAllBills_navigateToBills() {
+        composeTestRule.onNodeWithContentDescription("All Bills")
+            .performScrollTo() //아래 스크롤하지 않는 경우 See All 버튼을 찾을 수 없어 테스트 실패
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "bills")
+    }
+}
