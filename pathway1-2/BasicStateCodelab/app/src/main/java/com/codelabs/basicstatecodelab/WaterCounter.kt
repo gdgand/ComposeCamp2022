7a73773883643ef4c9f2a1502d4a4d591ba64973
tony.origogi <tony.origogi@kakaobrain.com>
2022-11-26 11:28:01 +0900

    Add WellnessTaskItem

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -16,15 +17,30 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        Text(
-            "You have $count glasses",
-        )
-        Button(
-            onClick = { count += 1 },
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text(text = "Add one")
+        if (count > 0) {
+            var showTask by remember {
+                mutableStateOf(true)
+            }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false })
+            }
+            Text(
+                "You have $count glasses",
+            )
+        }
 
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count += 1 },
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
