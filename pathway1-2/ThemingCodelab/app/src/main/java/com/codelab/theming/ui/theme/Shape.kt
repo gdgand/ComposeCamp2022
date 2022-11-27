45e5ae5df3800326157b2cb6d28133bc322be25a
jeongsanghyeon <dev.jusa@gmail.com>
2022-11-27 19:23:49 +0900

    1-2-2 완료

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/theme/Shape.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/theme/Shape.kt
@@ -14,20 +14,15 @@
  * limitations under the License.
  */
 
-package com.codelab.theming.ui.start
+package com.codelab.theming.ui.theme
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            Surface {
-                Home()
-            }
-        }
-    }
-}
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
