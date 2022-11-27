8f47bd50b60c239c230219d4915479aff2d249c7
Hooooong <h921013@gmail.com>
2022-11-27 15:43:08 +0900

    heungki:pathway1-2/BasicStateCodelab/ - 구성 가능한 함수의 메모리

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,14 +39,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WaterCount(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
 
-        Text(
-            text = "you've had $count glasses"
-        )
-
+        Text(text = "you've had ${count.value} glasses")
         Button(
-            onClick = { count++ },
+            onClick = { count.value++ },
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text(text = "Add one.")
