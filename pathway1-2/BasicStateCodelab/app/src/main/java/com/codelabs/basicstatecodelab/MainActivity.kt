1b0966f225523e3bf89bef3e1b279fcacd00e18f
조엘_전국진 <joel@afreecatv.com>
2022-11-27 09:25:25 +0900

    Jetpack Compose 상태  1~6

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -3,13 +3,21 @@ package com.codelabs.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
@@ -34,10 +42,35 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+//    var count by MutableState = remember { MutableState(0) }
+    var count by remember { mutableStateOf(0) }
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count != 0) {
+            Text(
+                text = "You've had $count glasses."
+            )
+        }
+        Button(
+            { count++ },
+            enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
+    }
+
+}
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     BasicStateCodelabTheme {
-        Greeting("Android")
+        WellnessScreen()
     }
 }
