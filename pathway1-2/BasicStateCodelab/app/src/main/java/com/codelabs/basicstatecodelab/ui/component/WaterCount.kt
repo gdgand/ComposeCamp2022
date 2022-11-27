bdd7d5ac23b400c8855374c137dce2348545a54d
yoonsangkwak <yunsang08@naver.com>
2022-11-27 13:25:11 +0900

    [ pathway 1-2 ] : State hoisting

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/component/WaterCount.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/component/WaterCount.kt
@@ -26,3 +26,29 @@ fun WaterCount(modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+        Button(
+            onClick = onIncrement,
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier)
+}
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/screen/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/screen/WellnessScreen.kt
@@ -2,9 +2,9 @@ package com.codelabs.basicstatecodelab.ui.screen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.codelabs.basicstatecodelab.ui.component.WaterCount
+import com.codelabs.basicstatecodelab.ui.component.StatefulCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCount(modifier)
+    StatefulCounter(modifier)
 }
