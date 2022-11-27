423e71e5e80b99ff4ed5990d20629bf467dd6983
YJM <jmyoon4488@gmail.com>
2022-11-27 16:47:52 +0900

    [Pathway 3] 아키텍처 및 상태 - 고급 상태 및 부수 효과

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
@@ -24,9 +24,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.samples.crane.ui.captionTextStyle
 import androidx.compose.ui.graphics.SolidColor
+
+class EditableUserInputState(private val hint: String, initialText: String) {
+
+    var text by mutableStateOf(initialText)
+
+    val isHint: Boolean
+        get() = text == hint
+
+    companion object {
+        val Saver: Saver<EditableUserInputState, *> = listSaver(
+            save = { listOf(it.hint, it.text) },
+            restore = {
+                EditableUserInputState(
+                    hint = it[0],
+                    initialText = it[1],
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberEditableUserInputState(hint: String): EditableUserInputState =
+//    remember(hint) {
+//        EditableUserInputState(hint, hint)
+//    }
+    rememberSaveable(hint, saver = EditableUserInputState.Saver) {
+        EditableUserInputState(hint, hint)
+    }
 
 @Composable
 fun CraneEditableUserInput(
@@ -52,6 +84,32 @@ fun CraneEditableUserInput(
                 if (!isHint()) onInputChanged(textState)
             },
             textStyle = if (isHint()) {
+                captionTextStyle.copy(color = LocalContentColor.current)
+            } else {
+                MaterialTheme.typography.body1.copy(color = LocalContentColor.current)
+            },
+            cursorBrush = SolidColor(LocalContentColor.current)
+        )
+    }
+}
+
+@Composable
+fun CraneEditableUserInput2(
+    state: EditableUserInputState = rememberEditableUserInputState(""),
+    caption: String? = null,
+    @DrawableRes vectorImageId: Int? = null,
+//    onInputChanged: (String) -> Unit
+) {
+    CraneBaseUserInput(
+        caption = caption,
+        tintIcon = { !state.isHint },
+        showCaption = { !state.isHint },
+        vectorImageId = vectorImageId
+    ) {
+        BasicTextField(
+            value = state.text,
+            onValueChange = { state.text = it },
+            textStyle = if (state.isHint) {
                 captionTextStyle.copy(color = LocalContentColor.current)
             } else {
                 MaterialTheme.typography.body1.copy(color = LocalContentColor.current)
--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -19,11 +19,14 @@ package androidx.compose.samples.crane.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
@@ -31,6 +34,27 @@ private const val SplashWaitTime: Long = 2000
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         // TODO Codelab: LaunchedEffect and rememberUpdatedState step
+        // Start a side effect to load things in the background
+        // and call onTimeout() when finished.
+        // Passing onTimeout as a parameter to LaunchedEffect
+        // is wrong! Don't do this. We'll improve this code in a sec.
+//        LaunchedEffect(onTimeout) {
+//            delay(SplashWaitTime) // Simulates loading things
+//            onTimeout()
+//        }
+
+        // This will always refer to the latest onTimeout function that
+        // LandingScreen was recomposed with
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // Create an effect that matches the lifecycle of LandingScreen.
+        // If LandingScreen recomposes or onTimeout changes,
+        // the delay shouldn't start again.
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
+
         // TODO: Make LandingScreen disappear after loading data
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
