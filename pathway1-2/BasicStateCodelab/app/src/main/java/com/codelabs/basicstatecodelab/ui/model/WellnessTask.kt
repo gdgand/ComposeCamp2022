4a76ccb679fa4dd1b55c119cf47e5019fb315fc7
yoonsangkwak <yunsang08@naver.com>
2022-11-27 13:36:48 +0900

    [ pathway 1-2 ] : Work with lists

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/model/WellnessTask.kt
@@ -0,0 +1,6 @@
+package com.codelabs.basicstatecodelab.ui.model
+
+data class WellnessTask(
+    val id: Int,
+    val label: String
+)
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/screen/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/screen/WellnessScreen.kt
@@ -1,10 +1,15 @@
 package com.codelabs.basicstatecodelab.ui.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.codelabs.basicstatecodelab.ui.component.StatefulCounter
+import com.codelabs.basicstatecodelab.ui.component.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
