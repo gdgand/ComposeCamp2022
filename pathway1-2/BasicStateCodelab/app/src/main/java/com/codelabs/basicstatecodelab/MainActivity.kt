e304fd37fadd6affd68e2d8030e82fcc5f69b0ed
Hooooong <h921013@gmail.com>
2022-11-27 15:39:26 +0900

    heungki:pathway1-2/BasicStateCodelab/ - Compose의 이벤트

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -3,8 +3,10 @@ package com.codelabs.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -33,11 +35,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WaterCount(modifier: Modifier = Modifier) {
-    var count = 0
-    Text(
-        text = "you've had $count glasses",
-        modifier = modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+
+        Text(
+            text = "you've had $count glasses"
+        )
+
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one.")
+        }
+    }
 }
 
 @Composable
