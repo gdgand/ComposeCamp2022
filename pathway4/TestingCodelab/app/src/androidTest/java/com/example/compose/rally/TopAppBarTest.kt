072e03cf31d72bcf77487f6838a8e51492fc1156
jaeryo2357 <jaeryo2357@naver.com>
2022-11-27 21:14:13 +0900

    Optional exercise

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -6,7 +6,9 @@ import androidx.compose.ui.test.hasParent
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
@@ -43,13 +45,31 @@ class TopAppBarTest {
             )
         }
 
-        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
-
         composeTestRule
             .onNode(
                 hasText(RallyScreen.Accounts.name.uppercase()) and
                         hasParent(
                             hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_changedLabel_whenClick() {
+        composeTestRule.setContent {
+            RallyApp()
+        }
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Bills.name)
+            .performClick()
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Bills.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Bills.name)
                         ),
                 useUnmergedTree = true
             )
