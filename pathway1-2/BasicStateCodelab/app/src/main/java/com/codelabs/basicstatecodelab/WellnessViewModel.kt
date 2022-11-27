828688f11b099dc50b4ee5b551902ea58d9fba7f
nari4169 <6k2emg@gmail.com>
2022-11-27 20:49:39 +0900

    mission completed

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessViewModel.kt
@@ -0,0 +1,27 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel : ViewModel() {
+    /**
+     * Don't expose the mutable list of tasks from outside the ViewModel.
+     * Instead define _tasks and tasks. _tasks is internal and mutable inside the ViewModel.
+     * tasks is public and read-only.
+     */
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) =
+        tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
