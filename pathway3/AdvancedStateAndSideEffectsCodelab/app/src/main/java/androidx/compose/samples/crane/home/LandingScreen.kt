6f663114c52538562e2b13b770932604be398cc9
dauni6 <45280927+dauni6@users.noreply.github.com>
2022-11-27 19:21:00 +0900

    pathway3 advancedState
    1. ViewModel에서 data의 stream을 emit할 때마다 컴포즈가 상태 변경을 알아차릴 수 있도록 collectAsState()를 사용한다.
    
    2. LaunchedEffect가 컴포지션을 종료하면 코루틴이 취소된다.
    
    3. snapshotFlow를 사용하여 Compose State<T> 객체를 Flow로 변환할 수 있다.
    
    4. DisposableEffect를 통해 키가 변경되거나 Composable이 Composition을 종료하면 SideEffect가 종료되도록 한다. 따라서 lifecycle에 추가하여 현재의 수명주기가 변경되거나 해당 Composable이 Composition을 종료할 때마다 삭제된다.
    
    5. derivedStateOf는 다른 State에서 파생된 Compose State를 원하는 경우에 사용한다. 이 함수를 사용하면 계산에서 사용되는 상태 중 하나가 변경될 때만 계산이 실행된다.

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,18 +20,34 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
-fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
+fun LandingScreen(
+    modifier: Modifier = Modifier,
+    onTimeout: () -> Unit // 랜딩 화면 표시/숨기기를 위해 상태를 변경하는 콜백
+) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         // TODO Codelab: LaunchedEffect and rememberUpdatedState step
         // TODO: Make LandingScreen disappear after loading data
+
+        // This will always refer to the latest onTimeout function that
+        // LandingScreen was recomposed with
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+        
+        LaunchedEffect(true) { // LaunchedEffect가 컴포지션을 종료하면 코루틴이 취소된다.
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
