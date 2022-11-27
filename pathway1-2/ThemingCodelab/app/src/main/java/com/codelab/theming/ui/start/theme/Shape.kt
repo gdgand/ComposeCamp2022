16064c9016ae588c183da317c0346873ebc44519
oykwon <oykwon@rsupport.com>
2022-11-27 18:05:22 +0900

    compose theme complete

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
@@ -0,0 +1,16 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by oykwon on 11/27/22.
+ */
+
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
