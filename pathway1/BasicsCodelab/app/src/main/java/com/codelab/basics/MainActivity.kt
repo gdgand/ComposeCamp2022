5c8906f84da59c1d86e17dce418eee4a4c2e6cac
polzak <polzak.yes@gmail.com>
2022-11-28 13:07:14 +0900

    07 Compose에서의 상태

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,6 +42,10 @@ private fun MyApp(modifier: Modifier = Modifier,
 
 @Composable
 private fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
@@ -50,16 +56,16 @@ private fun Greeting(name: String) {
                 .fillMaxWidth()
         ) {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.weight(1f).padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
 
             ElevatedButton(
-                onClick = { /*TODO*/ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
