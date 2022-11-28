d7ed66ff69055b6bed17057a9ca46527e2bdbc5a
leesh96 <suho2718@gmail.com>
2022-11-28 19:46:25 +0900

    [Pathway1-2] ~6. State driven UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -3,13 +3,14 @@ package com.codelabs.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,20 +35,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
-    Button(
-        onClick = { count++ },
-        modifier = Modifier.padding(top = 8.dp)
-    ) {
-        Text(text = "Add one")
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
     }
 }
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicStateCodelabTheme {
+        WellnessScreen()
+    }
 }
