bd11d5c8284e1a519193aef6ae467409d642b8c1
EaseTheWorld <easetheworld@gmail.com>
2022-11-28 22:35:45 +0900

    [Pathway1-2] 6 Working with text

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.theming.R
@@ -111,7 +112,8 @@ fun Header(
             modifier = Modifier
                 .fillMaxWidth()
                 .semantics { heading() }
-                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            style = MaterialTheme.typography.subtitle2
         )
     }
 }
@@ -140,11 +142,13 @@ fun FeaturedPost(
             val padding = Modifier.padding(horizontal = 16.dp)
             Text(
                 text = post.title,
-                modifier = padding
+                modifier = padding,
+                style = MaterialTheme.typography.h6
             )
             Text(
                 text = post.metadata.author.name,
-                modifier = padding
+                modifier = padding,
+                style = MaterialTheme.typography.body2
             )
             PostMetadata(post, padding)
             Spacer(Modifier.height(16.dp))
@@ -159,6 +163,9 @@ private fun PostMetadata(
 ) {
     val divider = "  •  "
     val tagDivider = "  "
+    val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+        background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+    )
     val text = buildAnnotatedString {
         append(post.metadata.date)
         append(divider)
@@ -168,7 +175,9 @@ private fun PostMetadata(
             if (index != 0) {
                 append(tagDivider)
             }
-            append(" ${tag.uppercase(Locale.getDefault())} ")
+            withStyle(tagStyle) {
+                append(" ${tag.uppercase(Locale.getDefault())} ")
+            }
         }
     }
     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
