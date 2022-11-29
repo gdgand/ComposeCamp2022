64e1811ab730553fceb9a43a3674019ac20e2f73
rosf73 <alswlzhsclq@naver.com>
2022-11-29 16:13:26 +0900

    AnimatedVisibility offset 변경 - Default 는 절반의 높이만 사용

--- pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -16,9 +16,7 @@
 
 package com.example.android.codelab.animation.ui.home
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.splineBasedDecay
+import androidx.compose.animation.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -289,10 +287,14 @@ private fun HomeFloatingActionButton(
  */
 @Composable
 private fun EditMessage(shown: Boolean) {
-    // TODO 2-2: The message should slide down from the top on appearance and slide up on
-    //           disappearance.
     AnimatedVisibility(
-        visible = shown
+        visible = shown,
+        enter = slideInVertically(
+            initialOffsetY = { fullHeight -> -fullHeight }
+        ),
+        exit = slideOutVertically(
+            targetOffsetY = { fullHeight -> -fullHeight }
+        )
     ) {
         Surface(
             modifier = Modifier.fillMaxWidth(),
