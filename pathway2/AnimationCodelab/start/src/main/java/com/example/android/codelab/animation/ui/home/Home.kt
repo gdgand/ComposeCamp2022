a381022cf5b2e1efb79d53dca0acf9546d2e97df
rosf73 <alswlzhsclq@naver.com>
2022-11-29 16:05:32 +0900

    지정된 Boolean 값의 변경에 따라 실행되는 애니메이션 - AnimatedVisibility 이용

--- pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -273,8 +273,7 @@ private fun HomeFloatingActionButton(
                 contentDescription = null
             )
             // Toggle the visibility of the content with animation.
-            // TODO 2-1: Animate this visibility change.
-            if (extended) {
+            AnimatedVisibility(extended) {
                 Text(
                     text = stringResource(R.string.edit),
                     modifier = Modifier
