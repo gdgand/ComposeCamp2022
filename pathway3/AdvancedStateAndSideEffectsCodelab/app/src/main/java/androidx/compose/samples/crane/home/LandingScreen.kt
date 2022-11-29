d444205fcbaa85165af1173e7c6325e60ebf8066
amy <essim@wemade.com>
2022-11-30 08:55:32 +0900

    [Jetpack Compose의 고급 상태 및 부작용] LaunchedEffect 및 rememberUpdatedState

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,10 +20,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
@@ -31,7 +35,19 @@ private const val SplashWaitTime: Long = 2000
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         // TODO Codelab: LaunchedEffect and rememberUpdatedState step
+        // This will always refer to the latest onTimeout function that
+        // LandingScreen was recomposed with
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
         // TODO: Make LandingScreen disappear after loading data
+        // Create an effect that matches the lifecycle of LandingScreen.
+        // If LandingScreen recomposes or onTimeout changes,
+        // the delay shouldn't start again.
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
+
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
