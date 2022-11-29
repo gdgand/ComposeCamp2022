a76b5e1e1a31aa02428628d667d7f38f2fb07be1
aurora32s <aurora32s@naver.com>
2022-11-29 23:52:00 +0900

    [Ch.4] Debugging tests

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -2,9 +2,13 @@ package com.example.compose.rally
 
 import androidx.compose.material.Text
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
 import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,8 +16,8 @@ class TopAppBarTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun myTest() {
+    @Before
+    fun setupRallyCompose() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
@@ -22,8 +26,19 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
+    }
+
+    @Test
+    fun myTest() {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists() // Still fail
     }
 }
