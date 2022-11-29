b857689936baa6a0821a4f469fd5c157d634cfde
kan5016@naver.com <kan5016@naver.com>
2022-11-29 13:37:53 +0900

    feat : Compose SideEffect

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,18 +20,28 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+        // LaunchedEffect 가 변하는동안 onTimeout 이 변경되었을 때 해당 값으로 LaunchedEffect 실행을 보장을 위해
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+        // Effect 가 LandingScreen 의 Lifecycle 을 따라가도록 상수를 key로 넣음
+        LaunchedEffect(true) {
+            delay(SplashWaitTime) // fake loading
+            onTimeout()
+        }
+
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
