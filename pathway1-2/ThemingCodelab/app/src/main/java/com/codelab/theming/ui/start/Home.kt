88d78ec386a268fd7c470f0903787dad75e1cf94
jay <nix@last30min.com>
2022-11-29 15:14:50 +0900

    7. 도형 사용

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -36,6 +36,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -202,6 +203,7 @@ fun PostItem(
             .padding(vertical = 8.dp),
         icon = {
             Image(
+                modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
                 painter = painterResource(post.imageThumbId),
                 contentDescription = null
             )
