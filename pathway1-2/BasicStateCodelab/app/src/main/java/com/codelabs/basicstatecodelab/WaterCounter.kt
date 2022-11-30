ce942059c404019c55dcc9fce13853fd5d355ebd
Chanbong Park <siprikorea@gmail.com>
2022-11-30 14:45:58 +0900

    Remember in Composition

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -14,16 +14,14 @@ import com.codelabs.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 fun WaterCounter(
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.padding(16.dp)
-    ) {
+    Column(modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        if (count > 0) {
-            Text("You've had $count glasses.")
+        if (count > 1) {
+            Text("You've $count glasses.")
         }
         Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
+            { count++ },
+            Modifier.padding(top = 8.dp),
             enabled = count < 10
         ) {
             Text("Add one")
