019c3d7e2ba96200c35869058a5cd7bea2a27f39
JaesungLeee <viki0114@naver.com>
2022-11-30 15:16:18 +0900

    [BasicStateCodelab] 5. Composition, Recomposition

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +21,12 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Log.e("WC", "Recomposition!")
 
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+//        val count = mutableStateOf(0)  // Recomposition이 발생할 때마다 0으로 초기화됨
+
+        // remember를 통해 recomposition이 발생하더라도 이전 상태를 기억할 수 있음
+        var count by remember {
+            mutableStateOf(0)
+        }
 
         Text("You've had $count glasses")
         Button(
