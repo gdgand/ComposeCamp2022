14ead3cc268d490a9d1b48cf872822ed5831f4dd
oreocube <oreocube13@gmail.com>
2022-11-30 13:51:28 +0900

    Themeing Codelab : Color.kt 추가

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 색을 정의할 때, primary 같은 이름보다 Red500 같은 이름으로 지어라
+// 다크모드 등에서 또다른 색깔이 primary 일 수 있음
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
