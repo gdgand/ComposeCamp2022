ef8eda73bdd067aa9cbac251781205b709d60d48
JaesungLeee <viki0114@naver.com>
2022-11-30 16:01:22 +0900

    [BasicStateCodelab] 9. 상태 호이스팅 * Composable을 Stateful하게 만드는 것보다 Stateless하게 만드는 것이 재사용성과 테스트 용이 측면에서 효율적이다.

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import java.util.concurrent.CountedCompleter
 
 /**
  * BasicStateCodelab
@@ -60,6 +61,72 @@ import androidx.compose.ui.unit.dp
 //        }
 //    }
 //}
+
+/**
+ * 9. 상태 호이스팅
+ * remember 또는 rememberSaveable를 통해 상태를 스테이트풀하게 만들 수 있다.
+ * 스테이풀한 컴포저블은 재사용 가능성이 적고 테스트 복잡성의 문제가 있다.
+ *
+ * 상태 호이스팅을 통해 스테이트리스하게 만들어야 한다.
+ * 컴포저블 내부에 상태관련 변수를 만드는 것이 아닌 컴포저블의 매개변수로 넣는다.
+ * 상태 호이스팅은 UDF를 따른다. (상태는 아래로 이벤트는 위로)
+ *
+ * 호이스팅한 상태는 공유할 수 있기 때문에 불필요한 리컴포지션을 방지하고 재사용성을 높이려면 컴포저블에 필요한 상태만 전달해야 한다.
+ */
+
+/**
+ * StatelessCounter
+ * 상태에 해당하는 count를 컴포저블 내부에 보유하고 있지 않음
+ * 단순히 count를 표시하며 count를 늘리는 이벤트에 해당하는 onIncrement를 전달하여 UDF 패턴을 따름
+ *
+ * ```kotlin
+ * @Composable
+ * fun StatefulCounter() {
+ *     var waterCount by remember { mutableStateOf(0) }
+ *     var juiceCount by remember { mutableStateOf(0) }
+ *
+ *     StatelessCounter(waterCount, { waterCount++ })
+ *     StatelessCounter(juiceCount, { juiceCount++ })
+ * }
+ * ```
+ *
+ * 이렇게 쓰면 사용자 이벤트에 의한 리컴포지션을 독립적으로 실행할 수 있다.
+ * waterCount가 증가하는 리컴포지션이 있어도 juiceCount는 증가하지 않는다.
+ *
+ */
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+
+        if (count > 0) {
+            Text("You've had $count glasses")
+        }
+        Button(onClick = onIncrement, modifier = Modifier.padding(top = 8.dp)) {
+            Text("Add One")
+        }
+    }
+}
+
+/**
+ * StatefulCounter
+ * 상태에 해당하는 count를 컴포저블 내부에서 보유함
+ */
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses")
+        }
+        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)) {
+            Text("Add One")
+        }
+    }
+}
+
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -13,5 +13,5 @@ import androidx.compose.ui.Modifier
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     Log.e("WS", "Recomposition!")
-    WaterCounter(modifier = modifier)
+    StatefulCounter(modifier = modifier)
 }
