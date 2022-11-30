c32a124e480bc6e992f1af55f67ecebf92e25587
jisang <jisangman89@gmail.com>
2022-11-30 23:26:59 +0900

    Update : ViewModel 및 LiveData

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -60,7 +60,7 @@ class PlantDetailFragment : Fragment() {
             composeView.setContent {
                 // You're in Compose world!
                 MaterialTheme {
-                    PlantDetailDescription()
+                    PlantDetailDescription(plantDetailViewModel)
                 }
             }
             viewModel = plantDetailViewModel
