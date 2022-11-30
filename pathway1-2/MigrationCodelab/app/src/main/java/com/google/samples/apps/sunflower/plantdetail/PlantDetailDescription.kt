2e699b13140dac8cbfcf385f3a33dd03e8e123eb
sikbongda <jys1016@gmail.com>
2022-11-30 22:22:00 +0900

    Use LiveData and ViewModel with compose.

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -23,11 +23,29 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
+
+@Composable
+fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
+    val plant: Plant? by plantDetailViewModel.plant.observeAsState()
+
+    plant?.let {
+        PlantDetailContent(it)
+    }
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant) {
+    PlantName(plant.name)
+}
 
 @Composable
 private fun PlantName(name: String) {
@@ -41,17 +59,19 @@ private fun PlantName(name: String) {
     )
 }
 
-@Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
-    }
-}
-
 @Preview
 @Composable
 private fun PlantNamePreview() {
     MaterialTheme {
         PlantName("Apple")
+    }
+}
+
+@Preview
+@Composable
+private fun PlantDetailContentPreview() {
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MaterialTheme {
+        PlantDetailContent(plant)
     }
 }
