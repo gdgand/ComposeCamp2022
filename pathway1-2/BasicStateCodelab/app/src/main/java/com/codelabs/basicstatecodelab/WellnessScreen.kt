c8491f6f85cf3e2a89f6b04949c5581e0bbcabab
CHAEWON NEW DESKTOP <kch9870@naver.com>
2022-11-30 18:30:13 +0900

    State of Jetpack Compose Compose State, Compose Event, 구성 가능한 함수의 메모리

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,12 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
