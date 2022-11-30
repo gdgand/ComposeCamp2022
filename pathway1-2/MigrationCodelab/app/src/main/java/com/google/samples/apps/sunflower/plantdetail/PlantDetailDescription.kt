1f367818403e352abd32c4e0951c70870419dfd3
sikbongda <jys1016@gmail.com>
2022-11-30 22:46:02 +0900

    Use MdcTheme

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -65,14 +66,6 @@ private fun PlantDescription(description: String) {
             it.text = htmlDescription
         }
     )
-}
-
-@Preview
-@Composable
-private fun PlantDescriptionPreview() {
-    MaterialTheme {
-        PlantDescription("HTML<br><br>description")
-    }
 }
 
 @Composable
@@ -128,7 +121,7 @@ fun PlantDetailContent(plant: Plant) {
 @Preview
 @Composable
 private fun PlantWateringPreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantWatering(7)
     }
 }
@@ -136,7 +129,7 @@ private fun PlantWateringPreview() {
 @Preview
 @Composable
 private fun PlantNamePreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantName("Apple")
     }
 }
@@ -145,7 +138,15 @@ private fun PlantNamePreview() {
 @Composable
 private fun PlantDetailContentPreview() {
     val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
-    MaterialTheme {
+    MdcTheme {
         PlantDetailContent(plant)
+    }
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+    MdcTheme {
+        PlantDescription("HTML<br><br>description")
     }
 }
