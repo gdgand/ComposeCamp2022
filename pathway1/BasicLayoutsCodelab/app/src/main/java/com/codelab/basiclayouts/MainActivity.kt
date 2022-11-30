8b917226ff3615452caa3020199bc558870ff8e9
jung eunjung <agjjung@avatye.com>
2022-11-30 17:26:17 +0900

    Pathway1 - BasicLayoutsCodelab 11. Bottom Navigation

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -47,6 +47,8 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Spa
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
@@ -219,7 +221,31 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 // Step: Bottom navigation - Material
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
-    // Implement composable here
+    BottomNavigation(
+        modifier,
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        BottomNavigationItem(
+            icon = {
+                   Icon(imageVector = Icons.Default.Spa, contentDescription = null)
+            },
+            label = {
+                    Text(text = stringResource(id = R.string.bottom_navigation_home))
+            },
+            selected = true,
+            onClick = {  }
+        )
+        BottomNavigationItem(
+            icon = {
+                   Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+            },
+            label = {
+                    Text(text = stringResource(id = R.string.bottom_navigation_profile))
+            },
+            selected = false,
+            onClick = {  }
+        )
+    }
 }
 
 // Step: MySoothe App - Scaffold
