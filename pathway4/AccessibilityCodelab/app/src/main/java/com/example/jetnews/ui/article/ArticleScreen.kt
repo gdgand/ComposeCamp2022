b30ae158673301f6e28672efd37926403c68c16c
yc.park <yc.park@deepfine.ai>
2022-11-30 09:14:13 +0900

    feat($compose): Visual element descriptions

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/ArticleScreen.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/ArticleScreen.kt
@@ -35,8 +35,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnews.R
 import com.example.jetnews.data.posts.PostsRepository
 import com.example.jetnews.data.posts.impl.post3
 import com.example.jetnews.model.Post
@@ -54,16 +56,16 @@ import com.example.jetnews.utils.supportWideScreen
 @Suppress("DEPRECATION") // allow ViewModelLifecycleScope call
 @Composable
 fun ArticleScreen(
-    postId: String?,
-    postsRepository: PostsRepository,
-    onBack: () -> Unit
+  postId: String?,
+  postsRepository: PostsRepository,
+  onBack: () -> Unit
 ) {
-    val postData = postsRepository.getPost(postId)!!
+  val postData = postsRepository.getPost(postId)!!
 
-    ArticleScreen(
-        post = postData,
-        onBack = onBack
-    )
+  ArticleScreen(
+    post = postData,
+    onBack = onBack
+  )
 }
 
 /**
@@ -74,45 +76,45 @@ fun ArticleScreen(
  */
 @Composable
 fun ArticleScreen(
-    post: Post,
-    onBack: () -> Unit
+  post: Post,
+  onBack: () -> Unit
 ) {
 
-    var showDialog by rememberSaveable { mutableStateOf(false) }
-    if (showDialog) {
-        FunctionalityNotAvailablePopup { showDialog = false }
-    }
+  var showDialog by rememberSaveable { mutableStateOf(false) }
+  if (showDialog) {
+    FunctionalityNotAvailablePopup { showDialog = false }
+  }
 
-    Scaffold(
-        topBar = {
-            InsetAwareTopAppBar(
-                title = {
-                    Text(
-                        text = "Published in: ${post.publication?.name}",
-                        style = MaterialTheme.typography.subtitle2,
-                        color = LocalContentColor.current
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = onBack) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = null
-                        )
-                    }
-                }
+  Scaffold(
+    topBar = {
+      InsetAwareTopAppBar(
+        title = {
+          Text(
+            text = "Published in: ${post.publication?.name}",
+            style = MaterialTheme.typography.subtitle2,
+            color = LocalContentColor.current
+          )
+        },
+        navigationIcon = {
+          IconButton(onClick = onBack) {
+            Icon(
+              imageVector = Icons.Filled.ArrowBack,
+              contentDescription = stringResource(id = R.string.cd_navigate_up)
             )
+          }
         }
-    ) { innerPadding ->
-        PostContent(
-            post = post,
-            modifier = Modifier
-                // innerPadding takes into account the top and bottom bar
-                .padding(innerPadding)
-                // center content in landscape mode
-                .supportWideScreen()
-        )
+      )
     }
+  ) { innerPadding ->
+    PostContent(
+      post = post,
+      modifier = Modifier
+        // innerPadding takes into account the top and bottom bar
+        .padding(innerPadding)
+        // center content in landscape mode
+        .supportWideScreen()
+    )
+  }
 }
 
 /**
@@ -122,20 +124,20 @@ fun ArticleScreen(
  */
 @Composable
 private fun FunctionalityNotAvailablePopup(onDismiss: () -> Unit) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        text = {
-            Text(
-                text = "Functionality not available \uD83D\uDE48",
-                style = MaterialTheme.typography.body2
-            )
-        },
-        confirmButton = {
-            TextButton(onClick = onDismiss) {
-                Text(text = "CLOSE")
-            }
-        }
-    )
+  AlertDialog(
+    onDismissRequest = onDismiss,
+    text = {
+      Text(
+        text = "Functionality not available \uD83D\uDE48",
+        style = MaterialTheme.typography.body2
+      )
+    },
+    confirmButton = {
+      TextButton(onClick = onDismiss) {
+        Text(text = "CLOSE")
+      }
+    }
+  )
 }
 
 @Preview("Article screen")
@@ -144,7 +146,7 @@ private fun FunctionalityNotAvailablePopup(onDismiss: () -> Unit) {
 @Preview("Article screen (large screen)", device = Devices.PIXEL_C)
 @Composable
 fun PreviewArticle() {
-    JetnewsTheme {
-        ArticleScreen(PostsRepository().getPost(post3.id)!!, {})
-    }
+  JetnewsTheme {
+    ArticleScreen(PostsRepository().getPost(post3.id)!!, {})
+  }
 }
