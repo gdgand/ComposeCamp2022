9ad02b82b6c6099c4d10fb2972f3cbbec41435eb
junyoungleee <jun991203@naver.com>
2022-12-01 22:15:49 +0900

    ✅ [Pathway1-2] ThemingCodelab 완료

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * 'on' 색상 : 이름이 지정도니 색상 중 하나 위에 있는 컨텐츠에 사용
+ *          -> '표면' 색상 컨테이너의 텍스트의 컬러 : on surface 색상상 */
+
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
