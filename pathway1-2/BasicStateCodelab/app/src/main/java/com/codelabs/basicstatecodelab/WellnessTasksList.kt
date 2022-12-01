914fc672e0aeeb96ea47f795201ebb0906a14d73
JieunAn <jieun.an@waycen.com>
2022-12-01 18:27:21 +0900

    [Pathway 1-2] State in Jetpack Compose

---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,24 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit
+) {
+    LazyColumn(modifier = modifier) {
+        items(list) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) })
+        }
+    }
+}
