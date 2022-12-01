f6e3ed275a1da57316b9d920e639fa0eea3cf797
jung eunjung <agjjung@avatye.com>
2022-12-01 10:42:51 +0900

    Pathway1-2 - BasicStateCodelab 6

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -14,14 +14,17 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)){
         var count by remember { mutableStateOf(0) }
 
-        Text(
-            text = "You've had $count glasses",
-        )
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
         Button(
             onClick = { count++ },
-            Modifier.padding(top = 8.dp)
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
-            Text(text = "Add one")
+            Text("Add one")
         }
     }
 }
+
+
