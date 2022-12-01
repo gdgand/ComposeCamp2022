c9f6246e64fff6d0be9a588cb35fd046a92c8024
hyuna <hyuna9787@gmail.com>
2022-12-02 00:32:16 +0900

    Apply MdcTheme

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,7 +21,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -30,6 +29,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.samples.apps.sunflower.R
@@ -70,7 +70,7 @@ class PlantDetailFragment : Fragment() {
                 }
             }
             composeView.setContent {
-                MaterialTheme {
+                MdcTheme {
                     PlantDetailDescription(plantDetailViewModel)
                 }
             }
