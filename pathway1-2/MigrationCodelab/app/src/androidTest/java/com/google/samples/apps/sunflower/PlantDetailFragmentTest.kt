08137b224f6c08613fed3762a8a08fb9a5f762ad
jisang <jisangman89@gmail.com>
2022-12-01 15:11:57 +0900

    Update : 테스트

--- pathway1-2/MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
+++ pathway1-2/MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/PlantDetailFragmentTest.kt
@@ -20,6 +20,9 @@ import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.Navigation.findNavController
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -50,6 +53,10 @@ class PlantDetailFragmentTest {
 
     @Rule
     @JvmField
+    val composeTestRule = createAndroidComposeRule<GardenActivity>()
+
+    @Rule
+    @JvmField
     val activityTestRule = ActivityScenarioRule(GardenActivity::class.java)
 
     // Note that keeping these references is only safe if the activity is not recreated.
@@ -59,18 +66,25 @@ class PlantDetailFragmentTest {
     fun jumpToPlantDetailFragment() {
         populateDatabase()
 
-        activityTestRule.scenario.onActivity { gardenActivity ->
+        composeTestRule.activityRule.scenario.onActivity { gardenActivity ->
             activity = gardenActivity
 
             val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
             findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
         }
+//        activityTestRule.scenario.onActivity { gardenActivity ->
+//            activity = gardenActivity
+//
+//            val bundle = Bundle().apply { putString("plantId", "malus-pumila") }
+//            findNavController(activity, R.id.nav_host).navigate(R.id.plant_detail_fragment, bundle)
+//        }
     }
 
     @Test
     fun testPlantName() {
-        onView(ViewMatchers.withText("Apple"))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        composeTestRule.onNodeWithText("Apple").assertIsDisplayed()
+//        onView(ViewMatchers.withText("Apple"))
+//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
