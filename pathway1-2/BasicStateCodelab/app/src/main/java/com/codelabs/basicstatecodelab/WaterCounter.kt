e7f8223e8f52164a05eff37cc7c70338641e594a
jung eunjung <agjjung@avatye.com>
2022-12-01 13:23:21 +0900

    Pathway1-2 - BasicStateCodelab 7

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -11,20 +12,45 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)){
+    Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            Text(text = "You've had $count glasses.")
+            Text("You've had $count glasses.")
         }
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
 }
 
+/*
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)){
+        var count by remember { mutableStateOf(0) }
 
+        if (count > 0) {
+            var showTask by remember {
+                mutableStateOf(true)
+            }
+            if(showTask) {
+                WellnessTaskItem(taskName = "Have you taken your 15 minute walk today?", onClose = { showTask = false })
+            }
+            Text(text = "You've had $count glasses.")
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "Clear water count")
+            }
+        }
+    }
+}*/
---
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
@@ -0,0 +1,45 @@
+package com.codelabs.basicstatecodelab
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        IconButton(onClick = onClose) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Close"
+            )
+        }
+    }
+}
+
+@Preview()
+@Composable()
+fun PreviewWellnessTaskItem() {
+    WellnessTaskItem(taskName = "This is a task", onClose = { })
+}
