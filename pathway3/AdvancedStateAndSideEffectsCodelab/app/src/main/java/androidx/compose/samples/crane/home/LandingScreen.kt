9a3d82c9e4c69a65937324af24a4b64f91f04c3e
algosketch <sjy_9484@naver.com>
2022-12-02 23:17:52 +0900

    pathway3 - state: step 4

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -19,19 +19,24 @@ package androidx.compose.samples.crane.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+        val currentOnTimeout by rememberUpdatedState(newValue = (onTimeout))
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
+
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
