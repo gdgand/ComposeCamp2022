1bad31970d82757d965e46bd033b520c28ae0893
JaesungLeee <viki0114@naver.com>
2022-12-03 17:06:11 +0900

    [AnimationCodelab] 7. 애니메이션 반복

--- pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -584,7 +584,19 @@ private fun WeatherRow(
 @Composable
 private fun LoadingRow() {
     // TODO 5: Animate this value between 0f and 1f, then back to 0f repeatedly.
-    val alpha = 1f
+    val infiniteTransition = rememberInfiniteTransition()
+    val alpha by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 1000
+                0.7f to 500
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+//    val alpha = 1f
     Row(
         modifier = Modifier
             .heightIn(min = 64.dp)
