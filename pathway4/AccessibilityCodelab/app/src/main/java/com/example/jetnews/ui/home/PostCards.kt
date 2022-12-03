ee10f92f0c4e93cab0a3a5345a134b702b787696
Durk-jae Yun <durkjae.yun@directionsoft.com>
2022-12-03 16:25:27 +0900

    4. 라벨 클릭

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
@@ -52,7 +52,13 @@ import com.example.jetnews.ui.theme.JetnewsTheme
 fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
     var openDialog by remember { mutableStateOf(false) }
     Row(
-        Modifier.clickable { navigateToArticle(post.id) }
+        Modifier.clickable(
+            onClickLabel = stringResource(id = R.string.action_read_article)
+        ) {
+            navigateToArticle(
+                post.id
+            )
+        }
     ) {
         Image(
             painter = painterResource(post.imageThumbId),
@@ -130,8 +136,11 @@ fun PostCardPopular(
 ) {
     Card(
         shape = MaterialTheme.shapes.medium,
-        modifier = modifier.size(280.dp, 240.dp),
-        onClick = { navigateToArticle(post.id) }
+        modifier = modifier
+            .clickable(onClickLabel = stringResource(id = R.string.action_read_article)) {
+                navigateToArticle(post.id)
+            }
+            .size(280.dp, 240.dp)
     ) {
         Column {
 
