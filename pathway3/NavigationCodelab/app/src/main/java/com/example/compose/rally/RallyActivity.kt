2eaf0406a8bb0f13968b4e33d7328bf5a26e95e5
larry.p <larry.p@kakaocorp.com>
2022-12-04 18:04:50 +0900

    feat - PW3 네비게이션 SingleAccountScreen으로 이동

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -31,6 +31,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.rally.ui.accounts.AccountsScreen
+import com.example.compose.rally.ui.accounts.SingleAccountScreen
 import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.overview.OverviewScreen
@@ -81,21 +82,36 @@ fun RallyApp() {
                         },
                         onClickSeeAllBills = {
                             navController.navigateSingleTopTo(Bills.route)
+                        },
+                        onAccountClick = { accountType ->
+                            navController.navigateToSingleAccount(accountType)
                         }
                     )
                 }
                 composable(route = Accounts.route) {
-                    AccountsScreen()
+                    AccountsScreen(
+                        onAccountClick = { accountType ->
+                            navController.navigateToSingleAccount(accountType)
+                        }
+                    )
                 }
                 composable(route = Bills.route) {
                     BillsScreen()
+                }
+                composable(
+                    route = SingleAccount.routeWithArgs,
+                    arguments = SingleAccount.arguments
+                ) { navBackStackEntry ->
+                    val accountType =
+                        navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+                    SingleAccountScreen(accountType)
                 }
             }
         }
     }
 }
 
-fun NavHostController.navigateSingleTopTo(route: String) {
+private fun NavHostController.navigateSingleTopTo(route: String) {
     navigate(route) {
         popUpTo(
             this@navigateSingleTopTo.graph.findStartDestination().id
@@ -105,4 +121,8 @@ fun NavHostController.navigateSingleTopTo(route: String) {
         launchSingleTop = true
         restoreState = true
     }
+}
+
+private fun NavHostController.navigateToSingleAccount(accountType: String) {
+    navigateSingleTopTo("${SingleAccount.route}/$accountType")
 }
