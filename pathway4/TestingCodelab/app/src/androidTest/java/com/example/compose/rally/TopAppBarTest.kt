771f579a82be596ab150a957f82a8fa2a26c705d
심은석 <eushim@gmarket.com>
2022-12-04 19:32:37 +0900

    Merged and unmerged Semantics trees

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -31,9 +31,17 @@ class TopAppBarTest {
         }
 
         // composeTestRule.onRoot().printToLog("currentLabelExists")
+        // composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
