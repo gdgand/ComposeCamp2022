2f7038c2d2e713d36d4fa91ee140726e1287cde7
joeloewi7178 <joeloewi7178@gmail.com>
2022-12-04 23:45:50 +0900

    step: view models and live data

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -110,7 +110,7 @@ class PlantDetailFragment : Fragment() {
 
             composeView.setContent {
                 MaterialTheme {
-                    PlantDetailDescription()
+                    PlantDetailDescription(plantDetailViewModel)
                 }
             }
         }
