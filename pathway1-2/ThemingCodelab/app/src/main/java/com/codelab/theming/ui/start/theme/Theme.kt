39fdee6ca6682ad4d80dccbf859d2f80956a00f5
sorea.no <sorae.no@thingsflow.com>
2022-12-04 16:55:54 +0900

    Theme 추가

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -0,0 +1,24 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun JetnewsTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = LightColors,
+        content = content
+    )
+}
+
+private val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
