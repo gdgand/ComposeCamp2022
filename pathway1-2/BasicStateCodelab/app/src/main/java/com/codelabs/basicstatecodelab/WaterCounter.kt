eb19f6388992451702bfee1891a5eae3ffaa059f
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2022-12-05 18:12:06 +0900

    [6 Compose에서의 상태]  9. 상태 호이스팅

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -10,23 +10,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-
         if (count > 0) {
             Text(
                 text = "You've had $count glasses."
             )
         }
-
-        Button(onClick = { count++ }, Modifier.padding(8.dp), enabled = count < 10) {
+        Button(onClick = onIncrement, Modifier.padding(8.dp), enabled = count < 10) {
             Text(text = "Add one")
         }
     }
 }
 
 @Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier)
+}
+
+@Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
