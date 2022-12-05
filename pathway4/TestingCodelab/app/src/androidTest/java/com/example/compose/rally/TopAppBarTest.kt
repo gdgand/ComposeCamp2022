05eb10d92f6ae501a84f6f2314052fd0a06404ae
JUNSEO <djawnstj44@naver.com>
2022-12-05 14:49:30 +0900

    TestingCodelab

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,67 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+//    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule(RallyActivity::class.java) // Testing in isolation
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {  },
+                currentScreen = RallyScreen.Accounts,
+            )
+        }
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                hasParent(
+                    hasContentDescription(RallyScreen.Accounts.name)
+                ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+    }
+
+}
