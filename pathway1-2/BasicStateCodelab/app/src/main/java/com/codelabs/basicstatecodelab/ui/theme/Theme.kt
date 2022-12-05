ad5deb7cbc01f90fd46bdb3ea57caccaac37c959
sunyoung <rhtjsdud95@fxgear.net>
2022-12-05 12:25:16 +0900

    [PATHWAY 1-2] 6. Compose에서의 상태

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/theme/Theme.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionContext
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
