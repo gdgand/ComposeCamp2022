af066409893668bf56e6d748bb67c5e526c47017
rosf73 <alswlzhsclq@naver.com>
2022-12-05 18:06:30 +0900

    Overview onClick 작성

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -80,7 +80,14 @@ fun RallyApp() {
                 modifier = Modifier.padding(innerPadding)
             ) {
                 composable(route = Overview.route) {
-                    OverviewScreen()
+                    OverviewScreen(
+                        onClickSeeAllAccounts = {
+                            navController.navigateSingleTopTo(Accounts.route)
+                        },
+                        onClickSeeAllBills = {
+                            navController.navigateSingleTopTo(Bills.route)
+                        }
+                    )
                 }
                 composable(route = Accounts.route) {
                     AccountsScreen()
