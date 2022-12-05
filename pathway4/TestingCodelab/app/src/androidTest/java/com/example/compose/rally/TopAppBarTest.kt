e65cb49343af8de2e3b80548989418ae8074c0c7
JaeSeon-Yu <stacey5165@naver.com>
2022-12-05 15:22:53 +0900

    [codelab] top app bar test 동기-애니메이션

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -52,11 +52,12 @@ class TopAppBarTest {
             )
         }
 
-        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
-
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertExists()
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(hasContentDescription(RallyScreen.Accounts.name)),
+                useUnmergedTree = true
+            ).assertExists()
     }
 
 }
