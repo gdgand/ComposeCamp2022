7e8358d315096b0f0139e6f802045334c735a4ac
bcchoi0202 <bcchoi0202@gmail.com>
2022-12-05 16:04:40 +0900

    7. Optional exercise complete

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,5 +1,9 @@
 package com.example.compose.rally
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.example.compose.rally.ui.components.RallyTopAppBar
@@ -10,7 +14,35 @@ class TopAppBarTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-    
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
     @Test
     fun rallyTopAppBarTest_currentLabelExists() {
         val allScreens = RallyScreen.values().toList()
@@ -29,6 +61,34 @@ class TopAppBarTest {
                 ),
                 useUnmergedTree = true
             ).assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_clickTabLabel() {
+        val allScreens = RallyScreen.values().toList()
+        var currentScreen by mutableStateOf(RallyScreen.Overview)
+
+        var assertionName = ""
+
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {
+                    currentScreen = it
+                    Log.e("clickTabLabel", "${currentScreen.name}, $assertionName")
+                    assert(currentScreen.name == assertionName)
+                },
+                currentScreen = currentScreen)
+        }
+
+        assertionName = RallyScreen.Overview.name
+        composeTestRule.onNodeWithContentDescription(RallyScreen.Overview.name).performClick()
+
+        assertionName = RallyScreen.Accounts.name
+        composeTestRule.onNodeWithContentDescription(RallyScreen.Accounts.name).performClick()
+
+        assertionName = RallyScreen.Bills.name
+        composeTestRule.onNodeWithContentDescription(RallyScreen.Bills.name).performClick()
 
     }
 }
