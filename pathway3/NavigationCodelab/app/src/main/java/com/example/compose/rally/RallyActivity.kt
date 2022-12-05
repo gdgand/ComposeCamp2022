d8e3e3751bf73ed42b05c96c0370e6971ed6b878
oreocube <oreocube13@gmail.com>
2022-12-05 15:36:29 +0900

    NavigationCodelab - Extract the NavHost

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -30,7 +30,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navDeepLink
 import com.example.compose.rally.ui.accounts.AccountsScreen
 import com.example.compose.rally.ui.accounts.SingleAccountScreen
 import com.example.compose.rally.ui.bills.BillsScreen
@@ -60,96 +59,79 @@ fun RallyApp() {
         val currentDestination = currentBackStack?.destination
         val currentScreen =
             rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview
-        Scaffold(
-            topBar = {
-                RallyTabRow(
-                    allScreens = rallyTabRowScreens,
-                    onTabSelected = { newScreen ->
-                        // navController.navigate(newScreen.route)
-                        navController.navigateSingleTopTo(newScreen.route)
-                    },
-                    currentScreen = currentScreen
-                )
-            }
-        ) { innerPadding ->
-            NavHost(
+        Scaffold(topBar = {
+            RallyTabRow(
+                allScreens = rallyTabRowScreens, onTabSelected = { newScreen ->
+                    // navController.navigate(newScreen.route)
+                    navController.navigateSingleTopTo(newScreen.route)
+                }, currentScreen = currentScreen
+            )
+        }) { innerPadding ->
+            RallyNavHost(
                 navController = navController,
-                startDestination = Overview.route,
                 modifier = Modifier.padding(innerPadding)
-            ) {
-                composable(route = Overview.route) {
-                    OverviewScreen(
-                        onClickSeeAllAccounts = {
-                            navController.navigateSingleTopTo(Accounts.route)
-                        },
-                        onClickSeeAllBills = {
-                            navController.navigateSingleTopTo(Bills.route)
-                        },
-                        onAccountClick = { accountType ->
-                            navController.navigateToSingleAccount(accountType)
-                        }
-                    )
-                }
-                composable(route = Accounts.route) {
-                    AccountsScreen(
-                        onAccountClick = { accountType ->
-                            navController.navigateToSingleAccount(accountType)
-                        }
-                    )
-                }
-                composable(route = Bills.route) {
-                    BillsScreen()
-                }
-//                composable(
-//                    route =
-//                    "${SingleAccount.route}/{${SingleAccount.accountTypeArg}}",
-//                    arguments = listOf(
-//                        navArgument(SingleAccount.accountTypeArg) { type = NavType.StringType }
-//                    )
-//                ) {
-//                    SingleAccountScreen()
-//                }
-                composable(
-                    route = SingleAccount.routeWithArgs,
-                    arguments = SingleAccount.arguments,
-                    deepLinks = SingleAccount.deepLinks
-                ) { navBackStackEntry ->
-                    // argument 꺼내오기
-                    val accountType =
-                        navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-                    SingleAccountScreen(accountType)
-                }
-            }
+            )
+        }
+    }
+}
+
+@Composable
+fun RallyNavHost(
+    navController: NavHostController, modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController, startDestination = Overview.route, modifier = modifier
+    ) {
+        composable(route = Overview.route) {
+            OverviewScreen(onClickSeeAllAccounts = {
+                navController.navigateSingleTopTo(Accounts.route)
+            }, onClickSeeAllBills = {
+                navController.navigateSingleTopTo(Bills.route)
+            }, onAccountClick = { accountType ->
+                navController.navigateToSingleAccount(accountType)
+            })
+        }
+        composable(route = Accounts.route) {
+            AccountsScreen(onAccountClick = { accountType ->
+                navController.navigateToSingleAccount(accountType)
+            })
+        }
+        composable(route = Bills.route) {
+            BillsScreen()
+        }
+        composable(
+            route = SingleAccount.routeWithArgs,
+            arguments = SingleAccount.arguments,
+            deepLinks = SingleAccount.deepLinks
+        ) { navBackStackEntry ->
+            // argument 꺼내오기
+            val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+            SingleAccountScreen(accountType)
         }
     }
 }
 
 
-fun NavHostController.navigateSingleTopTo(route: String) =
-    this.navigate(route) {
-        // popUpTo(startDestination) { saveState = true }
-        // 그래프 출발지로 pop. 큰 스택을 다시 구성하는 것을 피하기 위해.
-        popUpTo(
-            this@navigateSingleTopTo.graph.findStartDestination().id
-        ) {
-            saveState = true
-        }
-
-        // 스택 탑에 있는 친구와 동일하면 다시 쌓지 않기
-        // launchSingleTop 옵션을 사용하지 않으면 동일한 탭을 여러번 눌렀을 때
-        // 같은 화면이 계속해서 쌓이게 됨
-        launchSingleTop = true
-
-        // restoreState = true
-        // 이전의 상태로 복구해야 하는지 옵션
-        // 이전에 저장한 상태가 없다면 true로 설정해도 아무런 효과가 없다.
-        restoreState = true
+fun NavHostController.navigateSingleTopTo(route: String) = this.navigate(route) {
+    // popUpTo(startDestination) { saveState = true }
+    // 그래프 출발지로 pop. 큰 스택을 다시 구성하는 것을 피하기 위해.
+    popUpTo(
+        this@navigateSingleTopTo.graph.findStartDestination().id
+    ) {
+        saveState = true
     }
+
+    // 스택 탑에 있는 친구와 동일하면 다시 쌓지 않기
+    // launchSingleTop 옵션을 사용하지 않으면 동일한 탭을 여러번 눌렀을 때
+    // 같은 화면이 계속해서 쌓이게 됨
+    launchSingleTop = true
+
+    // restoreState = true
+    // 이전의 상태로 복구해야 하는지 옵션
+    // 이전에 저장한 상태가 없다면 true로 설정해도 아무런 효과가 없다.
+    restoreState = true
+}
 
 private fun NavHostController.navigateToSingleAccount(accountType: String) {
     this.navigateSingleTopTo("${SingleAccount.route}/$accountType")
 }
-
-
-
-
