27265b68fcf97545094dca268676974593f0e094
kim-yongsuk <dydtjr@98>
2022-12-06 15:45:43 +0900

    Merged unMerged Semantics trees

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -22,7 +22,11 @@ class TopAppBarTest {
         }
 
         composeRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(hasContentDescription(RallyScreen.Accounts.name)),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
