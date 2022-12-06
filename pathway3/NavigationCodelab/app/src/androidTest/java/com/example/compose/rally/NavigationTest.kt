96f9f26ab3e23ef519d9054a18032c43b417d70b
sangsulee <sangsulee@gmarket.com>
2022-12-06 17:49:31 +0900

    Feat: TestNavHostController 생성, 초기 컴포저블 구성

--- pathway3/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
+++ pathway3/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -1,11 +1,32 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import junit.framework.Assert.fail
 import org.junit.Rule
+import org.junit.Test
 
 class NavigationTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    lateinit var navController: TestNavHostController
+
+    @Test
+    fun rallyNavHost() {
+        composeTestRule.setContent {
+            // Creates a TestNavHostController
+            navController =
+                TestNavHostController(LocalContext.current)
+            // Sets a ComposeNavigator to the navController so it can navigate through composables
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            RallyNavHost(navController = navController)
+        }
+        fail()
+    }
 }
