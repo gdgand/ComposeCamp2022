14b7c6249dfbcc6e2327113be5a9ce6a25271a8f
amy <essim@wemade.com>
2022-12-06 10:23:27 +0900

    [Jetpack Compose의 접근성] 맞춤 병합

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/PostContent.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/article/PostContent.kt
@@ -134,7 +134,7 @@ private fun PostHeaderImage(post: Post) {
 @Composable
 private fun PostMetadata(metadata: Metadata) {
     val typography = MaterialTheme.typography
-    Row {
+    Row(Modifier.semantics(mergeDescendants = true) { }) {
         Image(
             imageVector = Icons.Filled.AccountCircle,
             contentDescription = null,
