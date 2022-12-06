3683659d42e349cc259bc3ac7017b258fad7655e
sangsulee <sangsulee@gmarket.com>
2022-12-06 17:42:11 +0900

    Feat: test init

---
+++ pathway3/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -0,0 +1,11 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+}
