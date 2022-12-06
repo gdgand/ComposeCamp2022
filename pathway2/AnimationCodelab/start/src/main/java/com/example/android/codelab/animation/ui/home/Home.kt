efab5cb6b0e9e2961c63caf19810f74bae417cc2
yeongjin <dudwls901@naver.com>
2022-12-06 18:37:37 +0900

    [UI] 사이즈 변경 애니메이션 적용

--- pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -376,11 +376,12 @@ private fun TopicRow(topic: String, expanded: Boolean, onClick: () -> Unit) {
         elevation = 2.dp,
         onClick = onClick
     ) {
-        // TODO 3: Animate the size change of the content.
+        // TODO 3: Animate the size change of the content. Done
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
+                .animateContentSize()
         ) {
             Row {
                 Icon(
