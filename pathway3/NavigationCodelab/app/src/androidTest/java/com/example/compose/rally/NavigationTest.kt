64c4b1c1fe4b99dee4820e1e5cf1f7551263242b
DESKTOP-QIUUF47\Admin <jihoon708@naver.com>
2022-12-06 22:13:29 +0900

    Done 2

---
+++ pathway3/NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -0,0 +1,36 @@
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.compose.rally.RallyNavHost
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupRallyNavHost() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            RallyNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun rallyNavHost_verifyOverviewStartDestination() {
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+    }
+}
