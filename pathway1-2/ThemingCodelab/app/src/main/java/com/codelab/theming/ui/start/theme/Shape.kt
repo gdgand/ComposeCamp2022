4de02ca3661f59a55b2017e54b92719147959f8a
Yena Choi <yena@deali.net>
2022-12-07 10:20:14 +0900

    Jetpack Compose 테마 설정 완료

--- pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
@@ -2,7 +2,11 @@ package com.codelab.theming.ui.start.theme
 
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 
 val JetnewsShapes = Shapes(
@@ -10,3 +14,13 @@ val JetnewsShapes = Shapes(
     medium = CutCornerShape(topStart = 24.dp),
     large = RoundedCornerShape(8.dp)
 )
+//
+//@Composable
+//fun FilledTextField(
+//    shape: Shape = MaterialTheme.shapes.small.copy(
+//        bottomStart = ZeroCornerSize,
+//        bottomEnd = ZeroCornerSize
+//    )
+//) {
+//
+//}
