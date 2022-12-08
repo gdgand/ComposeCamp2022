1cec595ebd9d393092877a8c5e7c8f4a32201a58
gudrmsglgl <gudrmsglgl@gmail.com>
2022-12-08 10:43:30 +0900

    gudrmsglgl #pathway3 - NavigationCodelab

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,16 +19,24 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.*
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.rally.navi.Router
+import com.example.compose.rally.navi.navigateSingleTopTo
+import com.example.compose.rally.ui.accounts.AccountsScreen
+import com.example.compose.rally.ui.accounts.SingleAccountScreen
+import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewScreen
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -47,19 +55,95 @@ class RallyActivity : ComponentActivity() {
 @Composable
 fun RallyApp() {
     RallyTheme {
-        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
+        //var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
+        val navController = rememberNavController()
+        val currentBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackStackEntry?.destination
+        val currentScreen = rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview
+
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { newScreen ->
+                        navController.navigateSingleTopTo(newScreen.route)
+                    },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
-            }
+            RallyNavHost(
+                navController = navController,
+                modifier = Modifier.padding(innerPadding)
+            )
         }
     }
+}
+
+@Composable
+fun RallyNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Overview.route,
+        modifier = modifier
+    ) {
+        routeOverViewScreen(
+            route = Overview.route,
+            onClickSeeAllAccounts = {
+                Router.navAccountScreen(navController)
+            },
+            onClickSeeAllBills = {
+                Router.navBillScreen(navController)
+            },
+            onAccountClick = { accountType ->
+                Router.navSingleAccountScreen(
+                    navController,
+                    argument = accountType
+                )
+            }
+        )
+        routeAccountScreen(
+            route = Accounts.route,
+            onAccountClick = { accountType ->
+                Router.navSingleAccountScreen(navController, argument = accountType)
+            }
+        )
+
+        composable(route = Bills.route) {
+            BillsScreen()
+        }
+        routeSingleAccountScreen(
+            route = SingleAccount.routeWithArgs
+        )
+    }
+}
+
+private fun NavGraphBuilder.routeOverViewScreen(
+    route: String,
+    onClickSeeAllAccounts: () -> Unit,
+    onClickSeeAllBills: () -> Unit,
+    onAccountClick: (String) -> Unit
+) = composable(route) {
+    OverviewScreen(onClickSeeAllAccounts, onClickSeeAllBills, onAccountClick)
+}
+
+private fun NavGraphBuilder.routeAccountScreen(
+    route: String,
+    onAccountClick: (String) -> Unit
+) = composable(route) {
+    AccountsScreen(onAccountClick)
+}
+
+private fun NavGraphBuilder.routeSingleAccountScreen(
+    route: String,
+) = composable(
+    route,
+    arguments = SingleAccount.arguments,
+    deepLinks = SingleAccount.deeplink
+) { navBackStackEntry ->
+    val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+    SingleAccountScreen(accountType = accountType)
 }
---
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/navi/Navigation.kt
@@ -0,0 +1,15 @@
+package com.example.compose.rally.navi
+
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+
+fun NavHostController.navigateSingleTopTo(route: String) =
+    this.navigate(route) {
+        popUpTo(
+            this@navigateSingleTopTo.graph.findStartDestination().id
+        ) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
