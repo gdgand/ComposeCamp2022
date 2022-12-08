613d4a1e1080157ebc2acb149cb461f718e8330a
Chanbong Park <siprikorea@gmail.com>
2022-12-08 15:44:09 +0900

    Testing in Jetpack Compose - Debugging tests

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,9 +1,8 @@
 package com.example.compose.rally
 
 import androidx.compose.material.Text
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -26,6 +25,6 @@ class TopAppBarTest {
 
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertIsSelected()
+            .assertExists()
     }
 }
