a4eecc7e057b8779bbd854eaf0276d24e1e36c82
comye1 <comye1@seoultech.ac.kr>
2022-12-09 00:25:42 +0900

    initialChecked와 checked

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
@@ -1,10 +1,13 @@
 package com.codelabs.basicstatecodelab
 
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-data class WellnessTask(
+class WellnessTask(
     val id: Int,
     val label: String,
-    var checked: MutableState<Boolean> = mutableStateOf(false)
-)
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
