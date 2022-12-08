4a30f172230ef7a73a43827a20fe7c5239476aee
dogeby <dogeby@gmail.com>
2022-12-09 05:41:07 +0900

    [1-2 BasicStateCodelab] 상태 호이스팅

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -21,3 +21,25 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun StatefulCounter() {
+    var waterCount by remember { mutableStateOf(0) }
+
+    var juiceCount by remember { mutableStateOf(0) }
+
+    StatelessCounter(waterCount, { waterCount++ })
+    StatelessCounter(juiceCount, { juiceCount++ })
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
