389fdbc9c408b507bfcbb11265f0a611241fa7d3
dogeby <dogeby@gmail.com>
2022-12-09 05:59:50 +0900

    [1-2 BasicStateCodelab] ViewModel의 상태

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTaskItem.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 
 import androidx.compose.material.Checkbox
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
 fun WellnessTaskItem(
@@ -40,19 +39,4 @@ fun WellnessTaskItem(
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
-}
-
-@Composable
-fun WellnessTaskItem(
-    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
-) {
-    var checkedState by rememberSaveable { mutableStateOf(false) }
-
-    WellnessTaskItem(
-        taskName = taskName,
-        checked = checkedState,
-        onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = onClose,
-        modifier = modifier,
-    )
 }
