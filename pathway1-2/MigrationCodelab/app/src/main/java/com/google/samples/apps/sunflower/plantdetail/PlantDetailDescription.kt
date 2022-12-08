2714171dd638ed986ae5366b92da9065a9971bd8
MalibinYun <nightmare73@naver.com>
2022-12-08 13:52:34 +0900

    [migration] 8. 추가 XML 코드 이전

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -21,9 +21,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
@@ -40,7 +40,12 @@ fun PlantDetailDescription(
 
 @Composable
 fun PlantDetailContent(plant: Plant) {
-    PlantName(name = plant.name)
+    Surface {
+        Column(Modifier.padding(dimensionResource(id = R.dimen.margin_normal))) {
+            PlantName(name = plant.name)
+            PlantWatering(wateringInterval = plant.wateringInterval)
+        }
+    }
 }
 
 @Composable
@@ -55,10 +60,51 @@ private fun PlantName(name: String) {
     )
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun PlantWatering(wateringInterval: Int) {
+    Column(Modifier.fillMaxWidth()) {
+        val centerWithPaddingModifier = Modifier
+                .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+                .align(Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(id = R.dimen.margin_normal)
+
+        Text(
+                text = stringResource(R.string.watering_needs_prefix),
+                color = MaterialTheme.colors.primaryVariant,
+                fontWeight = FontWeight.Bold,
+                modifier = centerWithPaddingModifier.padding(top = normalPadding)
+        )
+
+        val wateringIntervalText = pluralStringResource(id = R.plurals.watering_needs_suffix, count = wateringInterval, wateringInterval)
+        Text(
+                text = wateringIntervalText,
+                modifier = centerWithPaddingModifier.padding(bottom = normalPadding),
+        )
+    }
+}
+
 @Preview
 @Composable
 fun PlantNamePreview() {
     MaterialTheme {
         PlantName("Apple")
+    }
+}
+
+@Preview
+@Composable
+fun PlantWateringPreview() {
+    MaterialTheme {
+        PlantWatering(wateringInterval = 7)
+    }
+}
+
+@Preview
+@Composable
+fun PlantDetailContentPreview() {
+    MaterialTheme {
+        PlantDetailContent(plant = Plant("", "Apple", "", 1))
     }
 }
