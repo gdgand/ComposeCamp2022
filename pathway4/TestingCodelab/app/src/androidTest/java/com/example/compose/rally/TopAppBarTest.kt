5eea2c8c784236a64050369df94663bf6aa13a2a
larry.p <larry.p@kakaocorp.com>
2022-12-09 15:32:21 +0900

    feat - PW4 테스트 시멘틱트리 머지/언머지

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,8 +1,9 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
@@ -36,10 +37,37 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
-        // composeTestRule.onRoot().printToLog("currentLabelExists")
-
+        println("----------- Unmerged")
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onRoot(useUnmergedTree = true)
+            .printToLog("currentLabelExists")
+
+        println("----------- Merged")
+        composeTestRule
+            .onRoot(useUnmergedTree = false)
+            .printToLog("currentLabelExists")
+
+        /*
+        |-Node #8 at (l=203.0, t=137.0, r=505.0, b=205.0)px
+           | Role = 'Tab'
+           | Focused = 'false'
+           | Selected = 'true'
+           | ContentDescription = '[Accounts]'
+           | Actions = [OnClick, RequestFocus]
+           | MergeDescendants = 'true'
+           | ClearAndSetSemantics = 'true'
+           |  |-Node #12 at (l=305.0, t=137.0, r=505.0, b=190.0)px
+           |    Text = '[ACCOUNTS]'
+           |    Actions = [GetTextLayoutResult]
+         */
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
