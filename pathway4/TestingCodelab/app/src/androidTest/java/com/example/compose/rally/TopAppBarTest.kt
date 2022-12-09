3414064e3f4b02aed75176f8cc90718f94852513
larry.p <larry.p@kakaocorp.com>
2022-12-09 15:46:45 +0900

    feat - PW4 테스트 선택 과제, 다른탭 선택 상태 확인

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,12 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasParent
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
@@ -69,5 +71,32 @@ class TopAppBarTest {
                 useUnmergedTree = true
             )
             .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_selectBillsTab() {
+        // Given
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onRoot()
+            .printToLog("currentLabelExists")
+
+        // When
+        composeTestRule
+            .onNode(hasContentDescription(RallyScreen.Bills.name))
+            .performClick()
+
+        // Then
+        composeTestRule
+            .onNode(hasContentDescription(RallyScreen.Bills.name))
+            .assertIsDisplayed()
     }
 }
