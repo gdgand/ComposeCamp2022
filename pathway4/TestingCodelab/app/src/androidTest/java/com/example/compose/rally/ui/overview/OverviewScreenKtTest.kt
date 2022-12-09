8dcdfa65b6fbb3166899e79537c02c6d914613e1
kim.inwha <kim.inwha@navercorp.com>
2022-12-09 14:07:33 +0900

    [pathway4] TestingCodelab

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/ui/overview/OverviewScreenKtTest.kt
@@ -0,0 +1,24 @@
+package com.example.compose.rally.ui.overview
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class OverviewScreenKtTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
+}
