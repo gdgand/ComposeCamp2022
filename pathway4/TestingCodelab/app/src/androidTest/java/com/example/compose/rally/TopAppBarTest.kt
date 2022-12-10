243752dc39c08b719d9c9654e5bbe3cb0446823c
pjs3065 <tmakdlfwotjd@gmail.com>
2022-12-10 23:16:47 +0900

    jetpack compose 테스팅 - 4. Debugging tests

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,6 +3,8 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -23,8 +25,10 @@ class TopAppBarTest {
             )
         }
 
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertIsSelected()
+            .assertExists()
     }
 }
