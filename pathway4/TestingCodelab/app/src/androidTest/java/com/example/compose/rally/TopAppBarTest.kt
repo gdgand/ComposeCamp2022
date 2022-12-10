d0b04e6f19a124f0cef2ed075f7b5c87a2189af0
cwpark <cwpark@bbros.kr>
2022-12-11 00:59:41 +0900

    Jetpack ComposeTest
     - 디버깅 테스트
     - merged and unmerged semantics trees

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,11 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,8 +13,8 @@ class TopAppBarTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun rallyTopAppBarTest() {
+    @Before
+    fun setup() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
@@ -22,9 +23,29 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
+    }
 
+    @Test
+    fun rallyTopAppBarTest() {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists_useUnMergedTree() {
+        composeTestRule
+            .onNodeWithText(
+                text = RallyScreen.Accounts.name.uppercase(),
+                useUnmergedTree = true
+            )
+            .assertExists()
     }
 }
