427e1310439451ec836aec4371f373a707091bea
ridsync <ridsync@gmail.com>
2022-12-11 16:15:35 +0900

    feature(compose) : #0001 - Jetpack Compose Migration 완료

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantDetailBindingAdapters.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantDetailBindingAdapters.kt
@@ -22,15 +22,15 @@ import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.samples.apps.sunflower.GlideApp
 import com.google.samples.apps.sunflower.R
 
 @BindingAdapter("imageFromUrl")
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
-        Glide.with(view.context)
+        GlideApp.with(view.context)
             .load(imageUrl)
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(view)
