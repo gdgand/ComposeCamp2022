89160c887e6cdcaab972a0b2d0860627806f8814
cwpark <cwpark@bbros.kr>
2022-12-11 17:50:48 +0900

    Pathway4 - Jetpack Compose의 접근성
     - 스위치 및 체크박스

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Divider
@@ -43,6 +44,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -159,6 +163,11 @@ private fun TopicItem(itemTitle: String, selected: Boolean, onToggle: () -> Unit
     Row(
         modifier = Modifier
             .padding(horizontal = 16.dp, vertical = 8.dp)
+            .toggleable(
+                value = selected,
+                onValueChange = { onToggle() },
+                role = Role.Checkbox
+            )
     ) {
         Image(
             painter = image,
@@ -178,7 +187,7 @@ private fun TopicItem(itemTitle: String, selected: Boolean, onToggle: () -> Unit
         Spacer(Modifier.weight(1f))
         Checkbox(
             checked = selected,
-            onCheckedChange = { onToggle() },
+            onCheckedChange = null,
             modifier = Modifier.align(Alignment.CenterVertically)
         )
     }
