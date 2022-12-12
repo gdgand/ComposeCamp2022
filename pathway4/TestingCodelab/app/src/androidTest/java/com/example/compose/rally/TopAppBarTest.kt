ca9f7cf505d9d6436adb8c82782fe8a0cced2900
sangsulee <sangsulee@gmarket.com>
2022-12-12 21:22:18 +0900

    Add the ComposeTestRule

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,12 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    // TODO: Add tests
+}
