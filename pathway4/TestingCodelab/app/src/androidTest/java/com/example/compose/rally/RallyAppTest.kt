8f3f791f9f8c37d54eaf99570549bef0bf55bfde
sangsulee <sangsulee@gmarket.com>
2022-12-12 22:14:37 +0900

    Feat: homework: 탭 변경 후 잘 변경 되는지 테스트

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/RallyAppTest.kt
@@ -0,0 +1,39 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class RallyAppTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyAppTest() {
+        composeTestRule.setContent {
+            RallyApp()
+        }
+        Thread.sleep(1000)
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNode(
+                hasContentDescription("Accounts"),
+                useUnmergedTree = true
+            ).performClick()
+
+        Thread.sleep(1000)
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+        Thread.sleep(1000)
+
+    }
+}
