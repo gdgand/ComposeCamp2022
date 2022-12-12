5ed5b9e646a9733b04dcc5d069ef482d690936f0
sangsulee <sangsulee@gmarket.com>
2022-12-12 21:28:34 +0900

    Feat: rallyTopAppBarTest

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,12 +1,25 @@
 package com.example.compose.rally
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
+import org.junit.Test
 
 class TopAppBarTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    // TODO: Add tests
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        Thread.sleep(5000)
+    }
 }
