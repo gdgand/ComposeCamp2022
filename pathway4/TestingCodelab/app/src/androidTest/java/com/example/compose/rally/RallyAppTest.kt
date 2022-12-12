f0270cce660a17ec56b25007e8c0d20d24e6b89f
sangsulee <sangsulee@gmarket.com>
2022-12-12 22:14:57 +0900

    fin.4.1

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/RallyAppTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/RallyAppTest.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Rule
 import org.junit.Test
-
+ 
 class RallyAppTest {
     @get:Rule
     val composeTestRule = createComposeRule()
