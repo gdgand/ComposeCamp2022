14d8551c5fe139c78f6c013e013979fdf70a167b
middlefgr <2giwon@gmail.com>
2022-12-12 21:40:28 +0900

    [pathway 3 Navigation] 8. 딥 링크 지원 사용 설정

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -24,6 +24,7 @@ import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 
 /**
  * Contract for information needed on every Rally navigation destination
@@ -59,8 +60,11 @@ object SingleAccount : RallyDestination {
     const val accountTypeArg = "account_type"
     val routeWithArgs = "${route}/{${accountTypeArg}}"
     val arguments = listOf(
-        navArgument(SingleAccount.accountTypeArg) { type = NavType.StringType }
+        navArgument(accountTypeArg) { type = NavType.StringType }
     )
+    val deepLinks = listOf(navDeepLink {
+        uriPattern = "rally://$route/{$accountTypeArg}"
+    })
 }
 
 // Screens to be displayed in the top RallyTabRow
