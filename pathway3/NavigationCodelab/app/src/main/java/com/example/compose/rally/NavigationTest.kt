9bc741f792b81910e5aa37c85f5bd678e480c829
Sohee0428 <82200065+Sohee0428@users.noreply.github.com>
2022-12-12 19:36:12 +0900

    [3-4 탐색]  10. Compose Navigation 테스트

---
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/NavigationTest.kt
@@ -0,0 +1,37 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupRallyNavHost() {
+        composeTestRule.setContent {
+            navController =
+                TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            RallyNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun rallyNavHost_clickAllBills_navigateToBills() {
+        composeTestRule.onNodeWithContentDescription("All Bills")
+            .performScrollTo()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "bills")
+    }
+}
