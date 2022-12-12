834c688c45a5be01d54698a7309d8d5f37f22402
sangsulee <sangsulee@gmarket.com>
2022-12-12 21:31:10 +0900

    Fix: use theme

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -2,6 +2,7 @@ package com.example.compose.rally
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,11 +15,13 @@ class TopAppBarTest {
     fun rallyTopAppBarTest() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = { },
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
         }
         Thread.sleep(5000)
     }
