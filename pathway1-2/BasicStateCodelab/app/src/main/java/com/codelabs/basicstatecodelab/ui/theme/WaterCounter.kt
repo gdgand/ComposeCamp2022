69cffc725b530be796f7b38e6840f37c129925d8
soo <onemask@naver.com>
2022-12-13 01:31:11 +0900

    [#2-1State in Jetpack Compose] 7

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/theme/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/ui/theme/WaterCounter.kt
@@ -1,22 +1,26 @@
 package com.codelabs.basicstatecodelab.ui.theme
+
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.Button
-import androidx.compose.foundation.layout.Column
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
   Column(modifier = modifier.padding(16.dp)) {
-    var count = 0
-    Text("You've had $count glasses.")
+    var count by remember { mutableStateOf(0) }
+    if (count > 0) {
+      Text("You've had $count glasses.")
+    }
     Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
       Text("Add one")
     }
-  }
-}
+  }}
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
