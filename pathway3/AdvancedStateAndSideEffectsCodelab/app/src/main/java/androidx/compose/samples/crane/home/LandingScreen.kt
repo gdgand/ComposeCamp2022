54bacf80d2e874eddd4d85d6b336e5fa1f44840d
yein <gkshs1234@gmail.com>
2022-12-13 17:49:09 +0900

    Pathway3 Advanced State And Side Effects 완료

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,18 +20,33 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+        // 최신의 onTimeout 함수를 참조할 수 있게 해줌
+        val currentOnTimeout by rememberUpdatedState(newValue = onTimeout)
+
+        // key가 constant이므로 최초 1회만 실행됨
+        // 여기서 rememberCoroutineScope와 scope.launch { delay } 를 사용해도되나 ?
+        // 안됨 -> LaunchEffect는 side-effect를 사용하는 composable이 composition내에
+        // 있을 때에만 실행됨을 보장함
+        // remember~의 경우 해당 호출이 composition에 있든 없든 상관없이 해당 코루틴이
+        // 실행되게 되므로 사용하면 안됨 -> 자원 낭비
+        LaunchedEffect(key1 = true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
