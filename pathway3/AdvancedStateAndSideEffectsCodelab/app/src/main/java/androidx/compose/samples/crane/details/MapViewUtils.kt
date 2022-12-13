1acc42f18956badce3074aecb4e455fd63503c35
JHwan <pon5976@naver.com>
2022-12-13 15:11:18 +0900

    3-1 )AdvancedStateAndSideEffectsCodelab 완료

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/details/MapViewUtils.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/details/MapViewUtils.kt
@@ -16,12 +16,17 @@
 
 package androidx.compose.samples.crane.details
 
-import android.os.Bundle
 import androidx.annotation.FloatRange
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.core.os.bundleOf
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.MapView
 
@@ -30,20 +35,43 @@ import com.google.android.libraries.maps.MapView
  */
 @Composable
 fun rememberMapViewWithLifecycle(): MapView {
-    val context = LocalContext.current
-    // TODO Codelab: DisposableEffect step. Make MapView follow the lifecycle
-    return remember {
-        MapView(context).apply {
-            id = R.id.map
-            onCreate(Bundle())
+  val context = LocalContext.current
+  val mapView = remember {
+    MapView(context).apply {
+        id = R.id.map
+    }
+  }
+
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(key1 = lifecycle, key2 = mapView) {
+
+        val lifecycleObserver = getMapLifecycleObserver(mapView)
+        lifecycle.addObserver(lifecycleObserver)
+        onDispose {
+            lifecycle.removeObserver(lifecycleObserver)
         }
     }
+
+
+    return mapView
 }
 
 fun GoogleMap.setZoom(
-    @FloatRange(from = MinZoom.toDouble(), to = MaxZoom.toDouble()) zoom: Float
+  @FloatRange(from = MinZoom.toDouble(), to = MaxZoom.toDouble()) zoom: Float
 ) {
-    resetMinMaxZoomPreference()
-    setMinZoomPreference(zoom)
-    setMaxZoomPreference(zoom)
+  resetMinMaxZoomPreference()
+  setMinZoomPreference(zoom)
+  setMaxZoomPreference(zoom)
+}
+
+private fun getMapLifecycleObserver(mapView: MapView): LifecycleEventObserver = LifecycleEventObserver { _: LifecycleOwner, event: Lifecycle.Event ->
+  when (event) {
+    Lifecycle.Event.ON_CREATE -> mapView.onCreate(bundleOf())
+    Lifecycle.Event.ON_START -> mapView.onStart()
+    Lifecycle.Event.ON_RESUME -> mapView.onResume()
+    Lifecycle.Event.ON_STOP -> mapView.onStop()
+    Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+    Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+    else -> throw IllegalStateException()
+  }
 }
