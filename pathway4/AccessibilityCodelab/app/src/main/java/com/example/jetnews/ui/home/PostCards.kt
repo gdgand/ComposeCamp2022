7889f2ee950db69573e59d8d8c40da65e0cb5268
pmh <pmh@hwajintni.co.kr>
2022-12-13 15:53:48 +0900

    [4][접근성지원] 라벨 클릭
    Card 컴포저블(Surface 쓰는)은 코드랩과 다르게 onclicklabel deprecated 되서 다른식으로 문구를 지정해보려 했으나 실패함

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
@@ -52,7 +52,9 @@ import com.example.jetnews.ui.theme.JetnewsTheme
 fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
     var openDialog by remember { mutableStateOf(false) }
     Row(
-        Modifier.clickable { navigateToArticle(post.id) }
+        Modifier.clickable(
+            onClickLabel = stringResource(id = R.string.action_read_article)
+        ) { navigateToArticle(post.id) }
     ) {
         Image(
             painter = painterResource(post.imageThumbId),
@@ -130,7 +132,8 @@ fun PostCardPopular(
 ) {
     Card(
         shape = MaterialTheme.shapes.medium,
-        modifier = modifier.size(280.dp, 240.dp),
+        modifier = modifier
+            .size(280.dp, 240.dp),
         onClick = { navigateToArticle(post.id) }
     ) {
         Column {
