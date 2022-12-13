cbcf207cf2318ff14b9f26c12de1686520bbd0a9
hyuna <hyuna9787@gmail.com>
2022-12-14 03:12:53 +0900

    Apply deepLinks

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -24,6 +24,7 @@ import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 
 /**
  * Contract for information needed on every Rally navigation destination
@@ -57,8 +58,9 @@ object SingleAccount : RallyDestination {
     override val icon = Icons.Filled.Money
     override val route = "single_account"
     const val accountTypeArg = "account_type"
-    val routeWithArgs = "${route}/{${accountTypeArg}}"
+    val routeWithArgs = "$route/{${accountTypeArg}}"
     val arguments = listOf(navArgument(accountTypeArg) { type = NavType.StringType })
+    val deepLinks = listOf(navDeepLink { uriPattern = "rally://$route/{$accountTypeArg}" })
 }
 
 // Screens to be displayed in the top RallyTabRow
