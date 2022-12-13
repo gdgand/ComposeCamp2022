0925cf45913d7c735fe03b3695e6689efd9ef0b6
Hz <hz6290@gmail.com>
2022-12-13 20:27:50 +0900

    Complete: 4-1. Testing

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,38 @@
+package com.example.compose.rally
+
+import androidx.compose.material.Text
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBarTest() {
+
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                true
+            )
+            .assertExists()
+    }
+
+}
