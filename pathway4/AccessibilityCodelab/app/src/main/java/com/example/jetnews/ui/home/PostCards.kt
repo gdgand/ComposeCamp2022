1aac26f917e81914c513fde26d5ca3f9f6e14e1c
mbkim95 <mbkim95@naver.com>
2022-12-14 01:36:49 +0900

    [Accessibility] 맞춤 작업

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/PostCards.kt
@@ -39,6 +39,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.CustomAccessibilityAction
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.customActions
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,41 +55,46 @@ import com.example.jetnews.ui.theme.JetnewsTheme
 @Composable
 fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
     var openDialog by remember { mutableStateOf(false) }
+    val showFewerLabel = stringResource(id = R.string.cd_show_fewer)
     Row(
-        Modifier.clickable(onClickLabel = stringResource(id = R.string.action_read_article)) {
-            navigateToArticle(post.id)
-        }
+        Modifier
+            .clickable(
+                onClickLabel = stringResource(id = R.string.action_read_article),
+            ) {
+                navigateToArticle(post.id)
+            }
+            .semantics {
+                customActions = listOf(
+                    CustomAccessibilityAction(
+                        label = showFewerLabel,
+                        action = { openDialog = true; true }
+                    )
+                )
+            },
     ) {
-        Image(
-            painter = painterResource(post.imageThumbId),
+        Image(painter = painterResource(post.imageThumbId),
             contentDescription = null,
             modifier = Modifier
                 .padding(top = 16.dp, start = 16.dp, end = 16.dp)
                 .size(40.dp, 40.dp)
-                .clip(MaterialTheme.shapes.small)
-        )
-        Column(
-            Modifier
-                .weight(1f)
-                .padding(top = 16.dp, bottom = 16.dp)
-        ) {
+                .clip(MaterialTheme.shapes.small))
+        Column(Modifier
+            .weight(1f)
+            .padding(top = 16.dp, bottom = 16.dp)) {
             Text(post.title, style = MaterialTheme.typography.subtitle1)
             Row(Modifier.padding(top = 4.dp)) {
                 CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                     val textStyle = MaterialTheme.typography.body2
-                    Text(
-                        text = post.metadata.author.name,
-                        style = textStyle
-                    )
-                    Text(
-                        text = " - ${post.metadata.readTimeMinutes} min read",
-                        style = textStyle
-                    )
+                    Text(text = post.metadata.author.name, style = textStyle)
+                    Text(text = " - ${post.metadata.readTimeMinutes} min read", style = textStyle)
                 }
             }
         }
         CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            IconButton(onClick = { openDialog = true }) {
+            IconButton(
+                modifier = Modifier.clearAndSetSemantics { },
+                onClick = { openDialog = true },
+            ) {
                 Icon(
                     imageVector = Icons.Default.Close,
                     contentDescription = stringResource(R.string.cd_show_fewer),
@@ -94,32 +103,24 @@ fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
         }
     }
     if (openDialog) {
-        AlertDialog(
-            modifier = Modifier.padding(20.dp),
+        AlertDialog(modifier = Modifier.padding(20.dp),
             onDismissRequest = { openDialog = false },
             title = {
-                Text(
-                    text = stringResource(id = R.string.fewer_stories),
-                    style = MaterialTheme.typography.h6
-                )
+                Text(text = stringResource(id = R.string.fewer_stories),
+                    style = MaterialTheme.typography.h6)
             },
             text = {
-                Text(
-                    text = stringResource(id = R.string.fewer_stories_content),
-                    style = MaterialTheme.typography.body1
-                )
+                Text(text = stringResource(id = R.string.fewer_stories_content),
+                    style = MaterialTheme.typography.body1)
             },
             confirmButton = {
-                Text(
-                    text = stringResource(id = R.string.agree),
+                Text(text = stringResource(id = R.string.agree),
                     style = MaterialTheme.typography.button,
                     color = MaterialTheme.colors.primary,
                     modifier = Modifier
                         .padding(15.dp)
-                        .clickable { openDialog = false }
-                )
-            }
-        )
+                        .clickable { openDialog = false })
+            })
     }
 }
 
@@ -128,7 +129,7 @@ fun PostCardHistory(post: Post, navigateToArticle: (String) -> Unit) {
 fun PostCardPopular(
     post: Post,
     navigateToArticle: (String) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Card(
         shape = MaterialTheme.shapes.medium,
@@ -138,39 +139,24 @@ fun PostCardPopular(
     ) {
         Column {
 
-            Image(
-                painter = painterResource(post.imageId),
-                contentDescription = null, // decorative
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
+            Image(painter = painterResource(post.imageId), contentDescription = null, // decorative
+                contentScale = ContentScale.Crop, modifier = Modifier
                     .height(100.dp)
-                    .fillMaxWidth()
-            )
+                    .fillMaxWidth())
 
             Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    text = post.title,
+                Text(text = post.title,
                     style = MaterialTheme.typography.h6,
                     maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    text = post.metadata.author.name,
+                    overflow = TextOverflow.Ellipsis)
+                Text(text = post.metadata.author.name,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.body2
-                )
+                    style = MaterialTheme.typography.body2)
 
-                Text(
-                    text = stringResource(
-                        id = R.string.home_post_min_read,
-                        formatArgs = arrayOf(
-                            post.metadata.date,
-                            post.metadata.readTimeMinutes
-                        )
-                    ),
-                    style = MaterialTheme.typography.body2
-                )
+                Text(text = stringResource(id = R.string.home_post_min_read,
+                    formatArgs = arrayOf(post.metadata.date, post.metadata.readTimeMinutes)),
+                    style = MaterialTheme.typography.body2)
             }
         }
     }
