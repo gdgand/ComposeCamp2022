acd9f5baf5251a1c28c5c4258f67bbf3b409fe02
jangc <jangc.net@gmail.com>
2022-12-13 15:24:34 +0900

    State hoisting

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -1,21 +1,21 @@
 package com.codelab.basics.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightColorPalette = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
     /* Other default colors to override
     background = Color.White,
@@ -39,9 +39,8 @@ fun BasicsCodelabTheme(
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colors,
         typography = Typography,
-        shapes = Shapes,
         content = content
     )
 }
