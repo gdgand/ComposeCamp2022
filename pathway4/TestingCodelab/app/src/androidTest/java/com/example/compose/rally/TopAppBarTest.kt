1693ee5252775acd5bf2a8483ca8092a94af1d7b
JiSeokYeom <ygs01033@naver.com>
2022-12-13 20:32:32 +0900

    pathway4: Merged and unmerged Semantics trees

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,7 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -44,6 +41,7 @@ class TopAppBarTest {
             .assertIsSelected()
     }
 
+    @Test
     fun rallyTopAppBarTest_currentLabelExists() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
@@ -55,7 +53,13 @@ class TopAppBarTest {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
