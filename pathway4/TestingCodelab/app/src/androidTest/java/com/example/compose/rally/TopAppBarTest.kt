14de25260df061bc0bf20c117661f3f87a92aa31
yeongjin <dudwls901@naver.com>
2022-12-14 14:33:53 +0900

    [Test] 시멘틱 트리 확인하여 Accounts, ACCOUNT 모두 확인

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,6 +1,9 @@
 package com.example.compose.rally
 
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onRoot
@@ -15,7 +18,7 @@ class TopAppBarTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun rallyTopAppBarTest(){
+    fun rallyTopAppBarTest() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
@@ -31,20 +34,30 @@ class TopAppBarTest {
     }
 
     @Test
-    fun rallyTopAppBarTest_currentLabelExists(){
+    fun rallyTopAppBarTest_currentLabelExists() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
                 allScreens = allScreens,
                 onTabSelected = {},
-                currentScreen =  RallyScreen.Accounts
+                currentScreen = RallyScreen.Accounts
             )
         }
 
-        composeTestRule.onRoot().printToLog("currentLabelExists")
-        
+        composeTestRule.onRoot(
+            useUnmergedTree = true
+        ).printToLog("currentLabelExists")
+
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(
+                    RallyScreen.Accounts.name.uppercase()
+                ) and
+                    hasParent(
+                        hasContentDescription(RallyScreen.Accounts.name)
+                    ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
