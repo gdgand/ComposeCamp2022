a7c3f07b472d18b6e25a2651745085003abde536
soo <onemask@naver.com>
2022-12-15 02:00:33 +0900

    done 7

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/ui/overview/OverviewScreen.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/ui/overview/OverviewScreen.kt
@@ -49,8 +49,11 @@ import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.example.compose.rally.R
+import com.example.compose.rally.SingleAccount
 import com.example.compose.rally.data.UserData
+import com.example.compose.rally.navigateSingleTopTo
 import com.example.compose.rally.ui.components.AccountRow
 import com.example.compose.rally.ui.components.BillRow
 import com.example.compose.rally.ui.components.RallyAlertDialog
@@ -58,32 +61,44 @@ import com.example.compose.rally.ui.components.RallyDivider
 import com.example.compose.rally.ui.components.formatAmount
 import java.util.Locale
 
+private fun NavHostController.navigateToSingleAccount(accountType: String) {
+  this.navigateSingleTopTo("${SingleAccount.route}/$accountType")
+}
+
 @Composable
 fun OverviewScreen(
-    onClickSeeAllAccounts: () -> Unit = {},
-    onClickSeeAllBills: () -> Unit = {},
-    onAccountClick: (String) -> Unit = {},
+  onClickSeeAllAccounts: () -> Unit = {},
+  onClickSeeAllBills: () -> Unit = {},
+  onAccountClick: (String) -> Unit = {},
 ) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState())
-            .semantics { contentDescription = "Overview Screen" }
-    ) {
-        AccountsCard(
-            onClickSeeAll = onClickSeeAllAccounts,
-            onAccountClick = onAccountClick
-        )
-        Spacer(Modifier.height(RallyDefaultPadding))
-        AccountsCard(
-            onClickSeeAll = onClickSeeAllAccounts,
-            onAccountClick = onAccountClick
-        )
-        Spacer(Modifier.height(RallyDefaultPadding))
-        BillsCard(
-            onClickSeeAll = onClickSeeAllBills
-        )
-    }
+  Column(
+    modifier = Modifier
+      .padding(16.dp)
+      .verticalScroll(rememberScrollState())
+      .semantics { contentDescription = "Overview Screen" }
+  ) {
+    AccountsCard(
+      onClickSeeAll = onClickSeeAllAccounts,
+      onAccountClick = {
+        navController.navigateToSingleAccount(it)
+      }
+    )
+    OverviewScreen(
+      // ...
+      onAccountClick = { accountType ->
+        navController.navigateToSingleAccount(accountType)
+      }
+    )
+    Spacer(Modifier.height(RallyDefaultPadding))
+    AccountsCard(
+      onClickSeeAll = onClickSeeAllAccounts,
+      onAccountClick = onAccountClick
+    )
+    Spacer(Modifier.height(RallyDefaultPadding))
+    BillsCard(
+      onClickSeeAll = onClickSeeAllBills
+    )
+  }
 }
 
 /**
@@ -91,83 +106,83 @@ fun OverviewScreen(
  */
 @Composable
 private fun AlertCard() {
-    var showDialog by remember { mutableStateOf(false) }
-    val alertMessage = "Heads up, you've used up 90% of your Shopping budget for this month."
+  var showDialog by remember { mutableStateOf(false) }
+  val alertMessage = "Heads up, you've used up 90% of your Shopping budget for this month."
 
-    if (showDialog) {
-        RallyAlertDialog(
-            onDismiss = {
-                showDialog = false
-            },
-            bodyText = alertMessage,
-            buttonText = "Dismiss".uppercase(Locale.getDefault())
-        )
+  if (showDialog) {
+    RallyAlertDialog(
+      onDismiss = {
+        showDialog = false
+      },
+      bodyText = alertMessage,
+      buttonText = "Dismiss".uppercase(Locale.getDefault())
+    )
+  }
+  Card {
+    Column {
+      AlertHeader {
+        showDialog = true
+      }
+      RallyDivider(
+        modifier = Modifier.padding(start = RallyDefaultPadding, end = RallyDefaultPadding)
+      )
+      AlertItem(alertMessage)
     }
-    Card {
-        Column {
-            AlertHeader {
-                showDialog = true
-            }
-            RallyDivider(
-                modifier = Modifier.padding(start = RallyDefaultPadding, end = RallyDefaultPadding)
-            )
-            AlertItem(alertMessage)
-        }
-    }
+  }
 }
 
 @Composable
 private fun AlertHeader(onClickSeeAll: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .padding(RallyDefaultPadding)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+  Row(
+    modifier = Modifier
+      .padding(RallyDefaultPadding)
+      .fillMaxWidth(),
+    horizontalArrangement = Arrangement.SpaceBetween
+  ) {
+    Text(
+      text = "Alerts",
+      style = MaterialTheme.typography.subtitle2,
+      modifier = Modifier.align(Alignment.CenterVertically)
+    )
+    TextButton(
+      onClick = onClickSeeAll,
+      contentPadding = PaddingValues(0.dp),
+      modifier = Modifier.align(Alignment.CenterVertically)
     ) {
-        Text(
-            text = "Alerts",
-            style = MaterialTheme.typography.subtitle2,
-            modifier = Modifier.align(Alignment.CenterVertically)
-        )
-        TextButton(
-            onClick = onClickSeeAll,
-            contentPadding = PaddingValues(0.dp),
-            modifier = Modifier.align(Alignment.CenterVertically)
-        ) {
-            Text(
-                text = "SEE ALL",
-                style = MaterialTheme.typography.button,
-            )
-        }
+      Text(
+        text = "SEE ALL",
+        style = MaterialTheme.typography.button,
+      )
     }
+  }
 }
 
 @Composable
 private fun AlertItem(message: String) {
-    Row(
-        modifier = Modifier
-            .padding(RallyDefaultPadding)
-            // Regard the whole row as one semantics node. This way each row will receive focus as
-            // a whole and the focus bounds will be around the whole row content. The semantics
-            // properties of the descendants will be merged. If we'd use clearAndSetSemantics instead,
-            // we'd have to define the semantics properties explicitly.
-            .semantics(mergeDescendants = true) {},
-        horizontalArrangement = Arrangement.SpaceBetween
+  Row(
+    modifier = Modifier
+      .padding(RallyDefaultPadding)
+      // Regard the whole row as one semantics node. This way each row will receive focus as
+      // a whole and the focus bounds will be around the whole row content. The semantics
+      // properties of the descendants will be merged. If we'd use clearAndSetSemantics instead,
+      // we'd have to define the semantics properties explicitly.
+      .semantics(mergeDescendants = true) {},
+    horizontalArrangement = Arrangement.SpaceBetween
+  ) {
+    Text(
+      style = MaterialTheme.typography.body2,
+      modifier = Modifier.weight(1f),
+      text = message
+    )
+    IconButton(
+      onClick = {},
+      modifier = Modifier
+        .align(Alignment.Top)
+        .clearAndSetSemantics {}
     ) {
-        Text(
-            style = MaterialTheme.typography.body2,
-            modifier = Modifier.weight(1f),
-            text = message
-        )
-        IconButton(
-            onClick = {},
-            modifier = Modifier
-                .align(Alignment.Top)
-                .clearAndSetSemantics {}
-        ) {
-            Icon(Icons.Filled.Sort, contentDescription = null)
-        }
+      Icon(Icons.Filled.Sort, contentDescription = null)
     }
+  }
 }
 
 /**
@@ -175,53 +190,53 @@ private fun AlertItem(message: String) {
  */
 @Composable
 private fun <T> OverviewScreenCard(
-    title: String,
-    amount: Float,
-    onClickSeeAll: () -> Unit,
-    values: (T) -> Float,
-    colors: (T) -> Color,
-    data: List<T>,
-    row: @Composable (T) -> Unit
+  title: String,
+  amount: Float,
+  onClickSeeAll: () -> Unit,
+  values: (T) -> Float,
+  colors: (T) -> Color,
+  data: List<T>,
+  row: @Composable (T) -> Unit,
 ) {
-    Card {
-        Column {
-            Column(Modifier.padding(RallyDefaultPadding)) {
-                Text(text = title, style = MaterialTheme.typography.subtitle2)
-                val amountText = "$" + formatAmount(
-                    amount
-                )
-                Text(text = amountText, style = MaterialTheme.typography.h2)
-            }
-            OverViewDivider(data, values, colors)
-            Column(Modifier.padding(start = 16.dp, top = 4.dp, end = 8.dp)) {
-                data.take(SHOWN_ITEMS).forEach { row(it) }
-                SeeAllButton(
-                    modifier = Modifier.clearAndSetSemantics {
-                        contentDescription = "All $title"
-                    },
-                    onClick = onClickSeeAll,
-                )
-            }
-        }
+  Card {
+    Column {
+      Column(Modifier.padding(RallyDefaultPadding)) {
+        Text(text = title, style = MaterialTheme.typography.subtitle2)
+        val amountText = "$" + formatAmount(
+          amount
+        )
+        Text(text = amountText, style = MaterialTheme.typography.h2)
+      }
+      OverViewDivider(data, values, colors)
+      Column(Modifier.padding(start = 16.dp, top = 4.dp, end = 8.dp)) {
+        data.take(SHOWN_ITEMS).forEach { row(it) }
+        SeeAllButton(
+          modifier = Modifier.clearAndSetSemantics {
+            contentDescription = "All $title"
+          },
+          onClick = onClickSeeAll,
+        )
+      }
     }
+  }
 }
 
 @Composable
 private fun <T> OverViewDivider(
-    data: List<T>,
-    values: (T) -> Float,
-    colors: (T) -> Color
+  data: List<T>,
+  values: (T) -> Float,
+  colors: (T) -> Color,
 ) {
-    Row(Modifier.fillMaxWidth()) {
-        data.forEach { item: T ->
-            Spacer(
-                modifier = Modifier
-                    .weight(values(item))
-                    .height(1.dp)
-                    .background(colors(item))
-            )
-        }
+  Row(Modifier.fillMaxWidth()) {
+    data.forEach { item: T ->
+      Spacer(
+        modifier = Modifier
+          .weight(values(item))
+          .height(1.dp)
+          .background(colors(item))
+      )
     }
+  }
 }
 
 /**
@@ -229,23 +244,23 @@ private fun <T> OverViewDivider(
  */
 @Composable
 private fun AccountsCard(onClickSeeAll: () -> Unit, onAccountClick: (String) -> Unit) {
-    val amount = UserData.accounts.map { account -> account.balance }.sum()
-    OverviewScreenCard(
-        title = stringResource(R.string.accounts),
-        amount = amount,
-        onClickSeeAll = onClickSeeAll,
-        data = UserData.accounts,
-        colors = { it.color },
-        values = { it.balance }
-    ) { account ->
-        AccountRow(
-            modifier = Modifier.clickable { onAccountClick(account.name) },
-            name = account.name,
-            number = account.number,
-            amount = account.balance,
-            color = account.color
-        )
-    }
+  val amount = UserData.accounts.map { account -> account.balance }.sum()
+  OverviewScreenCard(
+    title = stringResource(R.string.accounts),
+    amount = amount,
+    onClickSeeAll = onClickSeeAll,
+    data = UserData.accounts,
+    colors = { it.color },
+    values = { it.balance }
+  ) { account ->
+    AccountRow(
+      modifier = Modifier.clickable { onAccountClick(account.name) },
+      name = account.name,
+      number = account.number,
+      amount = account.balance,
+      color = account.color
+    )
+  }
 }
 
 /**
@@ -253,34 +268,34 @@ private fun AccountsCard(onClickSeeAll: () -> Unit, onAccountClick: (String) -> 
  */
 @Composable
 private fun BillsCard(onClickSeeAll: () -> Unit) {
-    val amount = UserData.bills.map { bill -> bill.amount }.sum()
-    OverviewScreenCard(
-        title = stringResource(R.string.bills),
-        amount = amount,
-        onClickSeeAll = onClickSeeAll,
-        data = UserData.bills,
-        colors = { it.color },
-        values = { it.amount }
-    ) { bill ->
-        BillRow(
-            name = bill.name,
-            due = bill.due,
-            amount = bill.amount,
-            color = bill.color
-        )
-    }
+  val amount = UserData.bills.map { bill -> bill.amount }.sum()
+  OverviewScreenCard(
+    title = stringResource(R.string.bills),
+    amount = amount,
+    onClickSeeAll = onClickSeeAll,
+    data = UserData.bills,
+    colors = { it.color },
+    values = { it.amount }
+  ) { bill ->
+    BillRow(
+      name = bill.name,
+      due = bill.due,
+      amount = bill.amount,
+      color = bill.color
+    )
+  }
 }
 
 @Composable
 private fun SeeAllButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
-    TextButton(
-        onClick = onClick,
-        modifier = modifier
-            .height(44.dp)
-            .fillMaxWidth()
-    ) {
-        Text(stringResource(R.string.see_all))
-    }
+  TextButton(
+    onClick = onClick,
+    modifier = modifier
+      .height(44.dp)
+      .fillMaxWidth()
+  ) {
+    Text(stringResource(R.string.see_all))
+  }
 }
 
 private val RallyDefaultPadding = 12.dp
