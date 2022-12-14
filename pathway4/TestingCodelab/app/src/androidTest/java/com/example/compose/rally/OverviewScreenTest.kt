54ed29e1d00afdc4d8a72ba29eda917446a40b10
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-12-14 18:18:04 +0900

    Pathway 2 : Jetpack Compose의 고급 상태 및 부작용

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/OverviewScreenTest.kt
@@ -0,0 +1,29 @@
+package com.example.compose.rally
+
+/**
+ * Written by StephenLeeDev on 2022/12/14.
+ */
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.compose.rally.ui.overview.OverviewBody
+import org.junit.Rule
+import org.junit.Test
+
+class OverviewScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
+}
