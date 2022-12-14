8385a2504764246a7310489e99dd24d053ff796f
강신범 <mimic12@spidor.co.kr>
2022-12-14 19:04:41 +0900

    [Pathway 4] Create simple UI test 진행

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,40 @@
+package com.example.compose.rally
+
+import androidx.compose.material.Text
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun myTest() {
+        composeTestRule.setContent {
+            Text(text = "You can set any Compose content!")
+        }
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreen = RallyScreen.values().toList()
+
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = { /*TODO*/ },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+//        Thread.sleep(5000)
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+}
