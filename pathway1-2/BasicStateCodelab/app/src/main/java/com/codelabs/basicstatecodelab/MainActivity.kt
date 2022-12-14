30bca2f0edecccb2b707abe8f548c772f4159c80
mr_kim <rofalfl42@gmail.com>
2022-12-15 02:12:13 +0900

    [Compose Camp] 컴포즈 캠프 Pathway 1-2 첫번째 코스 Compose에서의 상태 진행

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,28 +18,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicStateCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyWellnessApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyWellnessApp() {
+    BasicStateCodelabTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            WellnessScreen()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(widthDp = 360, heightDp = 640)
 @Composable
-fun DefaultPreview() {
-    BasicStateCodelabTheme {
-        Greeting("Android")
-    }
+fun MyWellnessPreview() {
+    MyWellnessApp()
 }
