8f3a92bd4ae8ac3bddc4d95a90456ac9ee4cb632
dragonet85 <dragonet85@gmail.com>
2022-12-14 23:27:35 +0900

    animation - 콘텐츠 크기 변경 애니메이션

--- pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ pathway2/AnimationCodelab/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -333,6 +333,7 @@ private fun TopicRow(topic: String, expanded: Boolean, onClick: () -> Unit) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
+                .animateContentSize()
         ) {
             Row {
                 Icon(
