8e1d99bb63de128baad7d23ee0154d0159b130ca
mingu <35962595+kangmingu93@users.noreply.github.com>
2022-12-15 00:12:23 +0900

    Pathway4/TestingCodelab (#9)
    * 3. Create a simple UI test
    
    * 4. Debugging tests
    
    * 5. Merged and unmerged Semantics trees
    
    * 6. Synchronization

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/AnimatingCircleTests.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/AnimatingCircleTests.kt
@@ -21,13 +21,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.MainTestClock
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.unit.dp
 import androidx.test.filters.SdkSuppress
 import com.example.compose.rally.ui.components.AnimatedCircle
+import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
 import org.junit.Test
@@ -48,6 +47,54 @@ class AnimatingCircleTests {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTablSelected() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(hasContentDescription(RallyScreen.Accounts.name)),
+                useUnmergedTree = true
+            )
+            .assertExists()
+    }
 
     @Test
     fun circleAnimation_idle_screenshot() {
@@ -94,7 +141,9 @@ class AnimatingCircleTests {
         composeTestRule.setContent {
             RallyTheme {
                 AnimatedCircle(
-                    modifier = Modifier.background(Color.White).size(320.dp),
+                    modifier = Modifier
+                        .background(Color.White)
+                        .size(320.dp),
                     proportions = listOf(0.25f, 0.5f, 0.25f),
                     colors = listOf(Color.Red, Color.DarkGray, Color.Black)
                 )
