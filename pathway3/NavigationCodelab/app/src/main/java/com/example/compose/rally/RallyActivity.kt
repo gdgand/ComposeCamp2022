affc8cf790e0ab31bc720d8889ea0bc1f3c1c2c4
soo <onemask@naver.com>
2022-12-15 01:57:44 +0900

    done 5

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -28,6 +28,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.theme.RallyTheme
 
@@ -36,30 +40,45 @@ import com.example.compose.rally.ui.theme.RallyTheme
  * https://material.io/design/material-studies/rally.html
  */
 class RallyActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            RallyApp()
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      RallyApp()
     }
+  }
 }
+
+fun NavHostController.navigateSingleTopTo(route: String) =
+  this.navigate(route) {
+    popUpTo(
+      this@navigateSingleTopTo.graph.findStartDestination().id
+    ) {
+      saveState = true
+    }
+    launchSingleTop = true
+    restoreState = true
+  }
 
 @Composable
 fun RallyApp() {
-    RallyTheme {
-        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
-        Scaffold(
-            topBar = {
-                RallyTabRow(
-                    allScreens = rallyTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
-                    currentScreen = currentScreen
-                )
-            }
-        ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
-            }
-        }
-    }
+  RallyTheme {
+    val navController = rememberNavController()
+    val currentBackStack by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStack?.destination
+    val currentScreen = rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview
+
+
+    Scaffold(
+      topBar = {
+        RallyTabRow(
+          allScreens = rallyTabRowScreens,
+          onTabSelected = { newScreen ->
+            navController
+              .navigateSingleTopTo(newScreen.route)
+          },
+          currentScreen = currentScreen,
+        )
+      }
+    )
+  }
 }
