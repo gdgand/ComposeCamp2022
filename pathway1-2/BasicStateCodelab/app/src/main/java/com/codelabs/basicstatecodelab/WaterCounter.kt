518a359367c6932ce133479a83b0c74be9030b13
DongsikKim <dongsik.kim@navercorp.com>
2022-12-15 22:16:57 +0900

    상태 기반 UI

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -17,15 +17,19 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
-        Text(
-            text = "You've had ${count} galsess",
-            modifier = modifier.padding(16.dp)
-        )
+        if (count > 0) {
+            Text(
+                text = "You've had ${count} galsess",
+                modifier = modifier.padding(16.dp)
+            )
+        }
+
         Button(
             modifier = Modifier.padding(top = 8.dp),
             onClick = {
                 count++
-            }
+            },
+            enabled = count < 10
         ) {
             Text("Add One")
         }
