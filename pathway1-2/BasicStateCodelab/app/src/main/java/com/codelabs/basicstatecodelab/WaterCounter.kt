e0e99b93a9fd3229f5fad69fb1d928e352cf8507
DongsikKim <dongsik.kim@navercorp.com>
2022-12-15 23:01:29 +0900

    Compose에서 상태 복원

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,13 +1,14 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,23 +16,43 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
 
         if (count > 0) {
+            var showTask by rememberSaveable { mutableStateOf(true) }
+            if (showTask) {
+
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = {
+                        // 닫을때 할 행동
+                        showTask = false
+                    })
+            }
             Text(
-                text = "You've had ${count} galsess",
+                text = "You've had $count galsess",
                 modifier = modifier.padding(16.dp)
             )
         }
 
-        Button(
-            modifier = Modifier.padding(top = 8.dp),
-            onClick = {
-                count++
-            },
-            enabled = count < 10
-        ) {
-            Text("Add One")
+        Row(modifier = Modifier.padding(8.dp)) {
+            Button(
+                modifier = Modifier.padding(top = 8.dp),
+                onClick = {
+                    count++
+                },
+                enabled = count < 10
+            ) {
+                Text("Add One")
+            }
+            Button(
+                onClick = {
+                    count = 0
+                },
+                modifier = Modifier.padding(start = 8.dp, top = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 }
