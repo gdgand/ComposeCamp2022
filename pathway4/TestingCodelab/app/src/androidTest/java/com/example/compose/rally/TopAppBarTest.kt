dede536ee0bb5185498ec39c91d60e544e305010
jangc <jangc.net@gmail.com>
2022-12-15 13:10:32 +0900

    Testing in Jetpack Compose - 5. Merged and unmerged Semantics trees

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -38,10 +38,15 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
-        composeTestRule.onRoot().printToLog("currentLabelExists")
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
