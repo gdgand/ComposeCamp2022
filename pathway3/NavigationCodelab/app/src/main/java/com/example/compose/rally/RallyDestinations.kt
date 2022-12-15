98c29e44ef15f095490f8740435a66c8d04f7740
Jaeung Choi <jaeung.choi@navercorp.com>
2022-12-15 15:29:25 +0900

    jetpack Compose 탐색 - 8

--- pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ pathway3/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.compose.rally.ui.accounts.AccountsScreen
 import com.example.compose.rally.ui.accounts.SingleAccountScreen
 import com.example.compose.rally.ui.bills.BillsScreen
@@ -63,9 +64,8 @@ object SingleAccount : RallyDestination {
     override val route = "single_account"
     const val accountTypeArg = "account_type"
     val routeWithArgs = "${route}/{${accountTypeArg}}"
-    val arguments = listOf(
-        navArgument(accountTypeArg) { type = NavType.StringType }
-    )
+    val arguments = listOf(navArgument(accountTypeArg) { type = NavType.StringType })
+    val deepLinks = listOf(navDeepLink { uriPattern = "rally://$route/{$accountTypeArg}" })
 }
 
 // Screens to be displayed in the top RallyTabRow
