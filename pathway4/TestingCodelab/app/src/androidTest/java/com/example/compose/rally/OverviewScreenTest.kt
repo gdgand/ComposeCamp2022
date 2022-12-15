025bf612d201860c138065767a66d27c135748f7
WooSeok-03 <7458dntjr@gmail.com>
2022-12-15 21:45:17 +0900

    [ PATHWAY 4 : Codelap 1 ] 테스트

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/OverviewScreenTest.kt
@@ -0,0 +1,30 @@
+package com.example.compose.rally
+
+import androidx.compose.animation.core.*
+import androidx.compose.material.Card
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.compose.rally.ui.overview.OverviewBody
+import org.junit.Rule
+import org.junit.Test
+
+class OverviewScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
+}
