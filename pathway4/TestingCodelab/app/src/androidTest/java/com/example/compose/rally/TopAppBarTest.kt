958068b05913ceeb2c0f4fd86ece35b9a4f0508d
강신범 <mimic12@spidor.co.kr>
2022-12-15 18:18:26 +0900

    [Pathway 4] Merged and unmerged Semantics trees 진행

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -48,8 +48,16 @@ class TopAppBarTest {
             )
         }
 
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                        useUnmergedTree = true
+            )
             .assertExists()
     }
 }
