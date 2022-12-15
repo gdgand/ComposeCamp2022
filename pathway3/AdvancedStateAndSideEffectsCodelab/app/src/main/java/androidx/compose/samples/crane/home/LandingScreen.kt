cbfb045a4fac77f3dcb815ef5cc68ee55782d4f4
junyoungleee <jun991203@naver.com>
2022-12-15 16:36:08 +0900

    ✅ [Pathway3] AdvancedStateAndSideEffectsCodelab 완료

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,10 +20,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
@@ -31,7 +35,18 @@ private const val SplashWaitTime: Long = 2000
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         // TODO Codelab: LaunchedEffect and rememberUpdatedState step
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
         // TODO: Make LandingScreen disappear after loading data
+        // Compose의 side effect : Composable 함수 범위 밖에서 발생하는 앱 상테에 관한 변경사항
+        // onTimeout 콜백 : 랜딩 화면 표시/숨기기를 위해 상태를 변경하는 것이 발생
+        // onTimeout 호출 전에 코루틴을 통해 항목을 로드해야 하므로 코루틴의 context에서 상태 변경이 발생해야 함
+
+        // 수명 주기 동안 1번만 트리거하기 위해서 상수를 키로 사용
+        // onTimeout이 변경되면서 효과가 끝날 때 마지막 onTimeout이 호출된다는 보장 X -> rememberUpdatedState 사용
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)  // 임시
+            currentOnTimeout()
+        }
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
