c5fb2e2af5cee02897d8daf93f1b8e09b3867ded
yoonsangkwak <yunsang08@naver.com>
2022-12-16 21:44:55 +0900

    BasicCodelab

--- pathway1/BasicsCodelab/app/src/androidTest/java/com/example/basiccodelab/ExampleInstrumentedTest.kt
+++ pathway1/BasicsCodelab/app/src/androidTest/java/com/example/basiccodelab/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.codelab.basics
+package com.example.basiccodelab
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.codelab.basics", appContext.packageName)
+        assertEquals("com.example.basiccodelab", appContext.packageName)
     }
 }
