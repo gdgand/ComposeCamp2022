7c197f52c993a8cd62311586b017ef5351c7a29d
dmoon4117 <dmoon4117@naver.com>
2022-12-16 14:51:41 +0900

    PathWay 4 AccessibilityCodelab

--- pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/MainActivity.kt
+++ pathway4/AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/MainActivity.kt
@@ -17,12 +17,12 @@
 package com.example.jetnews.ui
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.example.jetnews.JetnewsApplication
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
