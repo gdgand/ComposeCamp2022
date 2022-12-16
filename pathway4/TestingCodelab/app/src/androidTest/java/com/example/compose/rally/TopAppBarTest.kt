f760902f7e2a47f3f56a0b67e401e58253fbd838
sorea.no <sorae.no@thingsflow.com>
2022-12-17 00:54:35 +0900

    디버깅테스트, 시맨틱 트리, 동기화

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,90 @@
+package com.example.compose.rally
+
+import androidx.compose.material.Text
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.theme.RallyTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun myTest() {
+        composeTestRule.setContent {
+            Text("You can set any Compose content!")
+        }
+    }
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = { },
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
+        }
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = { },
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+//    @Test
+//    fun rallyTopAppBarTest_currentLabelExists() {
+//        val allScreens = RallyScreen.values().toList()
+//        composeTestRule.setContent {
+//            RallyTopAppBar(
+//                allScreens = allScreens,
+//                onTabSelected = { },
+//                currentScreen = RallyScreen.Accounts
+//            )
+//        }
+//
+//        composeTestRule
+//            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+//            .assertExists()
+//    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
+
+    }
+}
---
+++ pathway4/TestingCodelab/app/src/main/java/com/example/compose/rally/memo.md
@@ -0,0 +1,14 @@
+https://developer.android.com/codelabs/jetpack-compose-testing?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fcompose%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-testing#0
+
+## createAndroidComposeRule
+In a Compose test, we can start the app's main activity similarly to how you would do it in the Android View world using Espresso, 
+for example. You can do this with createAndroidComposeRule.
+
+## 테스트 요약본
+https://developer.android.com/jetpack/compose/testing-cheatsheet?authuser=1
+https://developer.android.com/reference/kotlin/androidx/compose/ui/test/package-summary?authuser=1
+
+
+## Semantics tree
+Compose tests use a structure called the semantics tree to look for elements on the screen and read their properties.
+
